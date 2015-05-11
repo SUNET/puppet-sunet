6fa57c9e075811d26d0c535509b30ab6d9026d14
Leif Johansson <leifj@sunet.se>
2015-05-11 17:09:49 +0200

    uniquify

--- manifests/cloudimage.pp
+++ manifests/cloudimage.pp
@@ -20,15 +20,20 @@ define sunet::cloudimage (
   $image_url_a = split($image_url,"/")
   $image_name = $image_url_a[-1]
   $image_src = "/var/lib/libvirt/images/${image_name}"
-  file { "/var/lib/libvirt/images/${name}": ensure => directory } ->
-  exec {"wget -O${image_src} ${image_url}":
-     onlyif => "test ! -f ${image_src}"
+  file { "${name}_libvirt_images": 
+     path   => "/var/lib/libvirt/images/${name}",
+     ensure => directory } ->
+  exec { "${name}_fetch_image":
+     command => "wget -O${image_src} ${image_url}",
+     onlyif  => "test ! -f ${image_src}"
   }
-  file { "/var/lib/libvirt/images/${name}/${name}-init.sh":
+  file { "${name}_init_script":
+     path    => "/var/lib/libvirt/images/${name}/${name}-init.sh",
      content => template("sunet/cloudimage/mk_cloud_image.erb"),
      mode    => "0755"
   } -> 
-  exec { "/var/lib/libvirt/images/${name}/${name}-init.sh":
-     onlyif => "test ! -f /var/lib/libvirt/images/${name}/${name}.img"
+  exec { "${name}_init":
+     command => "/var/lib/libvirt/images/${name}/${name}-init.sh",
+     onlyif  => "test ! -f /var/lib/libvirt/images/${name}/${name}.img"
   }
 }
