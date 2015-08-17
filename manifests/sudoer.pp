ea7c8f66aadf111e36c3161615dad05e97374402
Leif Johansson <leifj@sunet.se>
2015-08-18 00:09:08 +0200

    avoid tag as is meta

--- manifests/sudoer.pp
+++ manifests/sudoer.pp
@@ -1,18 +1,18 @@
 include stdlib
 include concat
 
-define sunet::sudoer($user_name = undef, $command_line = undef, $tag = undef) {
-  $collection = $tag ? {
+define sunet::sudoer($user_name = undef, $command_line = undef, $collection = undef) {
+  $file = $collection ? {
     undef   => $name,
-    default => $sudoers
+    default => $collection
   }
-  ensure_resource('concat',"/etc/sudoers.d/${collection}",{
+  ensure_resource('concat',"/etc/sudoers.d/${file}",{
     owner   => 'root',
     group   => 'root',
     mode    => '0440' 
   })
-  concat::fragment {"sudoer_${collction}_${name}":
-    target  => "/etc/sudoers.d/${collection}",
+  concat::fragment {"sudoer_${file}_${name}":
+    target  => "/etc/sudoers.d/${file}",
     content => template('sunet/sudoers/sudoer.erb'),
     order   => '30'
   }
