f0c3bd0a847f233b9c81a5cfe5cb295fc20959a9
Leif Johansson <leifj@sunet.se>
2015-08-26 12:06:39 +0200

    better dependency order

--- manifests/dockerhost.pp
+++ manifests/dockerhost.pp
@@ -13,29 +13,29 @@ class sunet::dockerhost {
   class {'docker':
      manage_package     => false,
   } ->
-  package { 'unbound': ensure => 'latest' } ->
-  service { 'unbound': ensure => 'running' }
-
+  file { '/usr/local/etc/docker.d': ensure => directory } ->
   file { '/usr/local/etc/docker.d/20unbound':
     ensure  => file,
     path    => '/usr/local/etc/docker.d/20unbound',
     mode    => '0755',
     content => template('sunet/dockerhost/20unbound.erb'),
-  }
-
-  file { '/etc/logrotate.d/docker-containers':
-    ensure  => file,
-    path    => '/etc/logrotate.d/docker-containers',
-    mode    => '0644',
-    content => template('sunet/dockerhost/logrotate_docker-containers.erb'),
-  }
-
+  } ->
   file { '/etc/unbound/unbound.conf.d/docker.conf':
     ensure  => file,
     path    => '/etc/unbound/unbound.conf.d/docker.conf',
     mode    => '0644',
     content => template('sunet/dockerhost/unbound_docker.conf.erb'),
     notify  => Service['unbound'],
+  }
+  package { 'unbound': ensure => 'latest' } ->
+  service { 'unbound': ensure => 'running' }
+
+  ensure_resource('file','/etc/logrotate.d',{ ensure => directory })
+  file { '/etc/logrotate.d/docker-containers':
+    ensure  => file,
+    path    => '/etc/logrotate.d/docker-containers',
+    mode    => '0644',
+    content => template('sunet/dockerhost/logrotate_docker-containers.erb'),
   }
 
   ufw::allow { 'allow-docker-resolving_udp':
