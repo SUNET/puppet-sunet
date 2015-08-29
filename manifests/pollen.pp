3046b6fb3fa92bde293259f3a660a10ec14967ee
Leif Johansson <leifj@sunet.se>
2015-08-29 14:45:18 +0200

    re-pollinate every so often

---
+++ manifests/pollen.pp
@@ -0,0 +1,15 @@
+define sunet::pollen($server = "https://random.nordu.net", $device = "/dev/random") {
+   package {"pollinate": ensure => latest } ->
+   file { "/etc/default/pollinate": 
+      ensure => file,
+      owner  => root,
+      group  => root,
+      content => template("sunet/pollen/pollinate.erb")
+   } ->
+   cron {"repollinate": 
+      command => "pollinate -r"
+      user    => root,
+      hour    => "*",
+      minute  => "*/15"
+   }
+}
