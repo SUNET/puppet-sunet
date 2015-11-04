3e3cf5f03b75029008dcaee9b92a70e24425c5f4
Fredrik Thulin <fredrik@thulin.net>
2015-11-04 20:26:48 +0100

    rename file -> filename

--- manifests/snippets.pp
+++ manifests/snippets.pp
@@ -44,23 +44,23 @@ define sunet::snippets::file_line($filename, $line, $ensure = 'present') {
   case $ensure {
     default : { err ( "unknown ensure value ${ensure}" ) }
     present: {
-      exec { "/bin/echo '${line}' >> '${file}'":
-        unless => "/bin/grep -qFx '${line}' '${file}'"
+      exec { "/bin/echo '${line}' >> '${filename}'":
+        unless => "/bin/grep -qFx '${line}' '${filename}'"
       }
     }
     absent: {
-      exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
-        onlyif => "/bin/grep -qFx '${line}' '${file}'"
+      exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${filename}'":
+        onlyif => "/bin/grep -qFx '${line}' '${filename}'"
       }
     }
     uncomment: {
-      exec { "/bin/sed -i -e'/${line}/s/^#\\+//' '${file}'":
-        onlyif => "/bin/grep '${line}' '${file}' | /bin/grep '^#' | /usr/bin/wc -l"
+      exec { "/bin/sed -i -e'/${line}/s/^#\\+//' '${filename}'":
+        onlyif => "/bin/grep '${line}' '${filename}' | /bin/grep '^#' | /usr/bin/wc -l"
       }
     }
     comment: {
-      exec { "/bin/sed -i -e'/${line}/s/^\\(.\\+\\)$/#\\1/' '${file}'":
-        onlyif => "/usr/bin/test `/bin/grep '${line}' '${file}' | /bin/grep -v '^#' | /usr/bin/wc -l` -ne 0"
+      exec { "/bin/sed -i -e'/${line}/s/^\\(.\\+\\)$/#\\1/' '${filename}'":
+        onlyif => "/usr/bin/test `/bin/grep '${line}' '${filename}' | /bin/grep -v '^#' | /usr/bin/wc -l` -ne 0"
       }
     }
   }
