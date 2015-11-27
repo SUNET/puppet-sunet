6d740ade23c96da50c77d7167e31a5b3f5feebd9
John Van de Meulebrouck Brendgard <john@nordu.net>
2015-11-27 17:32:38 +0100

    Added Exec path to sunet::snippets::encrypted_swap()

--- manifests/snippets.pp
+++ manifests/snippets.pp
@@ -27,6 +27,7 @@ define sunet::snippets::encrypted_swap() {
   exec {'sunet_ecryptfs_setup_swap':
     command => '/usr/bin/ecryptfs-setup-swap -f',
     onlyif  => 'grep swap /etc/fstab | grep -ve ^# -e cryptswap | grep -q swap',
+    path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin', ],
   }
 
 }
