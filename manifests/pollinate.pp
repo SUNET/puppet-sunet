1fa5a79bd025643fa2e00acda877b66598ac741d
Leif Johansson <leifj@sunet.se>
2015-12-02 14:24:12 +0100

    bad syntax bad

--- manifests/pollinate.pp
+++ manifests/pollinate.pp
@@ -1,7 +1,7 @@
 define sunet::pollinate($device = "/dev/random") {
    if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease == '12.04' {
       apt::ppa {'ppa:ndn/pollen': }
-   } ->
+   }
    package {"pollinate": ensure => latest } ->
    file { "/etc/default/pollinate": 
       ensure => file,
