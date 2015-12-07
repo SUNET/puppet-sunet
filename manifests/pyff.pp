3bbcb1350b455b4cc00f27f987b0958307c8d4ba
Leif Johansson <leifj@sunet.se>
2015-12-07 11:25:24 +0100

    support choosing ip address to bind pyff to

--- manifests/pyff.pp
+++ manifests/pyff.pp
@@ -1,16 +1,20 @@
-define sunet::pyff($version = "latest", $image = "docker.sunet.se/pyff") {
+define sunet::pyff($version = "latest", $image = "docker.sunet.se/pyff", $ip = undef) {
+   $ip_addr = $ip ? {
+      undef   => "",
+      default => "${ip}:"
+   }
    sunet::docker_run{"pound-${name}":
       image    => "docker.sunet.se/pound",
       imagetag => "latest",
       volumes  => ["/etc/ssl:/etc/ssl"],
       env      => ["BACKEND_PORT=tcp://varnish-${name}.docker:80"],
-      ports    => ["443:443"],
+      ports    => ["${ip_addr}443:443"],
       start_on => "docker-varnish-${name}"
    }
    sunet::docker_run {"varnish-${name}":
       image    => 'docker.sunet.se/varnish',
       env      => ["BACKEND_PORT=tcp://pyff-${name}.docker:8080"],
-      ports    => ['80:80'],
+      ports    => ['${ip_addr}80:80'],
       start_on => "docker-pyff-${name}"
    }
    sunet::docker_run {"pyff-${name}":
