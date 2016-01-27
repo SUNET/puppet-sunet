81722cfd87670c726cc41433d1851a446b16223a
Leif Johansson <leifj@sunet.se>
2016-01-27 13:48:46 +0100

    try to do what eduid does

--- manifests/pyff.pp
+++ manifests/pyff.pp
@@ -3,21 +3,22 @@ define sunet::pyff($version = "latest", $image = "docker.sunet.se/pyff", $dir = 
       undef   => "",
       default => "${ip}:"
    }
-   sunet::docker_run{"pound-${name}":
+   $sanitised_title = regsubst($title, '[^0-9A-Za-z.\-]', '-', 'G')
+   sunet::docker_run{"pound-${sanitized_title}":
       image    => "docker.sunet.se/pound",
       imagetag => "latest",
       volumes  => ["/etc/ssl:/etc/ssl"],
-      env      => ["BACKEND_PORT=tcp://varnish-${name}.docker:80"],
+      env      => ["BACKEND_PORT=tcp://varnish-${sanitized_title}.docker:80"],
       ports    => ["${ip_addr}443:443"],
-      start_on => "docker-varnish-${name}"
+      start_on => "docker-varnish-${sanitized_title}"
    }
-   sunet::docker_run {"varnish-${name}":
+   sunet::docker_run {"varnish-${sanitized_title}":
       image    => 'docker.sunet.se/varnish',
-      env      => ["BACKEND_PORT=tcp://pyff-${name}.docker:8080"],
+      env      => ["BACKEND_PORT=tcp://pyff-${sanitized_title}.docker:8080"],
       ports    => ["${ip_addr}80:80"],
-      start_on => "docker-pyff-${name}"
+      start_on => "docker-pyff-${sanitized_title}"
    }
-   sunet::docker_run {"pyff-${name}":
+   sunet::docker_run {"pyff-${sanitized_title}":
       image     => $image,
       imagetag => $version,
       volumes   => ["$dir:/opt/metadata"],
