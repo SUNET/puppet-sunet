fbd144916c87558f4cabb2dae364a39f98c10808
Leif Johansson <leifj@sunet.se>
2016-02-03 12:49:41 +0100

    ucrandom client

---
+++ manifests/ucrandom.pp
@@ -0,0 +1,29 @@
+define sunet::ucrandom(
+   $server="random.nordu.net",
+   $port="4711",
+   $device="/dev/random") {
+   
+   file {'/usr/bin/ucrandom': 
+      owner   => root,
+      group   => root,
+      mode    => '0775',
+      content => template("sunet/ucrandom/ucrandom.erb")
+   } ->
+   file {'/etc/init/ucrandom.conf': 
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0660',
+      content => template("sunet/ucrandom/ucrandom.conf.erb")
+   } ->
+   file {'/etc/default': ensure => directory } ->
+   file {'/etc/default/ucrandom': 
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0660',
+      replace => no,
+      content => template("sunet/ucrandom/default.erb")
+   } ->
+   service {'ucrandom': ensure => running }
+}
