7e378d4e97609d012cf382f58fb72480824c3ecd
Leif Johansson <leifj@sunet.se>
2016-02-14 14:41:26 +0100

    privat github repo with deploy keys

---
+++ manifests/private_github_repo.pp
@@ -0,0 +1,42 @@
+define sunet::private_github_repo(
+      username   = 'github',
+      group      = 'github',
+      url        = undef,
+      id         = 'github'
+      manage_key = true,
+) {
+   ensure_resource('group',"${group}")
+   sunet::system_user { "${username}": 
+      username  => "${username}",   
+      group     => "${group}"
+   } ->
+   file { "/home/${username}/.ssh":
+      ensure    => directory,
+      mode      => '0700',
+      owner     => "${username}",
+      group     => "${group}"
+   } -> 
+   file { "/home/${username}/.ssh/config":
+      ensure    => 'file',
+      content   => "Host github.com\n    IdentityFile ~/.ssh/${id}\n",
+      mode      => '0644',
+      owner     => "${username}",
+      group     => "${group}"
+   } ->
+   exec { 'update-github-ssh-keys':
+      command   => 'ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts'
+   } -> 
+   vcsrepo { "${name}":
+      ensure    => present,
+      provider  => git,
+      source    => "${url}",
+      user      => "${username}"
+   }
+   if ($manage_key) {
+      exec { "${title}-ssh-keygen":
+         command => "ssh-keygen -N '' -C '${username}@${::fqdn}' -f '/home/${username}/.ssh/${id}' -t ecdsa",
+         user    => "${username}",
+         onlyif  => "test ! -f /home/${username}/.ssh/${id}"
+      }
+   }
+}
