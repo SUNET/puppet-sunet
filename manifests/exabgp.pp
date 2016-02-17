da7e982700a7bd05ed05158825ef4ffcce1c5de8
Leif Johansson <leifj@sunet.se>
2016-02-17 10:57:02 +0100

    simplify

--- manifests/exabgp.pp
+++ manifests/exabgp.pp
@@ -2,21 +2,18 @@ include stdlib
 
 define sunet::exabgp(
    $extra_arguments = [], 
-   $config          = undef,
+   $config          = "/etc/bgp/exabgp.conf",
    $version         = 'latest',
-   $port            = 179
+   $port            = 179,
+   $volume          = "/etc/bgp"
 ) {
    $safe_title = regsubst($title, '[^0-9A-Za-z.\-]', '-', 'G');
-   $cfg = $config ? {
-      undef    => $name,
-      default  => $config
-   }
    sunet::docker_run {"${safe_title}_exabgp":
       image    => 'docker.sunet.se/exabgp',
       imagetag => $version,
-      volumes  => ["${cfg}:${cfg}:ro"],
+      volumes  => ["${volume}:${volume}:ro"],
       ports    => ["${port}:${port}"],
-      command  => join(flatten([$cfg,$extra_arguments])," "),
+      command  => join(flatten([$config,$extra_arguments])," "),
    } ->
    sunet::snippets::no_icmp_redirects {"no_icmp_redirects_${safe_title}": }
 }
