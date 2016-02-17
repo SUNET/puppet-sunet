2bf388f47671ecd32f8bb46323466b51ede139ef
Leif Johansson <leifj@sunet.se>
2016-02-17 10:00:53 +0100

    exabgp full of syntax errors

---
+++ manifests/exabgp.pp
@@ -0,0 +1,27 @@
+include stdlib
+
+define sunet::exabgp(
+   $extra_arguments = [], 
+   $config          = undef,
+   $version         = 'latest',
+   $port            = 179
+) {
+   $safe_title = regsubst($title, '[^0-9A-Za-z.\-]', '-', 'G');
+   $cfg = $config ? {
+      undef    => $name,
+      default  => $config
+   }
+   sunet::docker_run {
+      image    => 'docker.sunet.se/exabgp',
+      imagetag => $version,
+      volumes  => ["${cfg}:${cfg}:ro"],
+      ports    => ["${port}:${port}"],
+      command  => flatten(["${cfg}",${extra_arguments}]),
+      require  => Exec["if_config_exists_${safe_title}"]
+   } ->
+   sunet::snippets::no_icmp_redirects {"no_icmp_redirects_${safe_title}": } ->
+   exec {"if_config_exists_${safe_title}":
+      command => '/bin/true',
+      onlyif  => "/usr/bin/test -e ${cfg}"
+   }
+}
