52c821683fd330c1569d98133bca803ea3f7e68f
Fredrik Thulin <fredrik@thulin.net>
2016-04-06 11:07:54 +0200

    cleanup, and try to resolve dependency loop issue in eduid

--- manifests/docker_run.pp
+++ manifests/docker_run.pp
@@ -10,18 +10,18 @@ define sunet::docker_run(
   $extra_parameters    = [],
   $command             = undef,
   $hostname            = undef,
-  $start_on            = [],  # used with 'depends'
-  $stop_on             = [],  # currently not used
+  $start_on            = [],    # used with 'depends' for compatibility
+  $stop_on             = [],    # currently not used, kept for compatibility
   $dns                 = [],
-  $before_start        = undef,
-  $before_stop         = undef,
-  $use_unbound         = false,
+  $before_start        = undef, # command executed before starting
+  $before_stop         = undef, # command executed before stopping
+  $use_unbound         = false, # deprecated, kept for compatibility
 ) {
-
   if $use_unbound {
     warning("docker-unbound is deprecated, container name resolution should continue to work using docker network with DNS")
   }
 
+  # If a non-default network is used, make sure it is created before this container starts
   $req = $net ? {
     'host'   => [],
     'bridge' => [],
@@ -30,9 +30,9 @@ define sunet::docker_run(
 
   $image_tag = "${image}:${imagetag}"
   docker::image { "${name}_${image_tag}" :  # make it possible to use the same docker image more than once on a node
-    image              => $image_tag,
-    require            => [Package['docker-engine'],
-                           ],
+    image   => $image_tag,
+    require => [Package['docker-engine'],
+                ],
   } ->
 
   docker::run { $name :
@@ -55,6 +55,6 @@ define sunet::docker_run(
     require            => flatten([$req]),
     before_start       => $before_start,
     before_stop        => $before_stop,
+    docker_service     => true,  # the service 'docker' is maintainer by puppet, so depend on it
   }
-
 }
