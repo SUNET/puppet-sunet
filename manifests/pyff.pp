23245ed2a7ba8804fa3091eede97efab425b07ae
Leif Johansson <leifj@sunet.se>
2016-05-24 15:03:41 +0200

    reverse depends?

--- manifests/pyff.pp
+++ manifests/pyff.pp
@@ -15,19 +15,18 @@ define sunet::pyff(
       volumes  => ["/etc/ssl:/etc/ssl"],
       env      => ["BACKEND_PORT=tcp://varnish-${sanitised_title}.docker:80"],
       ports    => ["${ip_addr}443:443"],
-      depends  => ["varnish-${sanitised_title}"]
    }
    sunet::docker_run {"varnish-${sanitised_title}":
       image    => 'docker.sunet.se/varnish',
       env      => ["BACKEND_PORT=tcp://pyff-${sanitised_title}.docker:8080"],
       ports    => ["${ip_addr}80:80"],
-      depends  => ["pyff-${sanitised_title}"]
+      depends  => ["pound-${sanitised_title}"]
    }
    sunet::docker_run {"pyff-${sanitised_title}":
       image       => $image,
       imagetag    => $version,
       volumes     => ["$dir:$dir"],
       env         => ["DATADIR=$dir",'LOGLEVEL=INFO'],
-      use_unbound => true
+      depends     => ["varnish-${sanitised_title}"]
    }
 }
