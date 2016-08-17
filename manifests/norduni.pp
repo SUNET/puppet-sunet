60e08ad3bfb235ce11611fe70028b4e620c910f5
Johan Lundberg <lundberg@nordu.net>
2016-08-17 14:39:20 +0200

    Added manifiest and templates for norduni

---
+++ manifests/norduni.pp
@@ -0,0 +1,127 @@
+class sunet::norduni {
+
+    $postgres_master_password = hiera('postgres_master_password', 'NOT_SET_IN_HIERA')
+    $postgres_ni_password = hiera('postgres_ni_password', 'NOT_SET_IN_HIERA')
+    $noclook_secret_key = hiera('noclook_secret_key', 'NOT_SET_IN_HIERA')
+
+    # Create local users for mapping to docker containers
+    sunet::system_user {'ni': username => 'ni', group => 'ni' }
+    sunet::system_user {'neo4j': username => 'neo4j', group => 'neo4j' }
+    sunet::system_user {'postgres': username => 'postgres', group => 'postgres' }
+    sunet::system_user {'www-data': username => 'www-data', group => 'www-data' }
+
+    # Directories/files that will be volume mounted in containers
+    file { '/var/opt/norduni/noclook/etc':
+      ensure  => directory,
+      owner   => 'ni',
+      group   => 'ni',
+      recurse => true,
+    } ->
+    file { '/var/opt/norduni/noclook/etc/dotenv':
+        ensure  => file,
+        owner   => 'ni',
+        group   => 'ni',
+        mode    => '0440',
+        content => template('sunet/norduni/norduni_dotenv.erb'),
+    }
+
+    file { '/var/opt/norduni/noclook/staticfiles':
+      ensure  => directory,
+      owner   => 'ni',
+      group   => 'www-data',
+      recurse => true,
+    }
+
+    file { '/var/opt/norduni/nginx/etc':
+      ensure  => directory,
+      owner   => 'www-data',
+      group   => 'www-data',
+      recurse => true,
+    } ->
+    file { '/var/opt/norduni/nginx/etc/default.conf':
+        ensure  => file,
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0440',
+        content => template('sunet/norduni/nginx_conf.erb'),
+    }
+
+    file { '/var/opt/norduni/postgres/init':
+      ensure  => directory,
+      owner   => 'postgres',
+      group   => 'postgres',
+      recurse => true,
+    } ->
+    file { '/var/opt/norduni/postgres/init/init-noclook-db.sh':
+        ensure  => file,
+        owner   => 'postgres',
+        group   => 'postgres',
+        mode    => '0770',
+        content => template('sunet/norduni/init-noclook-db.sh.erb'),
+    }
+
+    file { '/var/opt/norduni/data/postgresql':
+      ensure  => directory,
+      owner   => 'postgres',
+      group   => 'postgres',
+      recurse => true,
+    }
+
+    file { '/var/opt/norduni/data/neo4j':
+      ensure  => directory,
+      owner   => 'neo4j',
+      group   => 'neo4j',
+      recurse => true,
+    }
+
+    file { '/var/log/norduni':
+        ensure  => directory,
+        owner   => 'ni',
+        group   => 'ni',
+        mode    => '0755',
+    }
+
+    file { '/var/log/nginx':
+        ensure  => directory,
+        owner   => 'www-data',
+        group   => 'www-data',
+        mode    => '0755',
+    }
+
+    sunet::docker_run { 'norduni_postgres':
+        image    => 'postgres',
+        imagetag => 'latest',
+        volumes  => ['/var/opt/norduni/data/postgresql:/var/opt/postgresql', '/var/opt/norduni/postgres/init/init-noclook-db.sh:/docker-entrypoint-initdb.d/init-noclook-db.sh'],
+        env      => ["POSTGRES_USER=postgres",
+                     "POSTGRES_PASSWORD=${postgres_master_password}"]
+    }
+
+    sunet::docker_run { 'norduni_neo4j':
+        image    => 'neo4j',
+        imagetag => 'latest',
+        volumes  => ['/var/opt/norduni/data/neo4j:/data'],
+    }
+
+    sunet::docker_run { 'norduni_noclook':
+        image    => 'docker.sunet.se/norduni/noclook',
+        imagetag => 'latest',
+        volumes  => ['/var/opt/norduni/noclook/etc/dotenv:/var/opt/norduni/norduni/src/niweb/.env',
+                     '/var/opt/norduni/noclook/staticfiles:/var/opt/norduni/staticfiles',
+                     '/var/log/norduni:/var/log/norduni',
+                     '/var/opt/norduni/nistore:/var/opt/nistore'],
+        extra_parameters => ['--restart=on-failure:10'],
+        depends  => ['norduni-postgres', 'norduni-neo4j']
+    }
+
+    sunet::docker_run { 'norduni_nginx':
+        image            => 'nginx',
+        imagetag         => 'latest',
+        ports            => ['80:80', '443:443'],
+        volumes          => ['/var/log/nginx:/var/log/nginx',
+                             '/var/opt/norduni/nginx/etc/:/etc/nginx/conf.d',
+                             '/var/opt/norduni/noclook/staticfiles:/usr/share/nginx/html/static'],
+        extra_parameters => ['--restart=on-failure:10'],
+        depends          => ['norduni-noclook']
+    }
+
+}
