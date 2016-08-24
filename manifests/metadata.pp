bf28c7e1b8b6c00157910447a694291e3f1be5b0
Leif Johansson <leifj@sunet.se>
2016-08-24 15:16:03 +0200

    never check cert

--- manifests/metadata.pp
+++ manifests/metadata.pp
@@ -4,14 +4,10 @@ define sunet::metadata($url=undef,
       undef   => $name,
       default => $local
    }
-   $n_c_s = $check_certificate ? {
-      true    => '',
-      false   => '--no-check-certificate'
-   }
    $safe_name = regsubst($name, '[^0-9A-Za-z.\-]', '-', 'G')
    $fetch = "fetch_${safe_name}"
    cron {$fetch:
-      command => "/usr/bin/wget ${n_c_s} -q ${url} -N -O ${local}.tmp && chmod 0644 ${local}.tmp && test -s ${local}.tmp && mv ${local}.tmp ${local}",
+      command => "/usr/bin/wget --no-check-certificate -q ${url} -N -O ${local}.tmp && chmod 0644 ${local}.tmp && test -s ${local}.tmp && mv ${local}.tmp ${local}",
       user    => 'root',
       minute  => '*/5'
    }
