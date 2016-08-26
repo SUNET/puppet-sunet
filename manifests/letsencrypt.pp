ec8d7add08d43cbda6a6732b73bb97c6a32d5918
Leif Johansson <leifj@sunet.se>
2016-08-26 15:23:07 +0200

    letsencrypt.sh take1

---
+++ manifests/letsencrypt.pp
@@ -0,0 +1,41 @@
+class sunet::letsencrypt($domains={},
+                         $staging=false,
+                         $src_url='https://raw.githubusercontent.com/lukas2511/letsencrypt.sh/master/letsencrypt.sh') 
+{
+  validate_hash($domains)
+  $ca = $staging ? {
+     false => 'https://acme-v01.api.letsencrypt.org/directory',
+     true  => 'https://acme-staging.api.letsencrypt.org/directory'
+  }
+  ensure_resource('package','openssl',{ensure=>'latest'})
+  sunet::remote_file { "/usr/sbin/letsencrypt.sh":
+     remote_resource => $src_url,
+     mode            => '0755'
+  }
+  file { '/etc/letsencrypt.sh':
+     ensure  => 'directory',
+     mode    => '0600'
+  }
+  file { '/var/www/letsencrypt':
+     ensure  => 'directory'
+  }
+  file { '/etc/letsencrypt.sh/config':
+     ensure  => 'file',
+     content => template('sunet/letsencrypt/config.erb')
+  }
+  file { '/etc/letsencrypt.sh/domains.txt':
+     ensure  => 'file',
+     content => template('sunet/letsencrypt/domains.erb')
+  }
+  package {'lighttpd':
+     ensure  => latest
+  } -> 
+  service {'lighttpd':
+     ensure  => running
+  }
+  file {'/etc/lighttpd/conf-enabled/acme.conf':
+     ensure  => 'file',
+     content => template('sunet/letsencrypt/lighttpd.conf'),
+     notify  => Service['lighttpd']
+  }
+}
