ebbfc528393f55195da9a720eabb2a22cb878a4c
Leif Johansson <leifj@sunet.se>
2016-08-26 15:20:05 +0200

    do not update by default

--- manifests/remote_file.pp
+++ manifests/remote_file.pp
@@ -1,6 +1,6 @@
 define sunet::remote_file($remote_location=undef, $mode='0644'){
   exec{"retrieve_${title}":
-    command => "/usr/bin/wget -q ${remote_location} -N -O ${title}",
+    command => "/usr/bin/wget -q ${remote_location} -O ${title}",
     creates => $title,
   }
 
