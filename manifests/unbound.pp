93cd464211e9110ab170705616f032c03baa4a4a
Fredrik Thulin <fredrik@thulin.net>
2016-08-29 15:04:25 +0200

    fix?

--- manifests/unbound.pp
+++ manifests/unbound.pp
@@ -8,7 +8,8 @@ class sunet::unbound(
   if $use_apparmor {
     include apparmor
 
-    ensure_resource('file', '/etc/apparmor-cosmos' {ensure => 'directory'}) ->
+    ensure_resource('file', '/etc/apparmor-cosmos' {ensure => 'directory'})
+
     file {
       '/etc/apparmor-cosmos/usr.sbin.unbound':
         content => template('sunet/unbound/etc_apparmor_cosmos_usr.sbin.unbound.erb'),
