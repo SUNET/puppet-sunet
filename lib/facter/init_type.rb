b92aa941b603bcef71f8bc1e453f19e2310df252
Leif Johansson <leifj@sunet.se>
2016-09-15 22:40:33 +0200

    simpler way to do facts

--- lib/facter/init_type.rb
+++
@@ -1,3 +0,0 @@
-Facter.add(:init_type) do
-  setcode '(test x`pidof init | awk \'{print $NF}\'` = x1 && echo init) || (test x`pidof systemd | awk \'{print $NF}\'` = x1 && echo systemd) || echo unknown'
-end
