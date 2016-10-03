fca0af295161e7775dda94644793e9bbcb121295
Leif Johansson <leifj@sunet.se>
2016-10-03 15:01:07 +0200

    restart haveged daily

--- manifests/simple_entropy.pp
+++ manifests/simple_entropy.pp
@@ -1,5 +1,11 @@
 class sunet::simple_entropy {
    package {'haveged': ensure => latest } ->
-   service {'haveged': ensure => running }
+   service {'haveged': ensure => running } ->
+   cron { 'daily-restart-haveged': 
+      command => 'service haveged restart',
+      user    => 'root',
+      hour    => 2,
+      minute  => 10
+   }
    sunet::pollinate { 'https://random.nordu.net/': }
 }
