1324e19a458778e8ac2763de65d4637455ae3aa8
Leif Johansson <leifj@sunet.se>
2016-10-06 20:48:16 +0200

    generate cloud-init config

--- manifests/cloud_init.pp
+++ manifests/cloud_init.pp
@@ -7,10 +7,10 @@ class sunet::cloud_init {
 
 define sunet::cloud_init::config ($prio = "99", $config = {}) {
    ensure_resource ('class','sunet::cloud_init', { })
-   file { "/etc/cloud/cloud.cfg.d/$prio_$title.cfg":
+   file { "/etc/cloud/cloud.cfg.d/${prio}_${title}.cfg":
       owner   => root,
       group   => root,
-      content => inline_template("---\n<%= @config.to_yaml %>"),
+      content => inline_template("<%= @config.to_yaml %>"),
       notify  => Exec['update-cloud-init']
    }
 }
