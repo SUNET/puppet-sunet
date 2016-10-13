5fbf22ed9c871508af0468344d3536dd52b0bd8e
Leif Johansson <leifj@sunet.se>
2016-10-13 21:38:32 +0200

    hope the first address works

--- manifests/ssh_keyscan.pp
+++ manifests/ssh_keyscan.pp
@@ -13,7 +13,7 @@ class sunet::ssh_keyscan {
    }
    concat::fragment {"/etc/ssh/sunet_keyscan_hosts.txt_header":
       target  => "/etc/ssh/sunet_keyscan_hosts.txt",
-      content => "# do not edit by hand - maintained by sunet::ssh_keyscan",
+      content => "# do not edit by hand - maintained by sunet::ssh_keyscan\n",
       order   => '10',
       notify  => Exec['ssh-keyscan']
    }
@@ -23,12 +23,13 @@ define sunet::ssh_keyscan::host ($aliases = [], $address = undef) {
    ensure_resource('class','sunet::ssh_keyscan',{})
    $the_address = $address ? {
       undef   => dnsLookup($title),
-      default => $address
+      default => [$address]
    }
+   validate_array($the_address)
    $the_aliases = concat(any2array($aliases),[$title],[$the_address])
    concat::fragment {"${title}_sunet_keyscan":
       target  => "/etc/ssh/sunet_keyscan_hosts.txt",
-      content => inline_template("<%= the_address %> <%= the_aliases.join(',') %>"),
+      content => inline_template("<%= the_address[0] %> <%= the_aliases.join(',') %>\n"),
       order   => '20',
       notify  => Exec['ssh-keyscan']
    }
