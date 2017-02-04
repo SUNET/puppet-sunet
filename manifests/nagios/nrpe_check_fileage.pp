fb25606f253d4093946793e2f5fa186375d9f038
Leif Johansson <leifj@sunet.se>
2017-02-04 17:42:46 +0100

    nit

--- manifests/nagios/nrpe_check_fileage.pp
+++ manifests/nagios/nrpe_check_fileage.pp
@@ -10,6 +10,6 @@ define sunet::nagios::nrpe_check_fileage (
    }
 
    sunet::nagios::nrpe_command {"check_fileage_${title}":
-     command_line => "/usr/lib/nagios/plugins/check_fileage -f $_filename -w $warning_age -c $critical_age"
+     command_line => "/usr/lib/nagios/plugins/check_file_age -f $_filename -w $warning_age -c $critical_age"
    }
 }
