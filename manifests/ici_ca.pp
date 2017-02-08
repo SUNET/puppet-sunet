08806bfbb2fa8cf8fcb72fda68c4a992a2a2ca60
Leif Johansson <leifj@sunet.se>
2017-02-08 23:41:58 +0100

    nightly refresh of existing infra cert

--- manifests/ici_ca.pp
+++ manifests/ici_ca.pp
@@ -48,5 +48,10 @@ define sunet::ici_ca::rp()
    exec {"dl_infra_host_cert":
       command => "/usr/bin/dl_ici_cert $ca $host",
       onlyif  => "/usr/bin/test -f /etc/ssl/private/${host}_${ca}.key -a ! -f /etc/ssl/certs/${host}_${ca}.crt"
+   } ->
+   cron {"refresh_infra_host_cert":
+      command => "env RANDOM_SLEEP=30 /usr/bin/dl_ici_cert $ca $host",
+      hour    => "1",
+      minute  => "1"
    }
 }
