d06fe6c645b75bcd377533a892d82df90eb25d5a
Leif Johansson <leifj@sunet.se>
2017-02-14 23:22:56 +0100

    skip kernel threads in lax mode

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -59,7 +59,7 @@ class sunet::nagios($nrpe_service = 'nagios-nrpe-server') {
       command_line => '/usr/lib/nagios/plugins/check_procs -w 150 -c 200'
    }
    sunet::nagios::nrpe_command {'check_total_procs_lax':
-      command_line => '/usr/lib/nagios/plugins/check_procs -w 250 -c 400'
+      command_line => '/usr/lib/nagios/plugins/check_procs -k -w 150 -c 200'
    }
    sunet::nagios::nrpe_command {'check_uptime':
       command_line => '/usr/lib/nagios/plugins/check_uptime.pl -f'
