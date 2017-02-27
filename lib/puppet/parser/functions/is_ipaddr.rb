9ade14a704c906874b852d05d81d1234cabfa25e
Fredrik Thulin <fredrik@thulin.net>
2017-02-27 19:49:33 +0100

    bugfixes

--- lib/puppet/parser/functions/is_ipaddr.rb
+++ lib/puppet/parser/functions/is_ipaddr.rb
@@ -4,6 +4,9 @@
 # Puppet used to have validate_ and is_ functions for IPv4 and IPv6, but they
 # deprecated them without providing functioning replacements (for Puppet 3.7).
 #
+
+require 'ipaddr'
+
 module Puppet::Parser::Functions
   newfunction(:is_ipaddr, :type => :rvalue) do |args|
     if args.size < 1 or args.size > 2
@@ -17,33 +20,30 @@ module Puppet::Parser::Functions
       addr = [addr]
     end
 
-    if not addr.instance_of? Array
+    if ! addr.instance_of? Array
       err("First argument to is_ipaddr is not a string or array: #{addr}")
       return false
     end
 
-    if args.size == 2 and ipver.instance_of? != Integer
-      err("Seconf argument to is_ipaddr is not an integer: #{ipver}")
+    if args.size == 2 and ! ipver.is_a? Integer
+      err("Second argument to is_ipaddr is not an integer: #{ipver}")
       return false
     end
 
     addr.each do |this|
-      if ipver == 4 and not this.ipv4?
-        debug("#{addr} is not an IPv4 address")
-        return False
+      this_addr = IPAddr.new(this) rescue false
+      if ipver == 4 and ! this_addr.ipv4?
+        debug("#{this} is not an IPv4 address")
+        return false
       end
 
-      if ipver == 6 and not this.ipv6?
-        debug("#{addr} is not an IPv6 address")
-        return False
-      end
-
-      if not this.ip?
-        debug("#{addr} is not an IPv4/IPv6 address")
-        return False
+      if ipver == 6 and ! this_addr.ipv6?
+        debug("#{this} is not an IPv6 address")
+        return false
       end
     end
 
-    return True
+    debug("All inputs #{addr} found to be IP (#{ipver}) address(es).")
+    return true
   end
 end
