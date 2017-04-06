12319ab89e59ae8b4b173d630be22f6e7db026a5
Fredrik Thulin <fredrik@thulin.net>
2017-04-06 16:40:46 +0200

    router_id _is_ optional

--- manifests/exabgp/config.pp
+++ manifests/exabgp/config.pp
@@ -1,11 +1,11 @@
 define sunet::exabgp::config(
-  String $peer_address,
-  String $peer_as,
-  String $local_address,
-  String $local_as,
-  String $router_id     = undef,
-  String $monitor       = '/etc/bgp/monitor',
-  String $config        = '/etc/bgp/exabgp.conf',
+  String           $peer_address,
+  String           $peer_as,
+  String           $local_address,
+  String           $local_as,
+  Optional[String] $router_id     = undef,
+  String           $monitor       = '/etc/bgp/monitor',
+  String           $config        = '/etc/bgp/exabgp.conf',
 ) {
   sunet::misc::ufw_allow { "allow_bgp_peer_${peer_address}":
     from => $peer_address,
