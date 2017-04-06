55a81ccd580fc21a7b04d0f1b49cefa75b75085e
Fredrik Thulin <fredrik@thulin.net>
2017-04-06 16:28:07 +0200

    ufw allow bgp peers

--- manifests/exabgp/config.pp
+++ manifests/exabgp/config.pp
@@ -7,6 +7,11 @@ define sunet::exabgp::config(
   String $monitor       = '/etc/bgp/monitor',
   String $config        = '/etc/bgp/exabgp.conf',
 ) {
+  sunet::misc::ufw_allow { "allow_bgp_peer_${peer_address}":
+    from => $peer_address,
+    port => '179',
+  }
+
   file { "${config}":
     ensure  => file,
     content => template("sunet/exabgp/exabgp.conf.erb")
