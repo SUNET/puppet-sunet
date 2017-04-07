f259ec158e7274fe4f0ff33bea7faea869830544
Johan Lundberg <lundberg@nordu.net>
2017-04-07 12:43:56 +0200

    Changed path to packages

--- manifests/pypi.pp
+++ manifests/pypi.pp
@@ -1,11 +1,7 @@
 class sunet::pypi (
     $user   = 'pypi',
     $home   = '/opt/pypi',
-    $chroot = '/opt/pypi/chroot'
 ) {
-    package { ['rush']:
-      ensure     => absent,
-    } ->
     user {'pypi_user':
       name       => $user,
       shell      => '/bin/false',
@@ -37,39 +33,33 @@ class sunet::pypi (
       key     => "AAAAB3NzaC1yc2EAAAADAQABAAACAQC2bEPvMFlJwrdchpSyrKgG3Gv0ZHM0DKxHxMctfBNMFkXYzUiRc1kIsuHwca+w4iHPrSsLSbeLWL5rlOyON1xtWvXr2aqCIOr3N64zuaKvlNx9Sty6UFXuHUOo98F+1Zk3tFFXN/hCod+BGp2SqAR2ajWTVn7E+9MrmrSRXkFKUDonlX7LfVosSxUiqJ6Ths00syq01X28dxtrj6yk6Py9bgr8Vl/8ZUzNzc7Tr1y1I/96EvlSftzc5LIzuhUSt9Ge7PKudMi8qhlgyflHleEAy01AE65w+GGxt7kDyjmvv+PnQMbW4gK6R8hylGQMMdTZx/eAoZJSJTHjWlITY2Wm7oGphTVNY1hE1PNpYhSRLBFcpOcR5iViVC18ygFAncKAIDl6P4HNj03JOHV2wkKtbkscPG11G6w+hrwc8gjCuGXWwPYvtgj6t5RO0LiZnYqgVyrbjwJ2CDP2l9xAiPVbtHFby5CvmZmXWUW8XCJWOwJjKjaXy8IWt+g4TmIKeTnU+zU7Iu1EfI0lqlIzrVpnuf0MiE/2MPKYIp2YDbvy2KFq7ebS6Kd5TIupevmbsPQsZMBz7RNQV1btSGoi5bgowa03z5SLtho15UEloBS77b0SeXs/bx8+ghjx5o+DEuqWaTAcj1uPo1clWuVRhGoE+PYhzy+Ony2PLdcfm9GbpQ==",
       target  => "${home}/.ssh/authorized_keys",
     }
-    file { "${chroot}":
-      ensure  => directory,
-      owner   => $user,
-      group   => $user,
-      recurse => true,
-    } ->
-    file { "${chroot}/packages":
+    file { "${home}/packages":
       ensure  => directory,
       owner   => $user,
       group   => $user,
       recurse => true,
     } ->
     sunet::system_user {'www-data': username => 'www-data', group => 'www-data' } ->
-    file { '/opt/pypi/nginx':
+    file { "${home}/nginx":
       ensure  => directory,
       owner   => 'www-data',
       group   => 'www-data',
       recurse => true,
     } ->
-    file { '/opt/pypi/nginx/etc':
+    file { "${home}/nginx/etc":
       ensure  => directory,
       owner   => 'www-data',
       group   => 'www-data',
       recurse => true,
     } ->
-    file { '/opt/pypi/nginx/etc/default.conf':
+    file { "${home}/nginx/etc/default.conf":
         ensure  => file,
         owner   => 'www-data',
         group   => 'www-data',
         mode    => '0440',
         content => template('sunet/pypi/nginx_conf.erb'),
     } ->
-    file { '/opt/pypi/pypiserver/etc/start.sh':
+    file { "${home}/pypiserver/etc/start.sh":
         ensure  => file,
         owner   => $user,
         group   => $user,
@@ -85,7 +75,7 @@ class sunet::pypi (
     sunet::docker_compose {'pypi_docker_compose':
         service_name => 'pypi',
         description  => 'pypi service',
-        compose_dir  => '/opt/pypi/compose',
+        compose_dir  => "${home}/compose",
         content => $content,
     }
 }
