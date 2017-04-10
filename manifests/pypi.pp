4de3335ae22d6b6fa953ed15842613ea68505234
Johan Lundberg <lundberg@nordu.net>
2017-04-10 15:58:13 +0200

    Optional sftp config added because pypi needs it

--- manifests/pypi.pp
+++ manifests/pypi.pp
@@ -52,6 +52,13 @@ class sunet::pypi (
         mode    => '0644',
         content => template('sunet/pypi/sshd_config.erb'),
     } ->
+    class { 'sunet::security::configure_sshd':
+      configure_sftp = false,
+    }
+    sunet::misc::ufw_allow { "allow-ssh-from-all":
+      from => 'any',
+      port => '22',
+    }
 
     file { "${home}/packages":
       ensure  => directory,
