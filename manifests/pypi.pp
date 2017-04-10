47bfd137b3a1472722e10a20c4f3a8d648e0873e
Johan Lundberg <lundberg@nordu.net>
2017-04-10 15:40:16 +0200

    Set up sshd to allow chrooted sftp for user pypi

--- manifests/pypi.pp
+++ manifests/pypi.pp
@@ -8,6 +8,16 @@ class sunet::pypi (
       managehome => true,
       home       => '/opt/pypi',
     } ->
+
+    # OpenSSH require that the home dir is owned by root in
+    # order to allow chrooting the connecting user to it.
+    file { "${home}":
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'root',
+      recurse => false,
+    } ->
+
     file { "${home}/.ssh":
       ensure  => directory,
       owner   => $user,
@@ -32,7 +42,17 @@ class sunet::pypi (
       type    => "ssh-rsa",
       key     => "AAAAB3NzaC1yc2EAAAADAQABAAACAQC2bEPvMFlJwrdchpSyrKgG3Gv0ZHM0DKxHxMctfBNMFkXYzUiRc1kIsuHwca+w4iHPrSsLSbeLWL5rlOyON1xtWvXr2aqCIOr3N64zuaKvlNx9Sty6UFXuHUOo98F+1Zk3tFFXN/hCod+BGp2SqAR2ajWTVn7E+9MrmrSRXkFKUDonlX7LfVosSxUiqJ6Ths00syq01X28dxtrj6yk6Py9bgr8Vl/8ZUzNzc7Tr1y1I/96EvlSftzc5LIzuhUSt9Ge7PKudMi8qhlgyflHleEAy01AE65w+GGxt7kDyjmvv+PnQMbW4gK6R8hylGQMMdTZx/eAoZJSJTHjWlITY2Wm7oGphTVNY1hE1PNpYhSRLBFcpOcR5iViVC18ygFAncKAIDl6P4HNj03JOHV2wkKtbkscPG11G6w+hrwc8gjCuGXWwPYvtgj6t5RO0LiZnYqgVyrbjwJ2CDP2l9xAiPVbtHFby5CvmZmXWUW8XCJWOwJjKjaXy8IWt+g4TmIKeTnU+zU7Iu1EfI0lqlIzrVpnuf0MiE/2MPKYIp2YDbvy2KFq7ebS6Kd5TIupevmbsPQsZMBz7RNQV1btSGoi5bgowa03z5SLtho15UEloBS77b0SeXs/bx8+ghjx5o+DEuqWaTAcj1uPo1clWuVRhGoE+PYhzy+Ony2PLdcfm9GbpQ==",
       target  => "${home}/.ssh/authorized_keys",
-    }
+    } ->
+    # These changes adds chrooting for the pypi user as well as disables
+    # some stuff that it doesnt need, such as forwarding and tunnel.
+    file { "/etc/ssh/sshd_config":
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('sunet/pypi/sshd_config.erb'),
+    } ->
+
     file { "${home}/packages":
       ensure  => directory,
       owner   => $user,
