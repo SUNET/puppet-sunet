0d8c355e67a30b2dc7991e7bdf3e2745b2e92c11
Fredrik Thulin <fredrik@thulin.net>
2017-05-04 10:42:26 +0200

    fix

--- manifests/bird/peer.pp
+++ manifests/bird/peer.pp
@@ -10,12 +10,14 @@ define sunet::bird::peer(
       content  => template("sunet/bird/bgp.erb"),
       notify   => Service[$bird]
     }
-  } elsif is_ipaddr($this, 6) {
+  } elsif is_ipaddr($remote_ip, 6) {
     concat::fragment { "${name}_bgp_peer6":
       target   => '/etc/bird/bird6.conf',
       order    => '20',
       content  => template("sunet/bird/bgp.erb"),
       notify   => Service[$bird6]
     }
+  } else {
+    fail("Peer IP address $remote_ip is neither IPv4 nor IPv6")
   }
 }
