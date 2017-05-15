5e24f88931e86cc163ca5b75e4c39082dae90cb3
Fredrik Thulin <fredrik@thulin.net>
2017-05-15 09:19:16 +0200

    When looking for the default route interface, use the first one rather than the last.
    At least on Ubuntu 16.10, the route command will output the default route with the
    lowest metric (best) first, so we would want to use that interface as the default:
    
    $ route
    Kernel IP routing table
    Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
    default         10.0.1.1        0.0.0.0         UG    100    0        0 eth0
    default         10.0.2.1        0.0.0.0         UG    600    0        0 wifi0
    ...
    $

--- facts.d/ipaddress_default.sh
+++ facts.d/ipaddress_default.sh
@@ -3,7 +3,7 @@
 # FIXME: Might have to handle hosts with more than one IP-address on the "outside"
 # 	 interface, this script *should* pick one and still work, but ...
 
-INT=`route |grep default|awk 'END {print $NF}'`
+INT=$(route | grep default | head -1 | awk '{print $NF}')
 
 # This command works on Ubuntu 16.04 and earlier (?)
 ipaddr4=$(ifconfig $INT | grep "inet addr" | head -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
