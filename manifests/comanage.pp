7bb89bfa7329f60b2bc10cb0daabbe5482fbddeb
Einar Lönn <el@sunet.se>
2017-09-18 13:26:24 +0200

    Add postgres user to COmanage or host mount will break stuff

--- manifests/comanage.pp
+++ manifests/comanage.pp
@@ -3,6 +3,8 @@ class sunet::comanage (
     $home   = '/root/comanage',
 ) {
 
+    sunet::system_user {'postgres': username => 'postgres', group => 'postgres' }
+
     $content = template('sunet/comanage/docker-compose_comanage.yml.erb')
     sunet::docker_compose {'comanage_docker_compose':
         service_name => 'comanage',
