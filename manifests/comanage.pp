d53a7a1031a2dee1b39607b9a4c34af5f48c1521
Einar Lönn <el@sunet.se>
2017-10-24 10:28:27 +0200

    Trying workaround (nasty) for https://github.com/Internet2/comanage-registry-docker/issues/11

--- manifests/comanage.pp
+++ manifests/comanage.pp
@@ -60,12 +60,14 @@ class sunet::comanage (
     $comanage_security_salt = hiera('comanage_security_salt', 'NOT_SET_IN_HIERA')
     $comanage_security_seed = hiera('comanage_security_seed', 'NOT_SET_IN_HIERA')
 
+    # FIXME: Horrible but I have to do this to be able to mount passwd/group due to COmanage issue:
+    # https://github.com/Internet2/comanage-registry-docker/issues/11
     file { '/etc/shibboleth/shibboleth2.xml':
         ensure  => file,
         owner   => '_shibd',
         group   => '_shibd',
         path    => '/etc/shibboleth/shibboleth2.xml',
-        mode    => '0660',
+        mode    => '0666',
         content => template('sunet/comanage/shibboleth2.erb'),
     }
 
