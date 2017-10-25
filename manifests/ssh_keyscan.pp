b895979b8279efce565a37bad51fe943364ddf52
Fredrik Thulin <fredrik@thulin.net>
2017-10-25 13:42:03 +0200

    works now - de-lint

--- manifests/ssh_keyscan.pp
+++ manifests/ssh_keyscan.pp
@@ -5,39 +5,39 @@ class sunet::ssh_keyscan(
   String $hostsfile,
   String $keyfile = '/etc/ssh/ssh_known_hosts',
 ) {
-   exec {'sunet_ssh-keyscan':
-      command     => "ssh-keyscan -f ${hostsfile} > ${keyfile}.scan && mv ${keyfile}.scan ${keyfile}",
-      refreshonly => true,
-      subscribe   => File[$hostsfile],
-   }
+  exec {'sunet_ssh-keyscan':
+    command     => "ssh-keyscan -f ${hostsfile} > ${keyfile}.scan && mv ${keyfile}.scan ${keyfile}",
+    refreshonly => true,
+    subscribe   => File[$hostsfile],
+  }
 
-   concat {$hostsfile:
-      owner  => root,
-      group  => root,
-      mode   => '0640',
-   }
-   concat::fragment {"${hostsfile}_header":
-      target  => $hostsfile,
-      content => "# do not edit by hand - maintained by sunet::ssh_keyscan\n",
-      order   => '10',
-   }
+  concat {$hostsfile:
+    owner => root,
+    group => root,
+    mode  => '0640',
+  }
+  concat::fragment {"${hostsfile}_header":
+    target  => $hostsfile,
+    content => "# do not edit by hand - maintained by sunet::ssh_keyscan\n",
+    order   => '10',
+  }
 }
 
 define sunet::ssh_keyscan::host (
   Array $aliases = [],
   Optional[String] $address = undef,
 ) {
-   $hostsfile = '/etc/ssh/sunet_keyscan_hosts.txt'
-   ensure_resource('class','sunet::ssh_keyscan', {hostsfile => $hostsfile})
-   $the_address = $address ? {
-      undef   => dnsLookup($title),
-      default => [$address]
-   }
-   validate_array($the_address)
-   $the_aliases = concat(any2array($aliases),[$title],[$the_address])
-   concat::fragment {"${title}_sunet_keyscan":
-      target  => $hostsfile,
-      content => inline_template("<%= the_address[0] %> <%= the_aliases.join(',') %>\n"),
-      order   => '20',
-   }
+  $hostsfile = '/etc/ssh/sunet_keyscan_hosts.txt'
+  ensure_resource('class','sunet::ssh_keyscan', {hostsfile => $hostsfile})
+  $the_address = $address ? {
+    undef   => dnsLookup($title),
+    default => [$address]
+  }
+  validate_array($the_address)
+  $the_aliases = concat(any2array($aliases),[$title],[$the_address])
+  concat::fragment {"${title}_sunet_keyscan":
+    target  => $hostsfile,
+    content => inline_template("<%= the_address[0] %> <%= the_aliases.join(',') %>\n"),
+    order   => '20',
+  }
 }
