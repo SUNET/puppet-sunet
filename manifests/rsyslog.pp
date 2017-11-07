8032e74e2ec771a3e10ff1ba1548b70a969614e0
Fredrik Thulin <fredrik@thulin.net>
2017-11-07 16:06:45 +0100

    make syslog_servers a parameter and use some ensure_resource

--- manifests/rsyslog.pp
+++ manifests/rsyslog.pp
@@ -1,9 +1,9 @@
-class sunet::rsyslog {
-  $syslog_servers = hiera_array('syslog_servers',[])
-
-  package {'rsyslog':
+class sunet::rsyslog(
+  $syslog_servers = hiera_array('syslog_servers',[]),
+) {
+  ensure_resource('package', 'rsyslog', {
     ensure => 'installed'
-  }
+  })
 
   file { '/etc/rsyslog.d/60-remote.conf':
     ensure  => file,
@@ -12,9 +12,9 @@ class sunet::rsyslog {
     require => Package['rsyslog'],
   }
 
-  service { 'rsyslog':
+  ensure_resource('service', 'rsyslog', {
     ensure    => 'running',
     enable    => true,
     subscribe => File['/etc/rsyslog.d/60-remote.conf'],
-  }
+  })
 }
