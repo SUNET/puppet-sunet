a8c7143fcc6e1090b82d892754eaa9351472f1da
Fredrik Thulin <fredrik@thulin.net>
2017-11-10 13:52:27 +0100

    init

---
+++ manifests/misc/create_key_file.pp
@@ -0,0 +1,27 @@
+# Create a file with secret content retrieved from hiera.
+define sunet::misc::create_key_file(
+  $hiera_key,
+  $path       = undef,
+  $owner      = 'root',
+  $group      = 'root',
+  $mode       = '0440',
+) {
+  $_path = $path ? {
+    undef   => $name,
+    default => $path,
+  }
+
+  $key_content = hiera($hiera_key, 'NOT_SET_IN_HIERA')
+
+  if $key_content == 'NOT_SET_IN_HIERA' {
+    warning ("Key file data key '${hiera_key}' not set in hiera")
+  } else {
+    ensure_resource('file', $_path, {
+      ensure  => file,
+      owner   => $owner,
+      group   => $group,
+      mode    => $mode,
+      content => $key_content,
+      })
+  }
+}
