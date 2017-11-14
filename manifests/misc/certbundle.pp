399e5467d5204c4c882540afc15659d4c5ce4527
Fredrik Thulin <fredrik@thulin.net>
2017-11-14 16:26:24 +0100

    seems quoiting is needed for unless with ensure_resource

--- manifests/misc/certbundle.pp
+++ manifests/misc/certbundle.pp
@@ -29,10 +29,10 @@ define sunet::misc::certbundle(
 
   $script_args = join($script, ' ')
   #notice("Creating ${outfile} with command /usr/local/sbin/cert-bundler --syslog $script_args")
-  ensure_resource('exec', "create_${name}", {
-    command => "/usr/local/sbin/cert-bundler --syslog ${script_args}",
-    unless  => "/usr/local/sbin/cert-bundler --unless ${script_args}",
-    require => $req,
-    returns => [0, 1],
+  ensure_resource('exec', "create_${}", {
+    'command' => "/usr/local/sbin/cert-bundler --syslog ${script_args}",
+    'unless'  => "/usr/local/sbin/cert-bundler --unless ${script_args}",
+    'require' => $req,
+    'returns' => [0, 1],
   })
 }
