b7193a975eee6ebc8fbdbb2404b5326c6fd24f55
John Van de Meulebrouck Brendgard <john@sunet.se>
2017-11-22 17:13:37 +0100

    Added option to specify an apt proxy in cloudimage/user_data.erb

--- manifests/cloudimage.pp
+++ manifests/cloudimage.pp
@@ -20,6 +20,7 @@ define sunet::cloudimage (
   $ssh_keys    = undef,
   $description = '',
   $apt_dir     = '/etc/cosmos/apt',
+  $apt_proxy   = undef,
   $images_dir  = '/var/lib/libvirt/images',
   $pool_name   = 'default',
   $local_size  = '0',
