8c3ad6f72f0630a1ee91ae7ad827109088424a34
Fredrik Thulin <fredrik@thulin.net>
2017-12-04 11:49:21 +0100

    create molly-guard script to shut down exabgp early in reboot process

--- manifests/exabgp.pp
+++ manifests/exabgp.pp
@@ -16,6 +16,37 @@ define sunet::exabgp(
       ports       => ["${port}:${port}"],
       net         => 'host',
       command     => join(flatten([$config,$extra_arguments])," ")
-   } ->
+   }
+
    sunet::snippets::no_icmp_redirects {"no_icmp_redirects_${safe_title}": }
+
+   file {
+     '/etc/molly-guard/run.d/50-shutdown-exabgp':
+       owner   => 'root',
+       group   => 'root',
+       mode    => '0755',
+       require => Package['molly-guard'],
+       content => inline_template(@("END"/$n))
+       #!/bin/sh
+       #
+       # Shut down exabgp and wait for a couple of seconds. Created by Puppet (sunet::exabgp).
+       #
+
+       set -e
+
+       [ -f "$MOLLYGUARD_SETTINGS" ] && . "$MOLLYGUARD_SETTINGS"
+
+       if [ $MOLLYGUARD_DO_NOTHING = 1 ]; then
+           echo "Would have shut down ${safe_title}_exabgp now"
+           exit 0
+       fi
+
+       echo -n "Shutting down ${safe_title}_exabgp..."
+       service ${safe_title}_exabgp stop
+       echo -n 'waiting...'
+       sleep 2
+       echo 'done'
+       |END
+       ;
+   }
 }
