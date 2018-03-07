a33b90ba5e873fed81c04d8e11870acaec2ce054
Fredrik Thulin <fredrik@thulin.net>
2018-03-07 10:36:58 +0100

    initial compose

--- manifests/frontend/load_balancer/website.pp
+++ manifests/frontend/load_balancer/website.pp
@@ -4,18 +4,29 @@ define sunet::frontend::load_balancer::website(
   String $confdir,
   Hash   $config
 ) {
+  # Parameters used in frontend/docker-compose_template.erb
+  $instance = $name
+
   # 'export' config to a file
   file {
-    "${confdir}/${name}":
+    "${confdir}/${instance}":
       ensure  => 'directory',
       group   => 'sunetfrontend',
       mode    => '0750',
       ;
-    "${confdir}/${name}/config.yml":
+    "${confdir}/${instance}/config.yml":
       ensure  => 'file',
       group   => 'sunetfrontend',
       mode    => '0640',
       content => inline_template("# File created from Hiera by Puppet\n<%= @config.to_yaml %>\n"),
       ;
+  }
+
+  sunet::docker_compose { "frontend-${instance}":
+    content        => template('sunet/frontend/docker-compose_template.erb'),
+    service_prefix => 'frontend',
+    service_name   => $instance,
+    compose_dir    => "${confdir}/${name}/compose",
+    description    => "SUNET frontend setup for ${instance}",
   }
 }
