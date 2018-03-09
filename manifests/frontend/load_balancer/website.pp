fe9d7f50563317bd25e8849d3af4e7f005c334ef
Fredrik Thulin <fredrik@thulin.net>
2018-03-09 17:04:47 +0100

    add  tls_certificate_bundle to config

--- manifests/frontend/load_balancer/website.pp
+++ manifests/frontend/load_balancer/website.pp
@@ -4,6 +4,42 @@ define sunet::frontend::load_balancer::website(
   String $confdir,
   Hash   $config
 ) {
+  if ! has_key('tls_certificate_bundle', $config) {
+    # Put suitable certificate path in $config['tls_certificate_bundle']
+    if has_key($tls_certificates, 'snakeoil') {
+      $snakeoil = $tls_certificates['snakeoil']['bundle']
+    }
+    if has_key($tls_certificates, $name) {
+      # Site name found in tls_certificates - good start
+      $_tls_certificate_bundle = pick(
+        $tls_certificates[$name]['haproxy'],
+        $tls_certificates[$name]['certkey'],
+        $tls_certificates[$name]['infra_certkey'],
+        $tls_certificates[$name]['bundle'],
+        $tls_certificates[$name]['dehydrated_bundle'],
+        'NOMATCH',
+      )
+      if $_tls_certificate_bundle != 'NOMATCH' {
+        $tls_certificate_bundle = $_tls_certificate_bundle
+      } else {
+        $_site_certs = $tls_certificates[$name]
+        notice("None of the certificates for site ${name} matched my list (haproxy, certkey, infra_certkey, bundle): $_site_certs")
+        if $snakeoil {
+          $tls_certificate_bundle = $snakeoil
+        }
+      }
+    } elsif $snakeoil {
+      $tls_certificate_bundle = $snakeoil
+    }
+
+    if $snakeoil and $tls_certificate_bundle == $snakeoil {
+      notice("Using snakeoil certificate for site ${name}")
+    }
+    $config2 = merge($config, {'tls_certificate_bundle': $tls_certificate_bundle})
+  } else {
+    $config2 = $config
+  }
+
   # 'export' config to a file
   file {
     "${confdir}/${name}":
@@ -15,7 +51,7 @@ define sunet::frontend::load_balancer::website(
       ensure  => 'file',
       group   => 'sunetfrontend',
       mode    => '0640',
-      content => inline_template("# File created from Hiera by Puppet\n<%= @config.to_yaml %>\n"),
+      content => inline_template("# File created from Hiera by Puppet\n<%= @config2.to_yaml %>\n"),
       ;
   }
 
