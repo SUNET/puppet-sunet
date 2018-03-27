c2044fa2dd43ceb19c7112dbb09e3a652fbb636d
Fredrik Thulin <fredrik@thulin.net>
2018-03-27 15:35:37 +0200

    make more flexible for use in sunet-frontend

--- manifests/docker_compose.pp
+++ manifests/docker_compose.pp
@@ -1,18 +1,17 @@
 # Wrapper for garethr-docker docker_compose to do SUNET specific things
 define sunet::docker_compose(
-  String $content,
-  String $compose_dir,
-  String $description,
-  String $service_name,
-  String $service_prefix = 'sunet',
+  String        $content,
+  String        $compose_dir,
+  String        $description,
+  String        $service_name,
+  String        $service_prefix = 'sunet',
+  Array[String] $service_extras = [],
+  String        $compose_filename = "${service_name}.yml",
 ) {
-  $compose_file = "${compose_dir}/${service_name}/${service_name}.yml"
+  $compose_file = "${compose_dir}/${service_name}/${compose_filename}"
 
-  # docker-compose uses dirname as project name
-  ensure_resource('file', [$compose_dir, "${compose_dir}/${service_name}"], {
-      ensure => 'directory',
-      mode   => '700',
-  })
+  # docker-compose uses dirname as project name, so we add $service_name and put the compose_file in there
+  ensure_resource('sunet::misc::create_dir', ["${compose_dir}/${service_name}"], { owner => 'root', group => 'root', mode => '0700' })
 
   ensure_resource('file', $compose_file, {
       ensure  => 'file',
@@ -22,8 +21,9 @@ define sunet::docker_compose(
   })
 
   sunet::docker_compose_service { "${service_prefix}-${service_name}":
-    compose_file => $compose_file,
-    description  => $description,
-    require      => File[$compose_file],
+    compose_file   => $compose_file,
+    description    => $description,
+    require        => File[$compose_file],
+    service_extras => $service_extras,
   }
 }
