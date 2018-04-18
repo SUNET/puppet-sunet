779ffed8da065c8cd0a6bee4f2eb99dc4d0070b3
Fredrik Thulin <fredrik@thulin.net>
2018-04-18 13:26:48 +0200

    increase processes limits on frontend servers

--- manifests/nagios.pp
+++ manifests/nagios.pp
@@ -72,9 +72,16 @@ class sunet::nagios($nrpe_service = 'nagios-nrpe-server') {
          command_line => '/usr/lib/nagios/plugins/check_procs -w 150 -c 200'
       }
    } else {
-      sunet::nagios::nrpe_command {'check_total_procs_lax':
+     if ('frontend_server' in $facts['cosmos']['host_roles']) {
+       # There are more processes than normal on frontend hosts
+       sunet::nagios::nrpe_command {'check_total_procs_lax':
+         command_line => '/usr/lib/nagios/plugins/check_procs -k -w 500 -c 750'
+       }
+     } else {
+       sunet::nagios::nrpe_command {'check_total_procs_lax':
          command_line => '/usr/lib/nagios/plugins/check_procs -k -w 150 -c 200'
-      }
+       }
+     }
    }
    sunet::nagios::nrpe_command {'check_uptime':
       command_line => '/usr/lib/nagios/plugins/check_uptime.pl -f'
