fe1bf510b510a4119e63974b65be18d080582837
Leif Johansson <leifj@sunet.se>
2018-06-17 16:20:22 +0200

    support basic udp and tcp server config

--- manifests/rsyslog.pp
+++ manifests/rsyslog.pp
@@ -30,18 +30,32 @@ class sunet::rsyslog(
   }
 
   if ($tcp_port or $udp_port) {
-     $common = ['set /files/etc/rsyslog.conf/\$ModLoad imuxsock', 'set /files/etc/rsyslog.conf/\$ModLoad imklog']
+     augeas { "rsyslog_conf_imuxsock":
+        changes => ['set /files/etc/rsyslog.conf/\$ModLoad imuxsock'],
+        notify  => Service['rsyslog'],
+        onlyif  => "match /files/etc/rsyslog.conf/\$ModLoad/imuxsock size == 0"
+     }
+     augeas { "rsyslog_conf_imklog":
+        changes => ['set /files/etc/rsyslog.conf/\$ModLoad imklog'],
+        notify  => Service['rsyslog'],
+        onlyif  => "match /files/etc/rsyslog.conf/\$ModLoad/imklog size == 0"
+     }
      $set_udp = $udp_port ? {
         undef   => [],
-        default => ['set /files/etc/rsyslog.conf/\$ModLoad imudp',"set /files/etc/rsyslog.conf/\$UDPServerRun $udp_port"]
+        default => ["set /files/etc/rsyslog.conf/\$UDPServerRun $udp_port"]
      }
 
      $set_tcp = $tcp_port ? {
         undef   => [],
-        default => ['set /files/etc/rsyslog.conf/\$ModLoad imtcp',"set /files/etc/rsyslog.conf/\$TCPServerRun $tcp_port"]
+        default => ["set /files/etc/rsyslog.conf/\$TCPServerRun $tcp_port"]
      }
 
      if ($udp_port) {
+        augeas { "rsyslog_conf_imudp":
+           changes => ['set /files/etc/rsyslog.conf/\$ModLoad imudp'],
+           notify  => Service['rsyslog'],
+           onlyif  => "match /files/etc/rsyslog.conf/\$ModLoad/imudp size == 0"
+        }
         ufw::allow { "allow-syslog-udp-${udp_port}": 
            from  => "${udp_client}",
            ip    => 'any',
@@ -51,6 +65,11 @@ class sunet::rsyslog(
      }
 
      if ($tcp_port) {
+        augeas { "rsyslog_conf_imtcp":
+           changes => ['set /files/etc/rsyslog.conf/\$ModLoad imtcp'],
+           notify  => Service['rsyslog'],
+           onlyif  => "match /files/etc/rsyslog.conf/\$ModLoad/imtcp size == 0"
+        }
         ufw::allow { "allow-syslog-tcp-${tcp_port}":
            from  => "${tcp_client}",
            ip    => 'any',
@@ -66,6 +85,7 @@ class sunet::rsyslog(
         changes => $changes,
         notify  => Service['rsyslog']
      }
+
   }
 
 }
