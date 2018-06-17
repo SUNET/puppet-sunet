982c1ee9436f03b4d815c301ccad9bf45e7c0908
Leif Johansson <leifj@sunet.se>
2018-06-17 14:55:00 +0200

    support basic udp and tcp server config

--- manifests/rsyslog.pp
+++ manifests/rsyslog.pp
@@ -1,6 +1,8 @@
 class sunet::rsyslog(
   $syslog_servers = hiera_array('syslog_servers',[]),
   $relp_syslog_servers = hiera_array('relp_syslog_servers',[]),
+  $udp_port = hiera('udp_port',undef),
+  $tcp_port = hiera('tcp_port',undef),
 ) {
   ensure_resource('package', 'rsyslog', {
     ensure => 'installed'
@@ -17,11 +19,32 @@ class sunet::rsyslog(
     ensure    => 'running',
     enable    => true,
     subscribe => File['/etc/rsyslog.d/60-remote.conf'],
-    })
+  })
 
   if $relp_syslog_servers != [] {
     ensure_resource('package', 'rsyslog-relp', {
       ensure => 'installed'
       })
   }
+
+  $set_udp = $udp_port ? {
+     undef   => [],
+     default => ["set \$ModLoad imudp","set \$UDPServerRun $udp_port"]
+  }
+
+  $set_tcp = $tcp_port ? {
+     undef   => [],
+     default => ["set \$ModLoad imtcp","set \$TCPServerRun $tcp_port"]
+  }
+
+  if defined($tcp_port) || defined($udp_port) {
+     $changes = flatten($set_udp,$set_tcp)
+     include augeas
+     augeas { "rsyslog_conf":
+        context => "/files/etc/rsyslog.conf",
+        changes => $changes,
+        notify  => Service['rsyslog']
+     }
+  }
+
 }
