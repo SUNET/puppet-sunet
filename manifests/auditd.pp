2c0fb56cfebaae9e1a611bac401fe7398f2651e7
Leif Johansson <leifj@sunet.se>
2018-06-19 08:41:33 +0200

    basic auditd config

--- manifests/auditd.pp
+++ manifests/auditd.pp
@@ -2,6 +2,8 @@ class sunet::auditd {
    package { 'auditd': ensure => latest }
    service { 'auditd': ensure => running }
    augeas {'enable_auditd_syslog':
+      incl    => "/etc/audisp/plugins.d/syslog.conf/active",
+      lens    => "Simplevars.lns",
       changes => ["set /files/etc/audisp/plugins.d/syslog.conf/active yes"],
       notify  => Service['auditd']
    }
