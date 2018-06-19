c63a57bc73d34192781c78cf863c4716d39aeebe
Leif Johansson <leifj@sunet.se>
2018-06-19 08:50:17 +0200

    log audit to facility local0

--- manifests/auditd.pp
+++ manifests/auditd.pp
@@ -4,7 +4,8 @@ class sunet::auditd {
    augeas {'enable_auditd_syslog':
       incl    => "/etc/audisp/plugins.d/syslog.conf",
       lens    => "Simplevars.lns",
-      changes => ["set /files/etc/audisp/plugins.d/syslog.conf/active yes"],
+      changes => ["set /files/etc/audisp/plugins.d/syslog.conf/active yes",
+                  "set /files/etc/audisp/plugins.d/syslog.conf/args LOG_INFO LOG_LOCAL0"],
       notify  => Service['auditd']
    }
 }
