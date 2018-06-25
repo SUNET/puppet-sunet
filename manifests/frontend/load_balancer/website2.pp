80d2b3fa5a159f2b690331706a95839deecce44b
Fredrik Thulin <fredrik@thulin.net>
2018-06-25 16:46:17 +0200

    small run_command experiment

--- manifests/frontend/load_balancer/website2.pp
+++ manifests/frontend/load_balancer/website2.pp
@@ -84,6 +84,10 @@ define sunet::frontend::load_balancer::website2(
   $varnish_enabled  = pick($config['varnish_enabled'], false)
   $varnish_storage  = pick($config['varnish_storage'], 'malloc,100M')
 
+  $run_command = $instance ? {
+    'graphs' => 'haproxy',
+    default  => undef,
+  }
   sunet::docker_compose { "frontend-${instance}":
     content          => template('sunet/frontend/docker-compose_template.erb'),
     service_prefix   => 'frontend',
@@ -92,6 +96,7 @@ define sunet::frontend::load_balancer::website2(
     compose_filename => 'docker-compose.yml',
     description      => "SUNET frontend instance ${instance} (site ${site_name})",
     service_extras   => ["ExecStartPost=-${basedir}/scripts/configure-container-network ${name}"],
+    run_command      => $run_command,
   }
 
   each($config['backends']) | $k, $v | {
