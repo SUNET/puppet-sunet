5f2238deea146f30e3698b20c0a186add2836e7a
John Van de Meulebrouck Brendgard <john@sunet.se>
2018-06-27 18:13:56 +0200

    First iteration of working docker_registry conf

--- manifests/docker_registry.pp
+++ manifests/docker_registry.pp
@@ -1,2 +1,57 @@
-class sunet::docker_registry () {
+class sunet::docker_registry (
+    String $public_hostname          = 'docker.sunet.se',
+    String $registry_images_basedir  = '/var/lib/registry',
+    String $registry_conf_basedir    = '/etc/docker/registry',
+    String $apache_conf_basedir      = '/etc/apache2',
+) {
+    ensure_resource('sunet::system_user', 'www-data', {
+        username => 'www-data',
+        group    => 'www-data',
+    })
+
+    exec { 'registry_images_basedir_mkdir':
+        command => "/bin/mkdir -p ${registry_images_basedir}",
+        unless  => "/usr/bin/test -d ${registry_images_basedir}",
+    } ->
+
+    exec { 'registry_conf_basedir':
+        command => "/bin/mkdir -p ${registry_conf_basedir}",
+        unless  => "/usr/bin/test -d ${registry_conf_basedir}",
+    } ->
+
+    exec { 'apache_conf_basedir':
+        command => "/bin/mkdir -p ${apache_conf_basedir}/sites-available",
+        unless  => "/usr/bin/test -d ${apache_conf_basedir}/sites-available",
+    } ->
+
+    file { "${registry_conf_basedir}/config.yml":
+        ensure  => file,
+        mode    => '0640',
+        content => template('sunet/docker_registry/config.erb')
+    }
+
+    file { "${apache_conf_basedir}/sites-available/registry-auth-ssl.conf":
+        ensure  => file,
+        mode    => '0640',
+        group   => 'www-data',
+        require => Sunet::System_user['www-data'],
+        content => template('sunet/docker_registry/registry-auth-ssl.erb')
+    }
+
+    file { "/etc/ssl/certs/${public_hostname}-client-ca.crt":
+        ensure  => file,
+        mode    => '0444',
+        group   => 'www-data',
+        require => Sunet::System_user['www-data'],
+        content => template('sunet/docker_registry/ca.crt.erb')
+    }
+
+    sunet::docker_compose {'docker-registry':
+        content          => template('sunet/docker_registry/docker-compose_docker_registry.yml.erb'),
+        service_name     => 'docker-registry',
+        compose_dir      => "${registry_conf_basedir}",
+        compose_filename => 'docker-compose_docker_registry.yml',
+        description      => 'docker-registry service',
+    }
+
 }
