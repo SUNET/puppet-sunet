eefa54c9d51f44980363797eeb12e6d03e5bf880
Linus Nordberg <linus@sunet.se>
2019-03-06 16:24:13 +0100

    do install apache2
    So, require => Package['foo'] does not install package foo. Noted.

--- manifests/cgit/webserver.pp
+++ manifests/cgit/webserver.pp
@@ -4,14 +4,20 @@ class sunet::cgit::webserver(
   String $www_user = 'www-data',
   String $git_group = 'git',
 ) {
+  package { $package:
+    ensure => 'installed'
+  }
+
   service { "$service":
     ensure => 'running',
     enable => true,
     require => Package[$package],
   }
+
   exec { 'let web user read git repos':
     command => "adduser $www_user $git_group",
   }
+
   exec { 'enable CGI':
     command => 'a2enmod cgi',
     notify  => Service[$service],
