408476210a8d211e200ebc680ce9e1fcfd630180
Linus Nordberg <linus@sunet.se>
2019-03-13 13:02:33 +0100

    Give up on understanding puppet
    Cram everything into sunet::cgit. Not that it matters anyway.

--- manifests/apache2.pp
+++
@@ -1,9 +0,0 @@
-class sunet::apache2(
-) {
-  ensure_package(['apache2'])
-  service { 'apache2':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['apache2'],
-  }
-}
--- manifests/cgit.pp
+++ manifests/cgit.pp
@@ -1,8 +1,25 @@
 class sunet::cgit(
   String $package       = 'cgit',
   String $cgitrepo_path = '/home/git/repositories/',
+  String $www_user  = 'www-data',
+  String $git_group = 'git',
 ) {
-  sunet::cgit::webserver{ 'webserver for cgit': }
+  ensure_package(['apache2'])
+
+  service { 'apache2':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['apache2'],
+  }
+
+  exec { 'let web user read git repos':
+    command => "adduser $www_user $git_group",
+  }
+
+  exec { 'enable CGI':
+    command => 'a2enmod cgi',
+    notify  => Service['apache2'],
+  }
 
   file { '/etc/cgitrc':
     content => template('sunet/cgit/cgitrc.erb'),
