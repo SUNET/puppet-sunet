86bbb58708549857a509cd6d04d39335db851bf7
John Van de Meulebrouck Brendgard <john@sunet.se>
2019-03-13 19:13:02 +0100

    Some small fixes in manifests/cgit.pp

--- manifests/cgit.pp
+++ manifests/cgit.pp
@@ -19,14 +19,14 @@ class sunet::cgit(
     content => template('sunet/cgit/cgitrc.erb'),
   }
 
-  file { '/etc/apache2/sites-available/010-cgit':
+  file { '/etc/apache2/sites-available/010-cgit.conf':
     content => template('sunet/cgit/apache2-siteconf.erb'),
   }
 
   exec { 'enable 010-cgit':
     command => 'a2ensite 010-cgit',
     creates => '/etc/apache2/sites-enabled/010-cgit.conf',
-    onlyif  => 'test -s /etc/ssl/certs/git.sunet.se.crt',
+    onlyif  => 'test -s /etc/ssl/certs/git-prod-1.sunet.se.crt',
     notify  => Service['apache2'],
   }
 }
