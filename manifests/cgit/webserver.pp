e4f54f1bb6a04f1e6db9e5b388a0c3349e125e3a
Linus Nordberg <linus@sunet.se>
2019-03-13 12:47:15 +0100

    Try harder

--- manifests/cgit/webserver.pp
+++ manifests/cgit/webserver.pp
@@ -2,7 +2,7 @@ class sunet::cgit::webserver(
   String $www_user = 'www-data',
   String $git_group = 'git',
 ) {
-  sunet::apache2{'apache2 for cgit':}
+  class sunet::apache2{ 'apache2 for cgit': }
 
   exec { 'let web user read git repos':
     command => "adduser $www_user $git_group",
