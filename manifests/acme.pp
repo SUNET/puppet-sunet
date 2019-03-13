4edf155bb164893c973f18e3f26d6e131d0e7b25
Linus Nordberg <linus@sunet.se>
2019-03-13 21:31:53 +0100

    Stop apache from restarting too often

--- manifests/acme.pp
+++ manifests/acme.pp
@@ -9,11 +9,14 @@ class sunet::acme(
 
   exec { 'acme: enable proxy modules':
     command => 'a2enmod proxy proxy_http',
+    creates => ['/etc/apache2/mods-enabled/proxy.load',
+                '/etc/apache2/mods-enabled/proxy_http.load'],
     notify  => Service['apache2'],
   }
 
   exec { 'acme: enable acme conf':
     command => 'a2enconf acme',
+    creates => '/etc/apache2/conf-enabled/acme.conf',
     notify  => Service['apache2'],
   }
 
--- manifests/apache2.pp
+++ manifests/apache2.pp
@@ -10,6 +10,7 @@ class sunet::apache2(
 
   exec { 'enable TLS':
     command => 'a2enmod ssl',
+    creates => '/etc/apache2/mods-enabled/ssl.load',
     notify  => Service['apache2'],
   }
 }
