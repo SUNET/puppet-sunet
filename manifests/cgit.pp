56fc2cf3c3116523a68a7759bf4312db5b84d54d
Linus Nordberg <linus@sunet.se>
2019-03-13 16:40:14 +0100

    Add apache site config for cgit
    But only if the cert file exists, or we get a chicken-and-egg problem
    since this apache instance is used for the LE challenge.
    
    Also, use the cgid module rather than cgi -- apache with threaded MPM
    selects cgid.
    
    Also also, limit invocations with 'creates'.

--- manifests/cgit.pp
+++ manifests/cgit.pp
@@ -1,4 +1,5 @@
 class sunet::cgit(
+  String $fqdn,
   String $package       = 'cgit',
   String $cgitrepo_path = '/home/git/repositories/',
   String $www_user      = 'www-data',
@@ -9,11 +10,22 @@ class sunet::cgit(
   }
 
   exec { 'enable CGI':
-    command => 'a2enmod cgi',
+    command => 'a2enmod cgid',
+    creates => '/etc/apache2/mods-enabled/cgid.conf',
     notify  => Service['apache2'],
   }
 
   file { '/etc/cgitrc':
     content => template('sunet/cgit/cgitrc.erb'),
   }
+
+  file { '/etc/apache2/sites-available/010-cgit':
+    template => 'sunet/cgit/apache2-siteconf.erb',
+  }
+
+  exec { '/etc/apache2/sites-enabled/010-cgit.conf':
+    command => 'a2ensite 010-cgit',
+    creates => $name,
+    onlyif  => 'test -s /etc/ssl/certs/git.sunet.se.crt',
+    notify  => Service['apache2'],
 }
