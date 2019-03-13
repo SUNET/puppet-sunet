9a620ae2da11169e681d6f4a56fa304d92e5029e
Linus Nordberg <linus@sunet.se>
2019-03-13 12:52:05 +0100

    Syntax

--- manifests/cgit.pp
+++ manifests/cgit.pp
@@ -2,7 +2,7 @@ class sunet::cgit(
   String $package       = 'cgit',
   String $cgitrepo_path = '/home/git/repositories/',
 ) {
-  class sunet::cgit::webserver{ 'webserver for cgit': }
+  class { 'sunet::cgit::webserver': }
 
   file { '/etc/cgitrc':
     content => template('sunet/cgit/cgitrc.erb'),
--- manifests/cgit/webserver.pp
+++ manifests/cgit/webserver.pp
@@ -2,7 +2,7 @@ class sunet::cgit::webserver(
   String $www_user = 'www-data',
   String $git_group = 'git',
 ) {
-  class sunet::apache2{ 'apache2 for cgit': }
+  class { 'sunet::apache2': }
 
   exec { 'let web user read git repos':
     command => "adduser $www_user $git_group",
