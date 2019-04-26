fe2da419350834d14747c34d9b479e0eb1c7a18a
Fredrik Thulin <fredrik@thulin.net>
2019-04-26 18:23:17 +0200

    add stuff for ici-acme client

---
+++ manifests/ici_acme/client.pp
@@ -0,0 +1,28 @@
+class sunet::ici_acme::client(
+  String $ca_url => 'http://ca-test-1.sunet.se/',
+
+) {
+  package { 'python3-josepy':
+    ensure => 'installed',
+  }
+
+  sunet::misc::create_dir { '/etc/ici_acme/dehydrated': owner => 'root', group => 'root', mode => '0700' }
+
+  file {
+    '/etc/ici_acme/dehydrated/config':
+      ensure  => file,
+      mode    => '0600',
+      content => template('sunet/ici_acme/dehydrated_config.erb'),
+      ;
+    '/usr/local/sbin/ici_acme_client':
+        ensure  => file,
+        mode    => '0755',
+        content => template('sunet/ici_acme/ici_acme_client.erb'),
+      ;
+    '/usr/local/sbin/ici-acme-pre-auth.py':
+        ensure  => file,
+        mode    => '0755',
+        content => template('sunet/ici_acme/ici-acme-pre-auth.py.erb'),
+      ;
+  }
+}
