9e2680a6fbf8b65bc1da442cd7d745acfa94104a
Fredrik Thulin <fredrik@thulin.net>
2019-05-28 12:48:22 +0200

    try this

--- manifests/frontend/load_balancer.pp
+++ manifests/frontend/load_balancer.pp
@@ -89,8 +89,8 @@ class sunet::frontend::load_balancer(
     sunet::frontend::statsd { 'frontend_statsd':
       docker_image    => pick($config['statsd_image'], 'docker.sunet.se/eduid/statsd'),
       docker_imagetag => pick($config['statsd_imagetag'], 'stable'),
-      repeat_host     => pick($config['statsd_repeat_host'], ''),
-      repeat_port     => pick($config['statsd_repeat_port'], ''),
+      repeat_host     => $config['statsd_repeat_host'],
+      repeat_port     => $config['statsd_repeat_port'],
     }
 
     sunet::misc::ufw_allow { 'always-https-allow-http':
