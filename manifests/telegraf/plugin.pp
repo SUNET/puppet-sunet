78a9a753e3dae1c174c3723af2d82979c4d15a99
Leif Johansson <leifj@sunet.se>
2019-07-24 23:38:45 +0200

    avoid crashing

--- manifests/telegraf/plugin.pp
+++ manifests/telegraf/plugin.pp
@@ -1,11 +1,13 @@
 define sunet::telegraf::plugin($config=undef) {
-   $params = $config ? {
-      undef   => hiera("telegraf_plugin_$title",{}),
-      default => $config
-   }
-   file { "/etc/telegraf/telegraf.d/$title.conf": 
-      ensure  => file,
-      content => epp("sunet/telegraf/plugins/$title-conf.epp",$params),
-      notify  => Service['telegraf']
+   if (defined(Service['telegraf'])) {
+      $params = $config ? {
+         undef   => hiera("telegraf_plugin_$title",{}),
+         default => $config
+      }
+      file { "/etc/telegraf/telegraf.d/$title.conf": 
+         ensure  => file,
+         content => epp("sunet/telegraf/plugins/$title-conf.epp",$params),
+         notify  => Service['telegraf']
+      }
    }
 }
