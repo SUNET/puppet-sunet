e35e516536d622d9ce540f009a6fb4d53c696778
Fredrik Thulin <fredrik@thulin.net>
2019-09-05 12:42:51 +0200

    lint

--- manifests/snippets.pp
+++ manifests/snippets.pp
@@ -252,19 +252,19 @@ define sunet::snippets::ssh_command(
 
 # Same as the above, but for hosts using sunet::ssh_config.
 define sunet::snippets::ssh_command2(
-  $user         = 'root',
-  $manage_user  = false,
-  $ssh_key      = undef,
-  $ssh_key_type = undef,
-  $command      = "ls /tmp"
+  String $ssh_key,
+  String $ssh_key_type,
+  String $command,
+  String $user         = 'root',
+  Boolean $manage_user = false,
 ) {
   $safe_name = regsubst($name, '[^0-9A-Za-z.\-]', '-', 'G')
 
   if ($manage_user) {
-    ensure_resource('group',"${user}")
-    sunet::system_user { "${user}":
-      username  => "${user}",
-      group     => "${user}"
+    ensure_resource('group', $user)
+    sunet::system_user { $user:
+      username => $user,
+      group    => $user,
     }
   }
 
@@ -273,17 +273,18 @@ define sunet::snippets::ssh_command2(
     'type' => $ssh_key_type,
     'options' => join([
                        "command=\"${command}\"",
-                       "no-agent-forwarding",
-                       "no-port-forwarding",
-                       "no-pty",
-                       "no-user-rc",
-                       "no-X11-forwarding"
+                       'no-agent-forwarding',
+                       'no-port-forwarding',
+                       'no-pty',
+                       'no-user-rc',
+                       'no-X11-forwarding'
                        ], ','),
     }}
 
   $config = { $user => [$safe_name]}
   sunet::ssh_keys { $name:
-    config => $config,
+    config   => $config,
     database => $database,
+    order    => '200',
   }
 }
