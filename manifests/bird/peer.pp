7e135891edcf94a3b343b7b7cbd695cdb39ee6ac
Fredrik Thulin <fredrik@thulin.net>
2019-09-12 10:12:13 +0200

    bugfix notify - those are names of the services, not variables

--- manifests/bird/peer.pp
+++ manifests/bird/peer.pp
@@ -16,14 +16,14 @@ define sunet::bird::peer(
       target   => '/etc/bird/bird.conf',
       order    => '20',
       content  => template("sunet/bird/bgp.erb"),
-      notify   => Service[$bird]
+      notify  => Service['bird']
     }
   } elsif is_ipaddr($remote_ip, 6) {
     concat::fragment { "${name}_bgp_peer6":
       target   => '/etc/bird/bird6.conf',
       order    => '20',
       content  => template("sunet/bird/bgp.erb"),
-      notify   => Service[$bird6]
+      notify  => Service['bird6']
     }
   } else {
     fail("Peer IP address $remote_ip is neither IPv4 nor IPv6")
