e09bd87cf97008306281afaab7fb088f0abe6224
Fredrik Thulin <fredrik@thulin.net>
2019-10-11 10:46:48 +0200

    allow setting uid/gid

--- manifests/misc/system_user.pp
+++ manifests/misc/system_user.pp
@@ -1,9 +1,13 @@
 define sunet::misc::system_user(
-  String           $group,
-  Optional[String] $username = undef,
-  Boolean          $system = true,
-  Boolean          $managehome = false,
-  String           $shell = '/bin/false'
+  String                  $group,
+  Optional[String]        $username = undef,
+  Boolean                 $system = true,
+  Boolean                 $managehome = false,
+  String                  $shell = '/bin/false'
+  Optional[String]        $home = undef,
+  Optional[Integer]       $uid = undef,
+  Optional[Integer]       $gid = undef,
+  Optional[Array[String]] $groups = undef,
 ) {
 
   $_username = $username ? {
@@ -17,12 +21,16 @@ define sunet::misc::system_user(
   })
 
   ensure_resource('user', $name, {
-    ensure     => present,
+    ensure     => 'present',
     name       => $_username,
-    membership => minimum,
+    membership => 'minimum',
     system     => $system,
     require    => Group[ $group ],
     shell      => $shell,
     managehome => $managehome,
+    home       => $home,
+    uid        => $uid,
+    gid        => $group,
+    groups     => $groups,
   })
 }
