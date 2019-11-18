d1c39f0f96fe118d15b84dd9e421adeec2b9cee3
Fredrik Thulin <fredrik@thulin.net>
2019-11-18 15:50:50 +0100

    fix missing quote

--- manifests/dockerhost.pp
+++ manifests/dockerhost.pp
@@ -233,7 +233,7 @@ class sunet::dockerhost(
   if $run_docker_cleanup {
     # Cron job to remove old unused docker images
     sunet::scriptherder::cronjob { 'dockerhost_cleanup':
-      cmd           => '/bin/echo \'Running \"/usr/bin/docker system prune -af\" disabled since it removes too many tags\"',
+      cmd           => '/bin/echo \'Running \"/usr/bin/docker system prune -af\" disabled since it removes too many tags\"\'',
       special       => 'daily',
       ok_criteria   => ['exit_status=0', 'max_age=25h'],
       warn_criteria => ['exit_status=0', 'max_age=49h'],
