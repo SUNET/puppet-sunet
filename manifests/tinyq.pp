b52d746ce0e3f8874c516764f351799283a40418
Leif Johansson <leifj@sunet.se>
2020-04-27 11:11:09 +0200

    tinyq config

---
+++ manifests/tinyq.pp
@@ -0,0 +1,17 @@
+class sunet::tinyq() {
+   apt::ppa { 'ppa:sunet/tinyq': } ->
+   package { 'tinyq': ensure => latest } ->
+   service { 'tinyq': ensure => running }
+}
+
+define sunet::tinyq::component($config = {}, $template = undef, $order = '10', $ensure = 'file') {
+   $_template = $template ? {
+      undef   => $name,
+      default => $template
+   }
+   file {"/etc/tinyq/tinyq.d/${order}-${name}.tq":
+      ensure  => $ensure,
+      content => template("sunet/tinyq/${_template}.erb"),
+      notify  => Service['tinyq']
+   }
+}
