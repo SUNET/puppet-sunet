d07c8dcf05813f01d12c51c587570f0360fea92f
Leif Johansson <leifj@sunet.se>
2020-04-27 12:23:04 +0200

    tinyq namespace reorg

--- manifests/tinyq.pp
+++ manifests/tinyq.pp
@@ -3,23 +3,3 @@ class sunet::tinyq() {
    package { 'tinyq': ensure => latest } ->
    service { 'tinyq': ensure => running }
 }
-
-define sunet::tinyq::component($config = {}, $template = undef, $order = '99', $ensure = 'file') {
-   $_template = $template ? {
-      undef   => $name,
-      default => $template
-   }
-   file {"/etc/tinyq/tinyq.d/${order}-${name}.tq":
-      ensure  => $ensure,
-      content => template("sunet/tinyq/${_template}.erb"),
-      notify  => Service['tinyq']
-   }
-}
-
-class sunet::tinyq::library($ensure = 'file') {
-   sunet::tinyq::component { 'library': order => "10", ensure => $ensure }
-}
-
-class sunet::tinyq::daily_facter($ensure = 'file') {
-   sunet::tinyq::component { 'daily_facter': ensure => $ensure }
-}
