d90a3fa1e97cc8811b0c72a5ab71a7d236924c2b
Leif Johansson <leifj@sunet.se>
2020-05-07 16:50:10 +0200

    support removal

--- manifests/remote_file.pp
+++ manifests/remote_file.pp
@@ -1,10 +1,13 @@
-define sunet::remote_file($remote_location=undef, $mode='0644'){
-  exec{"retrieve_${title}":
-    command => "/usr/bin/wget -q ${remote_location} -O ${title}",
-    creates => $title,
+define sunet::remote_file($ensure=file, $remote_location=undef, $mode='0644'){
+  if ($ensure != 'absent') {
+     exec{"retrieve_${title}":
+       command => "/usr/bin/wget -q ${remote_location} -O ${title}",
+       creates => $title,
+     }
   }
 
   file{$title:
+    ensure  => ensure,
     mode    => $mode,
     require => Exec["retrieve_${title}"],
   }
