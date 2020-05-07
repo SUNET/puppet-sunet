f633246d3cbff232caae68840c85db8a05728057
Leif Johansson <leifj@sunet.se>
2020-05-07 16:21:33 +0200

    fticks converter

---
+++ manifests/fticks.pp
@@ -0,0 +1,28 @@
+class sunet::fticks($ensure=present,$url=undef,$args='',$pipe='/dev/fticks') {
+   ensure_resource(service,'rsyslog',{ensure=>running})
+   $_ensure_file = $ensure ? {
+       present   => file,
+       absent    => absent
+   }
+   $_ensure_service = $ensure ? {
+       present   => running
+       absent    => absent
+   }
+   sunet::remote_file{'/usr/sbin/fticks':
+       mode            => '0755',
+       ensure          => $_ensure_file,
+       remote_location => 'https://raw.githubusercontent.com/SUNET/flog/master/scripts/fticks.py'
+   }
+   file {'/etc/systemd/system/fticks.service':
+       ensure          => $_ensure_file,
+       content         => template('sunet/fticks/fticks.service.erb')
+   }
+   file {'/etc/rsyslog.d/70-sunet-fticks-pipe.conf':
+       ensure          => $_ensure_file,
+       content         => inline_template(":rawmsg,contains,"F-TICKS" |$pipe")
+       notify          => Service['rsyslog']
+   }
+   service {'fticks':
+      ensure           => $_ensure_service
+   }
+}
