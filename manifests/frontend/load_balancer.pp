037718ea7ff8ecf3be71740311e07154ac22f22e
Micke Nordin <kano@sunet.se>
2021-09-13 11:43:38 +0200

    Dummy commit

--- manifests/frontend/load_balancer.pp
+++ manifests/frontend/load_balancer.pp
@@ -8,8 +8,8 @@ class sunet::frontend::load_balancer(
 ) {
   $global_config = hiera_hash('sunet_frontend')
   $local_config = hiera_hash("sunet_frontend_local", undef)
-  # We put the local_config inside the load_balancer hash because otherwise this becomes a foot gun
-  # In the if clause below
+  # We put the local_config inside the load_balancer hash because otherwise this becomes
+  # a foot gun in the if clause below
   $config = deep_merge($global_config, { load_balancer => { local_config => $local_config}})
   if $config =~ Hash[String, Hash] {
     $confdir = "${basedir}/config"
