052d5480ca0484661bcd35e8d33ef70aeb918de9
Micke Nordin <kano@sunet.se>
2021-09-15 09:39:18 +0200

    Frontend: separate to different files

--- manifests/frontend/register_sites.pp
+++ manifests/frontend/register_sites.pp
@@ -33,28 +33,3 @@ class sunet::frontend::register_sites(
   }
 }
 
-class sunet::frontend::register_sites_array(
-  Array $sites,
-) {
-  file {
-    '/usr/local/bin/sunetfrontend-register':
-      ensure  => 'file',
-      mode    => '0755',
-      content => template('sunet/frontend/sunetfrontend-register.erb')
-      ;
-  }
-
-  $sites.each | $site | {
-    $fe_str = join($site['frontends'], ' ')
-    $port = $site['port']
-    $extra_args = pick($site['extra_args'], ' ')
-    $site_name = keys($site)[0]
-
-    cron { "sunetfronted_register_sites_${site_name}":
-      ensure  => present,
-      command => "/usr/local/bin/sunetfrontend-register ${extra_args} ${site_name} ${port} ${fe_str} > /dev/null 2>&1",
-      minute  => '*/3',
-      require => File['/usr/local/bin/sunetfrontend-register'],
-    }
-  }
-}
