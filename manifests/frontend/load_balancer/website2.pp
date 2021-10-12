037261d34ab27e08d6b3bb4e4b0d2fe4683a7bc3
Micke Nordin <kano@sunet.se>
2021-10-12 17:29:08 +0200

    Network bridgenames must be under 15 chars long

--- manifests/frontend/load_balancer/website2.pp
+++ manifests/frontend/load_balancer/website2.pp
@@ -8,6 +8,7 @@ define sunet::frontend::load_balancer::website2(
 ) {
   $instance  = $name
   $site_name = pick($config['site_name'], $instance)
+  $networkname = $instance[0,8]
 
   if ! has_key($config, 'tls_certificate_bundle') {
     # Put suitable certificate path in $config['tls_certificate_bundle']
