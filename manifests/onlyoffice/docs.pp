a54367cb174bac220abb7aac3de4f19d544a9c77
Micke Nordin <kano@sunet.se>
2021-12-20 11:37:06 +0000

    Add collabora, fix only office and remove postgres

--- manifests/onlyoffice/docs.pp
+++ manifests/onlyoffice/docs.pp
@@ -1,57 +1,52 @@
 # OnlyOffice document server
 define sunet::onlyoffice::docs(
-  Integer           $port         = 80,
-  Integer           $tls_port     = 443,
-  Optional[String]  $docker_image = 'onlyoffice/documentserver',
-  String            $docker_tag   = 'latest',
-  String            $basedir      = "/opt/onlyoffice/docs/${name}",
-  String            $hostname     = $::fqdn,
-  String            $db_host      = undef,
-  String            $db_name      = 'onlyoffice',
-  String            $db_user      = 'onlyoffice',
-  Enum['yes', 'no'] $letsencrypt  = 'no',
-  Optional[String]  $contact_mail = 'noc@sunet.se',
+  String           $basedir      = "/opt/onlyoffice/docs/${name}",
+  String           $contact_mail = 'noc@sunet.se',
+  String           $db_host      = undef,
+  String           $db_name      = 'onlyoffice',
+  String           $db_type      = 'mysql',
+  String           $db_user      = 'onlyoffice',
+  String           $docker_image = 'onlyoffice/documentserver',
+  String           $docker_tag   = 'latest',
+  Array[String]    $dns          = [],
+  String           $hostname     = $::fqdn,
+  Enum['yes','no'] $letsencrypt  = 'no',
+  Integer          $port         = 8080,
+  Integer          $tls_port     = 8443,
   ) {
+
+  sunet::system_user {'ds': username => 'ds',group => 'ds' }
+
+  $db_pwd = safe_hiera('document_db_password',undef)
+
+  $db_env = ["DB_HOST=${db_host}","DB_NAME=${db_name}","DB_USER=${db_user}","DB_TYPE=${db_type}"]
+  $db_pwd_env = $db_pwd ? {
+    undef   => [],
+    default => ["DB_PWD=${db_pwd}"]
+  }
+
+  $jwt_secret = safe_hiera('document_jwt_key',undef)
+  $jwt_env = $jwt_secret ? {
+    undef   => [],
+    default => ['JWT_ENABLED=true',"JWT_SECRET=${jwt_secret}"]
+  }
 
   $le_env = $letsencrypt ? {
     'no'    => [],
     default => ["LETS_ENCRYPT_DOMAIN=${hostname}","LETS_ENCRYPT_MAIL=${contact_mail}"]
   }
 
-  $jwt_secret = safe_hiera('onlyoffice_jwt_secret',undef)
-  $jwt_env = $jwt_secret ? {
-    undef   => [],
-    default => ['JWT_ENABLED=yes',"JWT_SECRET=${jwt_secret}"]
-  }
-
-  $db_pwd = safe_hiera('onlyoffice_db_pwd',undef)
-
-  $db_env = ["DB_HOST=${db_host}","DB_NAME=${db_name}","DB_USER=${db_user}"]
-  $db_pwd_env = $db_pwd ? {
-    undef   => [],
-    default => ["DB_PWD=${db_pwd}"]
-  }
-
+  $ds_environment = flatten([$db_env,$db_pwd_env,$jwt_env,$le_env,$misc_env])
   exec {"${name}_mkdir_basedir":
     command => "mkdir -p ${basedir}",
     unless  => "/usr/bin/test -d ${basedir}"
   }
-  -> file {[$basedir,"${basedir}/logs","${basedir}/data","${basedir}/data/certs",
-  "${basedir}/lib","${basedir}/db"]: ensure => directory }
-  -> exec {"${name}_create_dhparam":
-    command => "/usr/bin/openssl dhparam -out ${basedir}/data/certs/dhparam.pem 2048",
-    unless  => "/usr/bin/test -s ${basedir}/data/certs/dhparam.pem"
+  -> sunet::docker_compose { $name:
+    content          => template('sunet/onlyoffice/docker-compose.yml.erb'),
+    service_name     => 'onlyoffice',
+    compose_dir      => '/opt/',
+    compose_filename => 'docker-compose.yml',
+    description      => 'OnlyOffice Document Server',
   }
-  -> sunet::docker_run { $name:
-      image    => $docker_image,
-      imagetag => $docker_tag,
-      volumes  => [
-                    "${basedir}/logs:/var/log/onlyoffice",
-                    "${basedir}/data:/var/www/onlyoffice/Data",
-                    "${basedir}/lib:/var/lib/onlyoffice",
-                    "${basedir}/db:/var/lib/postgresql"
-                  ],
-      ports    => ["${port}:80","${tls_port}:443"],
-      env      => flatten([$le_env,$jwt_env,$db_env,$db_pwd_env]),
-  }
+  -> file {[$basedir,"${basedir}/logs","${basedir}/data","${basedir}/lib"]: ensure => directory }
 }
