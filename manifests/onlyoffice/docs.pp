903226de41a8da0e3f0cc758488d6a1fb162267f
Micke Nordin <kano@sunet.se>
2021-12-21 14:10:25 +0100

    Dito

--- manifests/onlyoffice/docs.pp
+++ manifests/onlyoffice/docs.pp
@@ -17,40 +17,11 @@ define sunet::onlyoffice::docs(
   ) {
 
   sunet::system_user {'ds': username => 'ds',group => 'ds' }
-
-  $amqp_env = ["AMQP_URI=${amqpuri}"]
-  $db_pwd = safe_hiera('document_db_password',undef)
-
-  $db_env = ["DB_HOST=${db_host}","DB_NAME=${db_name}","DB_USER=${db_user}","DB_TYPE=${db_type}"]
-  $db_pwd_env = $db_pwd ? {
-    undef   => [],
-    default => ["DB_PWD=${db_pwd}"]
-  }
-
-  $jwt_secret = safe_hiera('document_jwt_key',undef)
-  $jwt_env = $jwt_secret ? {
-    undef   => [],
-    default => ['JWT_ENABLED=true',"JWT_SECRET=${jwt_secret}"]
-  }
-
-  $le_env = $letsencrypt ? {
-    'no'    => [],
-    default => ["LETS_ENCRYPT_DOMAIN=${hostname}","LETS_ENCRYPT_MAIL=${contact_mail}"]
-  }
-
-  $ds_environment = flatten([$amqp_env,$db_env,$db_pwd_env,$jwt_env,$le_env])
   exec {"${name}_mkdir_basedir":
     command => "mkdir -p ${basedir}",
     unless  => "/usr/bin/test -d ${basedir}"
   }
-  -> sunet::docker_compose { $name:
-    content          => template('sunet/onlyoffice/docker-compose.yml.erb'),
-    service_name     => 'onlyoffice',
-    compose_dir      => '/opt/',
-    compose_filename => 'docker-compose.yml',
-    description      => 'OnlyOffice Document Server',
-  }
-  -> file {[$basedir,"${basedir}/logs","${basedir}/data","${basedir}/lib"]: ensure => directory }
+
   if $letsencrypt == 'no' {
     file {["${basedir}/certs"]: ensure => directory }
     -> exec {"${name}_create_key":
@@ -74,9 +45,40 @@ define sunet::onlyoffice::docs(
       command => '/usr/bin/dpkg-reconfigure ca-certificates',
       unless  => "/usr/bin/test -s ${basedir}/certs/linkrun.lock"
     }
-    exec {"${name}_linking_lock":
+    -> exec {"${name}_linking_lock":
       command => "/usr/bin/touch ${basedir}/certs/linkrun.lock",
       unless  => "/usr/bin/test -s ${basedir}/certs/linkrun.lock"
     }
   }
+
+  $le_env = $letsencrypt ? {
+    'no'    => [],
+    default => ["LETS_ENCRYPT_DOMAIN=${hostname}","LETS_ENCRYPT_MAIL=${contact_mail}"]
+  }
+
+  $amqp_env = ["AMQP_URI=${amqpuri}"]
+  $db_pwd = safe_hiera('document_db_password',undef)
+
+  $db_env = ["DB_HOST=${db_host}","DB_NAME=${db_name}","DB_USER=${db_user}","DB_TYPE=${db_type}"]
+  $db_pwd_env = $db_pwd ? {
+    undef   => [],
+    default => ["DB_PWD=${db_pwd}"]
+  }
+
+  $jwt_secret = safe_hiera('document_jwt_key',undef)
+  $jwt_env = $jwt_secret ? {
+    undef   => [],
+    default => ['JWT_ENABLED=true',"JWT_SECRET=${jwt_secret}"]
+  }
+
+
+  $ds_environment = flatten([$amqp_env,$db_env,$db_pwd_env,$jwt_env,$le_env])
+  sunet::docker_compose { $name:
+    content          => template('sunet/onlyoffice/docker-compose.yml.erb'),
+    service_name     => 'onlyoffice',
+    compose_dir      => '/opt/',
+    compose_filename => 'docker-compose.yml',
+    description      => 'OnlyOffice Document Server',
+  }
+  -> file {[$basedir,"${basedir}/logs","${basedir}/data","${basedir}/lib"]: ensure => directory }
 }
