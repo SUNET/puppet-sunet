f0cda566b774b056cc19588221913f1d69eab5bb
Micke Nordin <kano@sunet.se>
2021-12-21 13:50:45 +0100

    Run postgres and rabbitmq in separate container, and fix cert

--- manifests/onlyoffice/docs.pp
+++ manifests/onlyoffice/docs.pp
@@ -1,10 +1,11 @@
 # OnlyOffice document server
 define sunet::onlyoffice::docs(
+  String           $amqpuri      = 'amqp://guest:guest@rabbitmq',
   String           $basedir      = "/opt/onlyoffice/docs/${name}",
   String           $contact_mail = 'noc@sunet.se',
-  String           $db_host      = undef,
+  String           $db_host      = 'postgres',
   String           $db_name      = 'onlyoffice',
-  String           $db_type      = 'mysql',
+  String           $db_type      = 'postgres',
   String           $db_user      = 'onlyoffice',
   String           $docker_image = 'onlyoffice/documentserver',
   String           $docker_tag   = 'latest',
@@ -17,6 +18,7 @@ define sunet::onlyoffice::docs(
 
   sunet::system_user {'ds': username => 'ds',group => 'ds' }
 
+  $amqp_env = ["AMQP_URI=${amqpuri}"]
   $db_pwd = safe_hiera('document_db_password',undef)
 
   $db_env = ["DB_HOST=${db_host}","DB_NAME=${db_name}","DB_USER=${db_user}","DB_TYPE=${db_type}"]
@@ -36,26 +38,10 @@ define sunet::onlyoffice::docs(
     default => ["LETS_ENCRYPT_DOMAIN=${hostname}","LETS_ENCRYPT_MAIL=${contact_mail}"]
   }
 
-  $ds_environment = flatten([$db_env,$db_pwd_env,$jwt_env,$le_env])
+  $ds_environment = flatten([$amqp_env,$db_env,$db_pwd_env,$jwt_env,$le_env])
   exec {"${name}_mkdir_basedir":
-    command => "mkdir -p ${basedir}/data/certs",
-    unless  => "/usr/bin/test -d ${basedir}/data/certs"
-  }
-  -> exec {"${name}_create_key":
-    command => "/usr/bin/openssl genrsa -out ${basedir}/data/certs/onlyoffice.key 2048",
-    unless  => "/usr/bin/test -s ${basedir}/data/certs/onlyoffice.key"
-  }
-  -> exec {"${name}_create_csr":
-    command => "/usr/bin/openssl req -new -key ${basedir}/data/certs/onlyoffice.key -out ${basedir}/data/certs/onlyoffice.csr -subj '/C=SE/ST=Stockholm/L=Stockholm/O=SUNET/OU=Drive Team/CN=${hostname}'",
-    unless  => "/usr/bin/test -s ${basedir}/data/certs/onlyoffice.csr"
-  }
-  -> exec {"${name}_create_crt":
-    command => "/usr/bin/openssl x509 -req -days 3650 -signkey ${basedir}/data/certs/onlyoffice.key -in ${basedir}/data/certs/onlyoffice.csr -out ${basedir}/data/certs/onlyoffice.crt",
-    unless  => "/usr/bin/test -s ${basedir}/data/certs/onlyoffice.crt"
-  }
-  -> exec {"${name}_create_dhparam":
-    command => "/usr/bin/openssl dhparam -out ${basedir}/data/certs/dhparam.pem 2048",
-    unless  => "/usr/bin/test -s ${basedir}/data/certs/dhparam.pem"
+    command => "mkdir -p ${basedir}",
+    unless  => "/usr/bin/test -d ${basedir}"
   }
   -> sunet::docker_compose { $name:
     content          => template('sunet/onlyoffice/docker-compose.yml.erb'),
@@ -64,5 +50,36 @@ define sunet::onlyoffice::docs(
     compose_filename => 'docker-compose.yml',
     description      => 'OnlyOffice Document Server',
   }
-  -> file {[$basedir,"${basedir}/logs","${basedir}/data","${basedir}/data/certs","${basedir}/lib"]: ensure => directory }
+  -> file {[$basedir,"${basedir}/logs","${basedir}/data","${basedir}/certs","${basedir}/lib"]: ensure => directory }
+  if $letsencrypt == 'no' {
+    exec {"${name}_create_key":
+      command => "/usr/bin/openssl genrsa -out ${basedir}/certs/onlyoffice.key 2048",
+      unless  => "/usr/bin/test -s ${basedir}/certs/onlyoffice.key"
+    }
+    -> exec {"${name}_create_csr":
+      command => "/usr/bin/openssl req -new -key ${basedir}/certs/onlyoffice.key -out ${basedir}/certs/onlyoffice.csr -subj '/C=SE/ST=Stockholm/L=Stockholm/O=SUNET/OU=Skunk Works/CN=localhost'",
+      unless  => "/usr/bin/test -s ${basedir}/certs/onlyoffice.csr"
+    }
+    -> exec {"${name}_create_crt":
+      command => "/usr/bin/openssl x509 -req -days 3650 -signkey ${basedir}/certs/onlyoffice.key -in ${basedir}/data/certs/onlyoffice.csr -out ${basedir}/certs/onlyoffice.crt",
+      unless  => "/usr/bin/test -s ${basedir}/certs/onlyoffice.crt"
+    }
+    -> exec {"${name}_create_dhparam":
+      command => "/usr/bin/openssl dhparam -out ${basedir}/certs/dhparam.pem 2048",
+      unless  => "/usr/bin/test -s ${basedir}/certs/dhparam.pem"
+    }
+    -> file { "${name}_cert_link":
+      ensure => link,
+      name   => "${basedir}/certs/onlyoffice.crt",
+      target => '/usr/share/ca-certificates/onlyoffice.crt',
+    }
+    -> exec {"${name}_rebuild_cacerts":
+      command => '/usr/bin/dpkg-reconfigure ca-certificates',
+      unless  => "/usr/bin/test -s ${basedir}/certs/linkrun.lock"
+    }
+    -> exec {"${name}_linking_lock":
+      command => "/usr/bin/touch ${basedir}/certs/linkrun.lock",
+      unless  => "/usr/bin/test -s ${basedir}/certs/linkrun.lock"
+    }
+  }
 }
