a36316894230d921bd7c30dd9a01548569071506
Micke Nordin <kano@sunet.se>
2022-01-26 16:28:24 +0100

    Begin work on microk8s

---
+++ manifests/microk8s/node.pp
@@ -0,0 +1,17 @@
+# microk8s cluster node
+define sunet::microk8s::node(
+  Integer $failure_domain = 42,
+) {
+  package { 'snapd':
+    ensure   =>  latest,
+    provider => apt,
+  }
+  -> exec {'install_microk8s':
+    command => 'snap install microk8s --classic',
+    unless  => 'snap list microk8s',
+  }
+  -> file_line {'microk8s_ha_conf':
+    line => "failure-domain=${failure_domain}",
+    path => '/var/snap/microk8s/current/args/ha-conf'
+  }
+}
