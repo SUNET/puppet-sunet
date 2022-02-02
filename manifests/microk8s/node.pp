fa1d10c616b4b52eec489101b1e795b9d460564d
Micke Nordin <kano@sunet.se>
2022-02-02 17:27:34 +0100

    Fix firewall rules

--- manifests/microk8s/node.pp
+++ manifests/microk8s/node.pp
@@ -6,16 +6,41 @@ class sunet::microk8s::node(
     ensure   =>  latest,
     provider => apt,
   }
-  -> exec {'install_microk8s':
+  -> exec { 'install_microk8s':
     command => 'snap install microk8s --classic --channel=1.20/stable',
     unless  => 'snap list microk8s',
   }
-  -> file_line {'microk8s_ha_conf':
-    line => "failure-domain=${failure_domain}",
-    path => '/var/snap/microk8s/current/args/ha-conf'
+  -> file { '/etc/docker/daemon.json':
+    ensure => file,
+    source => template('sunetdrive/node/daemon.json.erb'),
+    mode   => '0644',
+  }
+  -> file { '/var/snap/microk8s/current/args/ha-conf':
+    ensure  => file,
+    content => "failure-domain=${failure_domain}\n",
+    mode    => '0660',
   }
   -> sunet::misc::ufw_allow { 'microk8s_ports':
-    from => '0.0.0.0/0',
+    from => 'any',
     port => [8080, 8443, 16443, 10250, 10255, 25000, 12379, 10257, 10259, 19001],
+  }
+  # This is how ufw::allow does it, but that lacks support for "on"
+  -> exec { 'allow-outgoing-on-calico':
+    path     => '/usr/sbin:/bin:/usr/bin',
+    unless   => 'ufw status | grep -qE "Anywhere (\(v6\) |)on vxlan.calico"',
+    provider => 'posix',
+    command  => 'ufw allow in on vxlan.calico',
+  }
+  -> exec { 'allow-incomming-on-calico':
+    path     => '/usr/sbin:/bin:/usr/bin',
+    unless   => 'ufw status | grep -qE "ALLOW OUT   Anywhere (\(v6\) |)on vxlan.calico"',
+    provider => 'posix',
+    command  => 'ufw allow in on vxlan.calico',
+  }
+  -> exec { 'iptables-allow-forward':
+    path     => '/usr/sbin:/bin:/usr/bin',
+    unless   => 'iptables -L FORWARD | grep -q "Chain FORWARD (policy ACCEPT)"',
+    provider => 'posix',
+    command  => 'iptables -P FORWARD ACCEPT',
   }
 }
