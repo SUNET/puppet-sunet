79e87c7c540ca839849a33c755c54008a7e1a151
Fredrik Thulin <fredrik@thulin.net>
2022-03-07 14:42:55 +0100

    setup files needed for dockerhost running nftables

--- manifests/dockerhost.pp
+++ manifests/dockerhost.pp
@@ -302,4 +302,19 @@ class sunet::dockerhost(
         ;
     }
   }
+
+  if $::sunet_nftables_opt_in == 'yes' or ( $::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemrelease, '22.04') >= 0 ) {
+    file {
+      '/etc/nftables/conf.d/200-dockerhost_nftables.nft':
+        ensure  => file,
+        mode    => '0400',
+        content => template('sunet/dockerhost/200-dockerhost_nftables.nft.erb'),
+        ;
+      '/etc/systemd/system/docker.service.d/docker_nftables_ns.conf':
+        ensure  => file,
+        mode    => '0400',
+        content => template('sunet/dockerhost/systemd_dropin_nftables_ns.conf.erb'),
+        ;
+    }
+  }
 }
