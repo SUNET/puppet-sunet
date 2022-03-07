5ebfee6ef162473f12c421fd4c2af910b502b514
Fredrik Thulin <fredrik@thulin.net>
2022-03-07 18:51:44 +0100

    add default logging

--- manifests/nftables.pp
+++ manifests/nftables.pp
@@ -1,4 +1,6 @@
-class sunet::nftables {
+class sunet::nftables(
+  Boolean $default_log = true,
+) {
     package { 'nftables':
         ensure => 'present',
     }
@@ -14,6 +16,15 @@ class sunet::nftables {
         mode   => '0644',
         source => 'puppet:///modules/sunet/nftables/nftables-conf.d-empty',
         notify => Service['nftables'],
+    }
+
+    if ($default_log) {
+      file { '/etc/nftables/conf.d/999-log.nft':
+        ensure => 'present',
+        mode   => '0755',
+        source => 'puppet:///modules/sunet/nftables/999-log.nft',
+        notify => Service['nftables'],
+      }
     }
 
     file { '/etc/nftables.conf':
