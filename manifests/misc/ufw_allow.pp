e518fd425ff04b93856e753904b4c6e30545681f
Fredrik Thulin <fredrik@thulin.net>
2022-03-08 09:04:22 +0100

    move utility function

--- manifests/misc/ufw_allow.pp
+++ manifests/misc/ufw_allow.pp
@@ -47,19 +47,3 @@ define sunet::misc::ufw_allow(
     }
   }
 }
-
-function sunet::misc::format_nft_set(String $prefix, Variant[String, Integer, Array, Undef] $arg) >> Variant[String, Undef] {
-  if $arg =~ Array {
-    $arg ? {
-      [] => undef,
-      default => sprintf('%s { %s }', $prefix, $arg.join(', '))
-    }
-  } elsif $arg =~ String {
-    $arg ? {
-      'any' => undef,
-      default => [$prefix, $arg].join(' ')
-    }
-  } elsif $arg =~ Integer {
-    [$prefix, $arg].join(' ')
-  }
-}
