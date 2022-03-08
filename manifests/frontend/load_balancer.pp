4cbf1f6862f80f52ef0da8206982ee0f0c7b6086
Fredrik Thulin <fredrik@thulin.net>
2022-03-08 14:18:32 +0100

    remove obsolete first version of websites.pp

--- manifests/frontend/load_balancer.pp
+++ manifests/frontend/load_balancer.pp
@@ -20,76 +20,75 @@ class sunet::frontend::load_balancer(
     }
 
     if has_key($config['load_balancer'], 'websites') {
-      #
-      # Old style config
-      #
-      fail("Migrate your frontend config from 'websites' to 'websites2'")
-    }
-    if has_key($config['load_balancer'], 'websites2') {
-      #
-      # New style config
-      #
-      sunet::exabgp::config { 'exabgp_config': }
-      file { '/etc/bgp/monitor':
-        ensure  => file,
-        mode    => '0755',
-        content => template('sunet/frontend/websites2_monitor.py.erb'),
-        notify  => Sunet::Exabgp['load_balancer'],
-      }
-
-      configure_peers { 'peers': router_id => $router_id, peers => $config['load_balancer']['peers'] }
-
-      configure_websites2 { 'websites':
-        websites  => $config['load_balancer']['websites2'],
-        basedir   => $basedir,
-        confdir   => $confdir,
-        scriptdir => $scriptdir,
-      }
+      $websites = $config['load_balancer']['websites']
+    } elsif has_key($config['load_balancer'], 'websites2') {
+      # name used during migration
+      $websites = $config['load_balancer']['websites2']
+    } else {
+      fail('Load balancer config contains neither "websites" nor "websites2"')
     }
 
-    $exabgp_imagetag = has_key($config['load_balancer'], 'exabgp_imagetag') ? {
-      true  => $config['load_balancer']['exabgp_imagetag'],
-      false => 'latest',
-    }
-    sunet::exabgp { 'load_balancer':
-      docker_volumes => ["${basedir}/haproxy/scripts:${basedir}/haproxy/scripts:ro",
-                         '/opt/frontend/monitor:/opt/frontend/monitor:ro',
-                         '/dev/log:/dev/log',
-                         ],
-      version        => $exabgp_imagetag,
-    }
-
-    sunet::frontend::api::server { 'sunetfrontend':
-      basedir    => $apidir,
-      docker_tag => pick($config['load_balancer']['api_imagetag'], 'latest'),
+    #
+    # New style config
+    #
+    sunet::exabgp::config { 'exabgp_config': }
+    file { '/etc/bgp/monitor':
+      ensure  => file,
+      mode    => '0755',
+      content => template('sunet/frontend/websites2_monitor.py.erb'),
+      notify  => Sunet::Exabgp['load_balancer'],
     }
 
-    sunet::frontend::telegraf { 'frontend_telegraf':
-      docker_image          => pick($config['load_balancer']['telegraf_image'], 'docker.sunet.se/eduid/telegraf'),
-      docker_imagetag       => pick($config['load_balancer']['telegraf_imagetag'], 'stable'),
-      docker_volumes        => pick($config['load_balancer']['telegraf_volumes'], []),
-      forward_url           => $config['load_balancer']['telegraf_forward_url'],
-      statsd_listen_address => pick($::ipaddress_docker0, 'no-address-provided'),
-    }
+    configure_peers { 'peers': router_id => $router_id, peers => $config['load_balancer']['peers'] }
 
-    sunet::misc::ufw_allow { 'always-https-allow-http':
-      from => 'any',
-      port => '80'
+    configure_websites2 { 'websites':
+      websites  => $config['load_balancer']['websites2'],
+      basedir   => $basedir,
+      confdir   => $confdir,
+      scriptdir => $scriptdir,
     }
+  }
 
-    # Create snakeoil bundle as fallback certificate for haproxy
-    $snakeoil_key = '/etc/ssl/private/ssl-cert-snakeoil.key'
-    $snakeoil_cert = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
-    $snakeoil_bundle = '/etc/ssl/snakeoil_bundle.crt'
-    sunet::misc::certbundle { 'snakeoil_bundle':
-      bundle => ["cert=${snakeoil_cert}",
-                 "key=${snakeoil_key}",
-                 "out=${snakeoil_bundle}",
-                 ],
-      group  => 'ssl-cert',
-    }
-  } else {
-    fail('No/bad SUNET frontend load balancer config found in hiera')
+  $exabgp_imagetag = has_key($config['load_balancer'], 'exabgp_imagetag') ? {
+    true  => $config['load_balancer']['exabgp_imagetag'],
+    false => 'latest',
+  }
+  sunet::exabgp { 'load_balancer':
+    docker_volumes => ["${basedir}/haproxy/scripts:${basedir}/haproxy/scripts:ro",
+                        '/opt/frontend/monitor:/opt/frontend/monitor:ro',
+                        '/dev/log:/dev/log',
+                        ],
+    version        => $exabgp_imagetag,
+  }
+
+  sunet::frontend::api::server { 'sunetfrontend':
+    basedir    => $apidir,
+    docker_tag => pick($config['load_balancer']['api_imagetag'], 'latest'),
+  }
+
+  sunet::frontend::telegraf { 'frontend_telegraf':
+    docker_image          => pick($config['load_balancer']['telegraf_image'], 'docker.sunet.se/eduid/telegraf'),
+    docker_imagetag       => pick($config['load_balancer']['telegraf_imagetag'], 'stable'),
+    docker_volumes        => pick($config['load_balancer']['telegraf_volumes'], []),
+    forward_url           => $config['load_balancer']['telegraf_forward_url'],
+    statsd_listen_address => pick($::ipaddress_docker0, 'no-address-provided'),
+  }
+
+  sunet::misc::ufw_allow { 'always-https-allow-http':
+    from => 'any',
+    port => '80'
+  }
+
+  # Create snakeoil bundle as fallback certificate for haproxy
+  $snakeoil_key = '/etc/ssl/private/ssl-cert-snakeoil.key'
+  $snakeoil_cert = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+  $snakeoil_bundle = '/etc/ssl/snakeoil_bundle.crt'
+  sunet::misc::certbundle { 'snakeoil_bundle':
+    bundle => ["cert=${snakeoil_cert}",
+                "key=${snakeoil_key}",
+                "out=${snakeoil_bundle}",
+                ],
+    group  => 'ssl-cert',
   }
 }
 
