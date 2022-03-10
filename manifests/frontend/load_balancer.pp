512855e281792573753ab56ba8cdded48060ce1a
Fredrik Thulin <fredrik@thulin.net>
2022-03-10 10:48:42 +0100

    move ip_nonlocal_bind to docker-compose
    Neater there, and might even have to be there now that Docker is in a
    separate namespace.

--- manifests/frontend/load_balancer.pp
+++ manifests/frontend/load_balancer.pp
@@ -56,25 +56,8 @@ class sunet::frontend::load_balancer(
   }
 }
 
-define sysctl_ip_nonlocal_bind() {
-  # Allow haproxy to bind() to a service IP address even if it haven't actually
-  # been set up on an interface yet
-  augeas { 'frontend_ip_nonlocal_bind_config':
-    context => '/files/etc/sysctl.d/10-sunet-frontend-ip-non-local-bind.conf',
-    changes => [
-                'set net.ipv4.ip_nonlocal_bind 1',
-                'set net.ipv6.ip_nonlocal_bind 1',
-                ],
-    notify  => Exec['reload_sysctl_10-sunet-frontend-ip-non-local-bind.conf'],
-  }
-
-  exec { 'reload_sysctl_10-sunet-frontend-ip-non-local-bind.conf':
-    command     => '/sbin/sysctl -p /etc/sysctl.d/10-sunet-frontend-ip-non-local-bind.conf',
-    refreshonly => true,
-  }
-}
-
-define configure_websites($websites, $basedir, $confdir, $scriptdir)
+# Create a sunet::frontend::load_balancer::website resource for every website in the config
+define configure_websites(Hash[String, Hash] $websites, String $basedir, String $confdir, String $scriptdir)
 {
   each($websites) | $site, $config | {
     create_resources('sunet::frontend::load_balancer::website', {$site => {}}, {
