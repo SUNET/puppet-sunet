f58e65c290ecf1a1f91f78e4dbd0fc5f8cb8449d
Fredrik Thulin <fredrik@thulin.net>
2022-03-30 15:29:42 +0200

    reduce the size of the default bridge v6 network

--- manifests/dockerhost.pp
+++ manifests/dockerhost.pp
@@ -6,8 +6,8 @@ class sunet::dockerhost(
   $storage_driver                             = undef,
   $docker_extra_parameters                    = undef,
   Boolean $run_docker_cleanup                 = true,
-  Variant[String, Boolean] $docker_network    = hiera('dockerhost_docker_network', '172.18.0.0/22'),
-  String $docker_network_v6                   = hiera('dockerhost_docker_network_v6', 'fd0c::/16'),
+  Variant[String, Boolean] $docker_network    = hiera('dockerhost_docker_network', '172.18.0.0/22'),  # default bridge
+  String $docker_network_v6                   = hiera('dockerhost_docker_network_v6', 'fd0c:d0c::/64'),  # default bridge
   Variant[String, Array[String]] $docker_dns  = $::ipaddress_default,
   Boolean $ufw_allow_docker_dns               = true,
   Boolean $manage_dockerhost_unbound          = false,
