08ac805b5ba8c5da4f4dad8aa4e366638dd8d14f
Fredrik Thulin <fredrik@thulin.net>
2022-04-01 08:48:08 +0200

    avoid installing docker-ce keys on ubuntu 22.04 to not get complaints from apt update

--- manifests/dockerhost.pp
+++ manifests/dockerhost.pp
@@ -17,61 +17,64 @@ class sunet::dockerhost(
   Boolean $write_daemon_config                = false,
   Boolean $enable_ipv6                        = false,
 ) {
-
-  # Remove old versions, if installed
-  package { ['lxc-docker-1.6.2', 'lxc-docker'] :
-    ensure => 'purged',
-  }
-
-  file {'/etc/apt/sources.list.d/docker.list':
-    ensure => 'absent',
-  }
-
-  if $docker_package_name != 'docker-engine' {
-    # transisition to docker-ce
-    exec { 'remove_dpkg_arch_i386':
-      command => '/usr/bin/dpkg --remove-architecture i386',
-      onlyif  => '/usr/bin/dpkg --print-foreign-architectures | grep i386',
+  if versioncmp($::operatingsystemrelease, '22.04') <= 0 {
+    # Remove old versions, if installed
+    package { ['lxc-docker-1.6.2', 'lxc-docker'] :
+      ensure => 'purged',
     }
 
-    package {'docker-engine': ensure => 'purged'}
-  }
-
-  # Add the dockerproject repository, then force an apt-get update before
-  # trying to install the package. See https://tickets.puppetlabs.com/browse/MODULES-2190.
-  #
-  sunet::misc::create_dir { '/etc/cosmos/apt/keys': owner => 'root', group => 'root', mode => '0755'}
-  file {
-    '/etc/cosmos/apt/keys/docker_ce-8D81803C0EBFCD88.pub':
-      ensure  => file,
-      mode    => '0644',
-      content => template('sunet/dockerhost/docker_ce-8D81803C0EBFCD88.pub.erb'),
-      ;
+    file {'/etc/apt/sources.list.d/docker.list':
+      ensure => 'absent',
     }
-  apt::key { 'docker_ce':
-    id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
-    source => '/etc/cosmos/apt/keys/docker_ce-8D81803C0EBFCD88.pub',
-  }
 
-  if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease == '14.04' {
-    $architecture = 'amd64'
-  } else {
-    $architecture = undef
-  }
+    if $docker_package_name != 'docker-engine' and $docker_package_name != 'docker.io' {
+      # transisition to docker-ce
+      exec { 'remove_dpkg_arch_i386':
+        command => '/usr/bin/dpkg --remove-architecture i386',
+        onlyif  => '/usr/bin/dpkg --print-foreign-architectures | grep i386',
+      }
 
-  # new source
-  apt::source {'docker_ce':
-    location     => 'https://download.docker.com/linux/ubuntu',
-    release      => $::lsbdistcodename,
-    repos        => $docker_repo,
-    key          => {'id' => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88'},
-    architecture => $architecture,
+      package {'docker-engine': ensure => 'purged'}
+    }
+
+    # Add the dockerproject repository, then force an apt-get update before
+    # trying to install the package. See https://tickets.puppetlabs.com/browse/MODULES-2190.
+    #
+    sunet::misc::create_dir { '/etc/cosmos/apt/keys': owner => 'root', group => 'root', mode => '0755'}
+    file {
+      '/etc/cosmos/apt/keys/docker_ce-8D81803C0EBFCD88.pub':
+        ensure  => file,
+        mode    => '0644',
+        content => template('sunet/dockerhost/docker_ce-8D81803C0EBFCD88.pub.erb'),
+        ;
+      }
+    apt::key { 'docker_ce':
+      id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+      source => '/etc/cosmos/apt/keys/docker_ce-8D81803C0EBFCD88.pub',
+      notify => Exec['dockerhost_apt_get_update'],
+    }
+
+    if $::operatingsystem == 'Ubuntu' and $::operatingsystemrelease == '14.04' {
+      $architecture = 'amd64'
+    } else {
+      $architecture = undef
+    }
+
+    # new source
+    apt::source {'docker_ce':
+      location     => 'https://download.docker.com/linux/ubuntu',
+      release      => $::lsbdistcodename,
+      repos        => $docker_repo,
+      key          => {'id' => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88'},
+      architecture => $architecture,
+      notify       => Exec['dockerhost_apt_get_update'],
+    }
   }
 
   if $docker_version =~ /^\d.*/ {
     # if it looks like a version number (as opposed to 'latest', 'installed', ...)
     # then pin it so that automatic/manual dist-upgrades don't touch the docker package
-    apt::pin { 'docker-ce':
+    apt::pin { 'docker package':
       packages => $docker_package_name,
       version  => $docker_version,
       priority => 920,  # upgrade, but do not downgrade
@@ -82,8 +85,6 @@ class sunet::dockerhost(
   exec { 'dockerhost_apt_get_update':
     command     => '/usr/bin/apt-get update',
     cwd         => '/tmp',
-    require     => [Apt::Key['docker_ce']],
-    subscribe   => [Apt::Key['docker_ce']],
     refreshonly => true,
   }
 
