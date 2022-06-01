bcd83f176d86b5714a99e6ca05b59635155e8726
Fredrik Thulin <fredrik@thulin.net>
2022-06-01 18:48:31 +0200

    need array here too

--- manifests/nftables/docker_expose.pp
+++ manifests/nftables/docker_expose.pp
@@ -1,7 +1,7 @@
 # set up DNAT port forwarding of $port to Docker
 define sunet::nftables::docker_expose(
   Variant[String, Array[String]] $allow_clients, # Allow traffic 'from' this IP (or list of IP:s).
-  Variant[Integer, String] $port,  # Allow traffic to this port (or list of ports).
+  Variant[Integer, String, Array] $port,  # Allow traffic to this port (or list of ports).
   Enum['tcp', 'udp'] $proto = 'tcp',
   String $docker_v4 = '172.16.0.2',
   String $docker_v6 = 'fd00::2',
