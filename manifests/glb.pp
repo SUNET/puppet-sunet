050edd37b101f73524f0f90e9bf3d6417afbfc47
Joao Paulo Oliveira de Araujo Rangel Pamplona <paulo@nordu.net>
2022-09-23 17:45:51 +0200

    updated to use latest geodns databases

--- manifests/glb.pp
+++ manifests/glb.pp
@@ -7,10 +7,35 @@ class sunet::glb(
    file { '/etc/geodns':
       ensure => 'directory'
    }
+
+   file { '/etc/GeoLiteFiles':
+      ensure => 'directory'
+   }
+
+   $geodns_key = safe_hiera('geodns_license',undef)
+
+   if $geodns_key != 'NOT_SET_IN_HIERA' {
+
+      exec { 'fetch_geodns_db':
+        command => "curl -s -o /etc/GeoLiteFiles/GeoLite2-Country.tar.gz \"https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&suffix=tar.gz&license_key=${geodns_key}\" && tar xvzf /etc/GeoLiteFiles/GeoLite2-Country.tar.gz -C /etc/GeoLiteFiles/ --wildcards '*.mmdb' | ln -fs $(xargs echo) /etc/GeoLiteFiles/GeoLite2-Country.mmdb",
+        onlyif  => "test ! -f /etc/GeoLiteFiles/GeoLite2-Country.mmdb"
+      }
+
+
+      sunet::scriptherder::cronjob { "refresh_geodns":
+           cmd               => "curl -s -o /etc/GeoLiteFiles/GeoLite2-Country.tar.gz \"https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&suffix=tar.gz&license_key=${geodns_key}\" && tar xvzf /etc/GeoLiteFiles/GeoLite2-Country.tar.gz -C /etc/GeoLiteFiles/ --wildcards '*.mmdb' | ln -fs $(xargs echo) /etc/GeoLiteFiles/GeoLite2-Country.mmdb",
+           weekday           => '1',
+           hour              => '6',
+           minute            => '1',
+           ok_criteria       => ['exit_status=0'],
+           warn_criteria     => ['max_age=30m']
+      }
+   }
+
    sunet::docker_run { 'geodns':
       image    => 'docker.sunet.se/geodns',
       imagetag  => $geodns_version,
-      volumes  => ['/etc/geodns:/etc/geodns'],
+      volumes  => ['/etc/geodns:/etc/geodns','/etc/GeoLiteFiles:/usr/share/GeoIP'],
       ports    => ["${::ipaddress_default}:53:5353/udp","${::ipaddress_default}:53:5353"],
       command  => "-port 5353"
    }
