91d52daf6d079d526b43c9b09a3c0e37c450b929
Micke Nordin <kano@sunet.se>
2022-09-30 16:39:10 +0200

    microk8s: bridge eyaml and kubernetes
    This patch makes it possible to add secrets with
    ./edit-secrets <fqdn of one of the kubernetes servers>
    
    The format of the secret MUST be in this format:
    
    microk8s_secrets:
      <namespace>:
        <name>:
          key: <key>
          value: DEC(1)::PKCS7[<value>]!
    
    That is, look something like this:
    microk8s_secrets:
      argocd:
        kanotestsecret:
          key: kanotestar
          value: DEC(1)::PKCS7[hemlis]!
    
    It is possible to specify multiple secrets for multiple namespaces like so:
    
    microk8s_secrets:
      namespace1:
        secret1:
          key: kanotestar
          value: DEC(1)::PKCS7[hemlis]!
        secret2:
          key: kanotestarigen
          value: DEC(1)::PKCS7[hemlis2]!
      namespace2:
        secret1:
          key: kanotestar
          value: DEC(1)::PKCS7[hemlis]!
        secret2:
          key: kanotestarigen
          value: DEC(1)::PKCS7[hemlis2]!
    
    Like anything in puppet, secrets removed from local.eyaml will not be
    automatically removed, and will need to be deleted with:
    
    kubectl delete secret ...

---
+++ lib/puppet/parser/functions/set_microk8s_secret.rb
@@ -0,0 +1,20 @@
+
+module Puppet::Parser::Functions
+  newfunction(:set_microk8s_secret, :type => :rvalue) do |args|
+
+    if args.size != 4
+      err('Invalid use of function set_microk8s_secret')
+    end
+
+    namespace = args[0]
+    name = args[1]
+    key = args[2]
+    value = args[3]
+
+    command  = "microk8s kubectl -n #{namespace} create secret generic #{name} " +
+    "--from-literal=#{key}=#{value} --dry-run=client -o yaml " +
+    '| microk8s kubectl apply -f -'
+    result = %x[ #{command} ]
+    return result
+  end
+end
