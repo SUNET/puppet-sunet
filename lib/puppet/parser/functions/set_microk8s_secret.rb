4a5f51bba6467f9cc5d2a434f4cf4d425a904a8a
Micke Nordin <kano@sunet.se>
2022-10-03 18:38:49 +0200

    Fix syntax

--- lib/puppet/parser/functions/set_microk8s_secret.rb
+++ lib/puppet/parser/functions/set_microk8s_secret.rb
@@ -1,7 +1,5 @@
-
 module Puppet::Parser::Functions
   newfunction(:set_microk8s_secret, :type => :rvalue) do |args|
-
     if args.size != 3
       err('Invalid use of function set_microk8s_secret')
     end
@@ -10,13 +8,13 @@ module Puppet::Parser::Functions
     name = args[1]
     secret = args[2]
 
-    command  = "microk8s kubectl -n #{namespace} create secret generic #{name} " +
-    secret.each do | key, value | {
+    command  = "microk8s kubectl -n #{namespace} create secret generic #{name} "
+    secret.each do |key, value|
       command += "--from-literal=\"#{key}\"=\"#{value}\" "
-    }
+    end
     command += "--dry-run=client -o yaml " +
-    '| microk8s kubectl apply -f -'
-    result = %x[ #{command} ]
+               '| microk8s kubectl apply -f -'
+    result = %x[#{command}]
     return result
   end
 end
