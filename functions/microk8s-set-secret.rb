dc74cf1742a137f6839f0ad231fd5f809c3bb7b9
Micke Nordin <kano@sunet.se>
2022-10-03 09:52:51 +0200

    Microk8s secrets: use ruby function

---
+++ functions/microk8s-set-secret.rb
@@ -0,0 +1,15 @@
+Puppet::Functions.create_function(:'microk8s::node::set_secret') do
+  dispatch :set_secret do
+    param 'String', :namespace
+    param 'String', :name
+    param 'String', :key
+    param 'String', :value
+  end
+
+  def set_secret(namespace, name, key, value)
+    command  = "microk8s kubectl -n #{namespace} create secret generic #{name}" +
+    "--from-literal=#{key}=#{value} --dry-run=client -o yaml " +
+    '| microk8s kubectl apply -f -'
+    result = %x[ #{command} ]
+  end
+end
