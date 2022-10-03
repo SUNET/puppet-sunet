e9bec5d658da2fbcba630b2b0217d280e867ed11
Micke Nordin <kano@sunet.se>
2022-10-03 12:10:53 +0200

    Same

--- functions/microk8s/set_secret.rb
+++ functions/microk8s/set_secret.rb
@@ -1,10 +1,10 @@
 Puppet::Functions.create_function(:'sunet::microk8s::set_secret') do
-  dispatch :set_secret do
-    param 'String', :namespace
-    param 'String', :name
-    param 'String', :key
-    param 'String', :value
-  end
+#  dispatch :set_secret do
+#    param 'String', :namespace
+#    param 'String', :name
+#    param 'String', :key
+#    param 'String', :value
+#  end
 
   def set_secret(namespace, name, key, value)
     command  = "microk8s kubectl -n #{namespace} create secret generic #{name}" +
