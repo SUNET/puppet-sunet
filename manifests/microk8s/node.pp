7c27262e818062f7259a0cc0cec10e82cce2e3ec
Micke Nordin <kano@sunet.se>
2022-10-03 14:51:45 +0200

    Correct name for function

--- manifests/microk8s/node.pp
+++ manifests/microk8s/node.pp
@@ -89,7 +89,7 @@ class sunet::microk8s::node(
   $namespaces = hiera_hash('microk8s_secrets', {})
   $namespaces.each |String $namespace, Hash $secrets| {
       $secrets.each |String $name, Hash $secret| {
-      if has_value($secret, 'key') and has_value($secret, 'value'){
+      if has_key($secret, 'key') and has_key($secret, 'value'){
         $key = $secret['key']
         $value = $secret['value']
         set_microk8s_secret($namespace, $name, $key, $value)
