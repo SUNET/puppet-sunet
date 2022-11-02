df5bafa5545cbb535560456bb2d57c7cdf564054
Micke Nordin <kano@sunet.se>
2022-11-02 17:08:23 +0100

    A simple redis cluster

---
+++ manifests/rediscluster.pp
@@ -0,0 +1,34 @@
+# A cluster class
+class sunet::rediscluster(
+  Integer $numnodes = 3,
+)
+{
+  include stdlib
+
+  docker_compose { 'rediscluster_compose':
+    content          => template('rediscluster/docker-compose.yml.erb'),
+    service_name     => 'redis',
+    compose_dir      => '/opt/',
+    compose_filename => 'docker-compose.yml',
+    description      => 'Redis Cluster',
+  }
+
+  range(0, $numnodes - 1).each |$i|{
+    $portnum = 7000 + $i
+
+    file { "/opt/redis/node-${i}":
+      ensure  => directory,
+    }
+    -> file { "/opt/redis/node-${i}/data":
+      ensure  => directory,
+    }
+    -> file { "/opt/redis/node-${i}/data/server.conf":
+      ensure  => present,
+      content => template('rediscluster/server.conf.erb'),
+    }
+    -> sunet::misc::ufw_allow { "redis_port_${portnum}":
+      from => '0.0.0.0/0',
+      port => $portnum,
+    }
+  }
+}
