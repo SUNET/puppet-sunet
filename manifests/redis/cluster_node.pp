b1d4adcb8560f994246895c8c0e3477d42bef779
Fredrik Thulin <fredrik@thulin.net>
2022-11-15 23:11:09 +0100

    new redis with server and sentinel in a compose

---
+++ manifests/redis/cluster_node.pp
@@ -0,0 +1,73 @@
+# Set up and run Redis in a server+sentinel configuration using Docker compose.
+define sunet::redis::cluster_node (
+  String           $public_ip       = $::facts['ipaddress_default'],
+  Array            $allow_clients   = [],
+  Array            $cluster_nodes   = [],
+  String           $master_ip       = pick($cluster_nodes[0], $::facts['ipaddress_default']),
+  Optional[String] $docker_image    = undef,
+  String           $docker_tag      = 'latest',
+  String           $basedir         = "/opt/redis/${name}"
+) {
+  $username = 'redis'
+  $group = 'redis'
+
+  ensure_resource('sunet::system_user', $username, {
+      username => $username,
+      group    => $group,
+  })
+
+  file {
+    $basedir:
+      ensure  => 'directory',
+      mode    => '0750',
+      group   => $group,
+      require => Sunet::System_user[$username],
+      ;
+    "${basedir}/etc":
+      ensure  => 'directory',
+      mode    => '0750',
+      group   => $group,
+      require => Sunet::System_user[$username],
+      ;
+    "${basedir}/data":
+      ensure  => 'directory',
+      mode    => '0770',
+      group   => $group,
+      require => Sunet::System_user[$username],
+      ;
+  }
+
+  # Write a configuration file for the redis-server
+  sunet::redis::config { 'server':
+    filename      => "${basedir}/etc/redis-server.conf",
+    cluster_nodes => $cluster_nodes,
+  }
+
+  # Write a configuration file for the redis-sentinel
+  sunet::redis::config { 'sentinel':
+    filename        => "${basedir}/etc/redis-sentinel.conf",
+    cluster_nodes   => $cluster_nodes,
+    sentinel_config => 'yes',
+  }
+
+  if $docker_image {
+    sunet::docker_compose { $name:
+      service_name => 'redis',
+      description  => 'Redis database',
+      content      => template('sunet/redis/docker-compose_cluster_node.yml.erb'),
+    }
+
+    if $::facts['sunet_nftables_opt_in'] == 'yes' or ( $::facts['operatingsystem'] == 'Ubuntu' and
+    versioncmp($::facts['operatingsystemrelease'], '22.04') >= 0 ) {
+      sunet::nftables::docker_expose { 'redis-server' :
+        allow_clients => $allow_clients,
+        port          => 6379,
+      }
+
+      sunet::nftables::docker_expose { 'redis-sentinel' :
+        allow_clients => $cluster_nodes,
+        port          => 26379,
+      }
+    }
+  }
+}
---
+++ manifests/redis/config.pp
@@ -0,0 +1,43 @@
+# Write a Redis configuration file.
+define sunet::redis::config (
+  String            $filename,
+  String            $public_ip       = $::facts['ipaddress_default'],
+  Integer           $port            = 6379,
+  String            $bind            = '0.0.0.0',    # need specific IP, otherwise redis 3.2+ will deny clients
+  Enum['yes', 'no'] $protected_mode  = 'yes',
+  String            $username        = 'redis',
+  String            $group           = 'redis',
+  Array             $cluster_nodes   = [],
+  Enum['yes', 'no'] $cluster_config  = 'no',
+  String            $cluster_name    = 'redis-cluster',
+  Enum['yes', 'no'] $sentinel_config = 'no',
+  String            $master_ip       = pick($cluster_nodes[0], $::facts['ipaddress_default']),
+  Integer           $master_port     = 6379,
+) {
+  # Configure all nodes as replicaof $master_ip, unless this node actually has $master_ip as $public_ip
+  $replica_node = $master_ip ? {
+    $public_ip => 'no',
+    default    => 'yes',
+  }
+
+  if $replica_node == 'yes' {
+    notice("Configuring Redis node replicaof ${master_ip} port ${master_port}")
+  } else {
+    notice("Configuring as Redis master with IP ${master_ip} as public_ip")
+  }
+
+  if $sentinel_config == 'yes' {
+    notice("Configuring Redis Sentinel to monitor ${master_ip} port ${master_port}")
+  }
+
+  file {
+    $filename:
+      ensure  => 'file',
+      mode    => '0640',
+      group   => $group,
+      content => template('sunet/redis/cluster_node.conf.erb'),
+      require => Sunet::System_user[$username],
+      force   => true,
+      ;
+  }
+}
