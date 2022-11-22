2084c234a11fb347a5e32c81f18a3f56d690f9d8
Johan Wassberg <jocar@sunet.se>
2022-11-22 09:40:06 +0100

    Forgot to add file extension

--- manifests/cgit.pp
+++ manifests/cgit.pp
@@ -62,7 +62,7 @@ class sunet::cgit(
     exec { 'enable 010-cgit':
         command => 'a2ensite 010-cgit',
         creates => '/etc/apache2/sites-enabled/010-cgit.conf',
-        onlyif  => "test -s /etc/ssl/certs/$fqdn",
+        onlyif  => "test -s /etc/ssl/certs/$fqdn.crt",
         notify  => Service['apache2'],
     }
 
