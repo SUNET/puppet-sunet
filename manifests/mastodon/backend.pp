8a84204aaf08981efbd37a59646e2fc16a1a0e42
Micke Nordin <kano@sunet.se>
2023-01-11 10:32:59 +0100

    Try to not clobber permissions on files for redis

--- manifests/mastodon/backend.pp
+++ manifests/mastodon/backend.pp
@@ -7,7 +7,6 @@ class sunet::mastodon::backend(
   $db_pass=safe_hiera('db_pass')
   $redis_pass=safe_hiera('redis_pass')
 
-  user { 'redis': ensure => present, system => true }
 
   # Composefile
   sunet::docker_compose { 'mastodon_backend':
@@ -19,9 +18,6 @@ class sunet::mastodon::backend(
   }
   file { "/opt/mastodon_backend/redis":
     ensure => directory,
-    owner  => 'redis',
-    group  => 'redis',
-    mode   => '0751',
   }
 
   file { "/opt/mastodon_backend/postgres":
@@ -32,9 +28,6 @@ class sunet::mastodon::backend(
   }
   file { '/opt/mastodon_backend/redis/server.conf':
     ensure  => file,
-    owner   => 'redis',
-    group   => 'redis',
-    mode    => '0640',
     content => template('sunet/mastodon/backend/redis.conf.erb'),
   }
   sunet::misc::ufw_allow { 'backend_ports':
