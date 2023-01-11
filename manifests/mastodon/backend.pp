c28dfa0f8e45331cf28e566c338198340ec51d74
Johan Wassberg <jocar@sunet.se>
2023-01-11 10:08:49 +0100

    Redis is cranky on permissons

--- manifests/mastodon/backend.pp
+++ manifests/mastodon/backend.pp
@@ -7,6 +7,8 @@ class sunet::mastodon::backend(
   $db_pass=safe_hiera('db_pass')
   $redis_pass=safe_hiera('redis_pass')
 
+  user { 'redis': ensure => present, system => true }
+
   # Composefile
   sunet::docker_compose { 'mastodon_backend':
     content          => template('sunet/mastodon/backend/docker-compose.yml.erb'),
@@ -15,19 +17,23 @@ class sunet::mastodon::backend(
     compose_filename => 'docker-compose.yml',
     description      => 'Mastodon backend services',
   }
-  $tl_dirs = ['postgres', 'redis']
-  $tl_dirs.each | $dir| {
-    file { "/opt/mastodon_backend/${dir}":
-      ensure => directory,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0751',
-    }
+  file { "/opt/mastodon_backend/redis":
+    ensure => directory,
+    owner  => 'redis',
+    group  => 'redis',
+    mode   => '0751',
+  }
+
+  file { "/opt/mastodon_backend/postgres":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0751',
   }
   file { '/opt/mastodon_backend/redis/server.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
+    owner   => 'redis',
+    group   => 'redis',
     mode    => '0640',
     content => template('sunet/mastodon/backend/redis.conf.erb'),
   }
