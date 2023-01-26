b458fba5d5bbebbeed83c6c5ec9dbeb3d428ca32
Micke Nordin <kano@sunet.se>
2023-01-26 15:30:02 +0100

    Docker host packages: Don't step on any toes
    jq and python3-yaml will be installed in a number of manifests and we
    don't want to crash them

--- manifests/dockerhost.pp
+++ manifests/dockerhost.pp
@@ -112,11 +112,13 @@ class sunet::dockerhost(
     }
   }
 
-  package { [
-    'python3-yaml',  # check_docker_containers requirement
-    'jq',            # restart_unhealthy_containers requirement
-  ] :
+  package { 'dockerhost_pyyaml' :
     ensure => 'installed',
+    name   => 'python3-yaml',  # check_docker_containers requirement
+  }
+  package { 'dockerhost_jq' :
+    ensure => 'installed',
+    name   => 'jq',            # restart_unhealthy_containers requirement
   }
 
   $docker_command = $docker_package_name ? {
