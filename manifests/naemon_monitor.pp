fae5dd0822319c388dfebbe9285672aefb7b8be4
Johan Wassberg <jocar@sunet.se>
2023-02-02 08:39:39 +0100

    Merge from legacy branch sunet

--- manifests/naemon_monitor.pp
+++ manifests/naemon_monitor.pp
@@ -13,7 +13,9 @@ class sunet::naemon_monitor(
   String $nagflux_tag = 'latest',
   String $grafana_tag = '9.1.6',
   Hash $manual_hosts = {},
+  Hash $additional_entities = {},
   String $nrpe_group = 'nrpe',
+  Optional[String] $default_host_group = undef,
 ){
 
   require stdlib
@@ -199,18 +201,11 @@ class sunet::naemon_monitor(
     require => File['/etc/naemon/conf.d/cosmos/'],
   }
 
-  $tmp_hostgroups =  $::roles + $manual_hosts
-  $all_hosts = flatten(map($tmp_hostgroups) |$hgn, $members| {
-    $groups = map($tmp_hostgroups[$hgn]) |$member| {
-      "$member"
-    }
-  })
-  $hostgroups_with_new_all = { 'all' => unique($all_hosts) }
-  $hostgroups  = $tmp_hostgroups + $hostgroups_with_new_all
-
   class { 'nagioscfg':
-    hostgroups     => $hostgroups,
+    hostgroups     => $::roles,
+    additional_entities => $additional_entities,
     config         => 'naemon_monitor',
+    default_host_group => $default_host_group,
     manage_package => false,
     manage_service => false,
     cfgdir         => '/etc/naemon/conf.d/nagioscfg',
