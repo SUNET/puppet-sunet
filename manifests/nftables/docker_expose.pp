b92fe17cfbab0cd25bf2c273b62cde34fdfc0a33
Fredrik Thulin <fredrik@thulin.net>
2023-02-08 23:30:26 +0100

    parameterise destination address of DNAT
    For DNAT to v6 enabled containers, the IP address of the container has
    to be provided. Docker won't set up forwarding (well, maybe with the
    userland proxy which 1) masks the source address and 2) seems to be out
    of fashion) for IPv6 the way it does with IPv4 :/

--- manifests/nftables/docker_expose.pp
+++ manifests/nftables/docker_expose.pp
@@ -1,11 +1,16 @@
 # set up DNAT port forwarding of $port to Docker
-define sunet::nftables::docker_expose(
+# @param allow_clients  Clients to allow traffic from. List of IP addresses or CIDR blocks.
+# @param port           Port to allow traffic to.
+# @param proto          Protocol to allow traffic for.
+# @param dnat_v4_addr   IPv4 address of Docker bridge (the IP address of docker on the to_docker veth pair).
+# @param dnat_v6_addr   IPv6 address to use for DNAT. Docker won't set up DNAT for IPv6, so provide the containers v6 addr here.
+define sunet::nftables::docker_expose (
   Variant[String, Array[String]] $allow_clients, # Allow traffic 'from' this IP (or list of IP:s).
   Variant[Integer, String] $port,  # Allow traffic to this port (or list of ports).
   Enum['tcp', 'udp'] $proto = 'tcp',
-  String $docker_v4 = '172.16.0.2',
-  String $docker_v6 = 'fd00::2',
   String $iif = 'eth0',
+  String $dnat_v4_addr = '172.16.0.2',
+  String $dnat_v6_addr = 'fd00::2',
 ) {
   $safe_name = regsubst($title, '[^0-9A-Za-z_]', '_', 'G')
 
