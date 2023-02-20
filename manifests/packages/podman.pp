d1af1f1dcded4083573a9ba5921541b7e1bbbe62
Micke Nordin <kano@sunet.se>
2023-02-20 16:36:38 +0100

    PODMAN: First stab at packaging
    We use the pattern of sunet::packages:: to install:
    * curl
    * gpg
    * pip
    * podman
    * podman-compose
    
    In a way which allows others to do the same without conflicts.
    
    We also add the following classes which are analogous to their docker* siblings:
    * podmanhost
    * podman_compose
    * podman_compose_service

---
+++ manifests/packages/podman.pp
@@ -0,0 +1,31 @@
+# Install podman from kubic
+class sunet::packages::podman(
+) {
+  include sunet::packages::curl
+  include sunet::packages::gpg
+  include stdlib
+
+  if $::operatingsystem == 'Ubuntu' {
+    $reponame = 'xUbuntu'
+  } else {
+    $reponame = $::operatingsystem
+  }
+  exec { 'podman_repo_key':
+    creates => '/etc/apt/trusted.gpg.d/libcontainers.gpg',
+    command => "curl https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/${reponame}_${::operatingsystemrelease}/Release.key | gpg --dearmor > /etc/apt/trusted.gpg.d/libcontainers.gpg",
+    unless  => 'test -f /etc/apt/trusted.gpg.d/libcontainers.gpg',
+  }
+  -> file { 'podman_repo_file':
+    ensure  => file,
+    name    => '/etc/apt/sources.list.d/libcontainers.list',
+    content => "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/${reponame}_${::operatingsystemrelease}/ /\n",
+    unless  => 'test -f /etc/apt/sources.list.d/libcontainers.list',
+  }
+  -> exec { 'podman_update':
+    command => 'apt update',
+    unless  => 'dpkg -l podman'
+  }
+  -> package { 'podman':
+    ensure => latest
+  }
+}
