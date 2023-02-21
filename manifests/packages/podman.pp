46ab8051efaaf09bcfdf27a4ac4689d66368e0da
Micke Nordin <kano@sunet.se>
2023-02-21 11:15:49 +0100

    Use operatingsystemmajrelease

--- manifests/packages/podman.pp
+++ manifests/packages/podman.pp
@@ -12,13 +12,13 @@ class sunet::packages::podman(
   }
   exec { 'podman_repo_key':
     creates => '/etc/apt/trusted.gpg.d/libcontainers.gpg',
-    command => "curl https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/${reponame}_${::operatingsystemrelease}/Release.key | gpg --dearmor > /etc/apt/trusted.gpg.d/libcontainers.gpg",
+    command => "curl https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/${reponame}_${::operatingsystemmajrelease}/Release.key | gpg --dearmor > /etc/apt/trusted.gpg.d/libcontainers.gpg",
     unless  => 'test -f /etc/apt/trusted.gpg.d/libcontainers.gpg',
   }
   -> file { 'podman_repo_file':
     ensure  => file,
     name    => '/etc/apt/sources.list.d/libcontainers.list',
-    content => "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/${reponame}_${::operatingsystemrelease}/ /\n",
+    content => "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/${reponame}_${::operatingsystemmajrelease}/ /\n",
   }
   -> exec { 'podman_update':
     command => 'apt update',
