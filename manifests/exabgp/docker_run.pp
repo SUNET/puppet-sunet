bd83d53ebf6c19a732b7551abcef94f6a1d2ea1a
Fredrik Thulin <fredrik@thulin.net>
2023-02-21 14:24:37 +0100

    squash-merge 11 months of Sunet Frontend 2.0 development from the eduid_dev branch
    Noteworthy changes:
    
      - nothing runs as uid/gid 0
      - uses nftables
      - working IPv6 connectivity to _and_ from haproxy containers
      - update to Python 3 in various helper scripts
    - exabgp runs outside of Docker (because of TCP-MD5 and --net host not
        working in namespaced Docker)

--- manifests/exabgp/docker_run.pp
+++ manifests/exabgp/docker_run.pp
@@ -1,24 +1,17 @@
-# Old manifest for running exabgp. Look in manifests/exabgp/ instead.
-define sunet::exabgp(
+# Run exabgp in a standalone Docker container, using --net host
+define sunet::exabgp::docker_run(
   Array   $extra_arguments = [],
-  String  $config          = '/etc/bgp/exabgp.conf',
   String  $version         = 'latest',
   String  $volume          = '/etc/bgp',
+  String  $config          = '/etc/bgp/exabgp.conf',
   Array   $docker_volumes  = [],
 ) {
   $safe_title = regsubst($title, '[^0-9A-Za-z.\-]', '-', 'G');
-
-  sunet::exabgp::docker_run { $name :
+  sunet::docker_run {"${safe_title}_exabgp":
       image    => 'docker.sunet.se/sunet/docker-sunet-exabgp',
       imagetag => $version,
       volumes  => flatten(["${volume}:${volume}:ro", $docker_volumes]),
       net      => 'host',
       command  => join(flatten([$config, $extra_arguments]),' ')
-  }
-
-  sunet::snippets::no_icmp_redirects {"no_icmp_redirects_${safe_title}": }
-
-  sunet::exabgp::molly_guard { $name :
-    service_name => "docker-${safe_title}_exabgp",
   }
 }
---
+++ manifests/exabgp/package.pp
@@ -0,0 +1,24 @@
+# Install exabgp as a package
+define sunet::exabgp::package() {
+  ensure_resource('package', 'exabgp', {ensure => 'installed'})
+
+  # Make exabgp run as non-root
+  sunet::snippets::file_line { 'exabgp_service_user':
+    ensure   => 'uncomment',
+    filename => '/lib/systemd/system/exabgp.service',
+    line     => 'User=exabgp',
+    notify   => [Class['sunet::systemd_reload']],
+  }
+  sunet::snippets::file_line { 'exabgp_service_group':
+    ensure   => 'uncomment',
+    filename => '/lib/systemd/system/exabgp.service',
+    line     => 'Group=exabgp',
+    notify   => [Class['sunet::systemd_reload']],
+  }
+
+  service { 'exabgp':
+    ensure   => running,
+    provider => 'systemd',
+    enable   => true,
+  }
+}
