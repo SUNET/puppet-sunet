357c814543b30741cc57fb22869bfb6c131cc852
Fredrik Thulin <fredrik@thulin.net>
2023-02-23 16:18:41 +0100

    make sure the to_docker interface is set up before starting services

--- manifests/docker_run.pp
+++ manifests/docker_run.pp
@@ -24,64 +24,77 @@ define sunet::docker_run(
   Boolean $uid_gid_consistency = true,
   Boolean $fetch_docker_image  = true,
 ) {
-  if $use_unbound {
-    warning('docker-unbound is deprecated, container name resolution should continue to work using docker network with DNS')
-  }
-
-  if $uid_gid_consistency {
-    $_uid_gid = [
-      '/etc/passwd:/etc/passwd:ro',  # uid consistency
-      '/etc/group:/etc/group:ro',    # gid consistency
-    ]
-  } else { $_uid_gid = [] }
-
-  # Use start_on for docker::run $depends, unless the new argument 'depends' is given
-  $_depends = $depends ? {
-    []      => any2array($start_on),
-    default => flatten([$depends]),
-  }
-
-  # If a non-default network is used, make sure it is created before this container starts
-  $req = $net ? {
-    'host'   => [],
-    'bridge' => [],
-    default  => [Docker_network[$net]],
-  }
-
-  $image_tag = "${image}:${imagetag}"
-  if $fetch_docker_image {
-    docker::image { "${name}_${image_tag}" :  # make it possible to use the same docker image more than once on a node
-      ensure  => $ensure,
-      image   => $image_tag,
-      require => Class['sunet::dockerhost'],
+  if $::facts['sunet_nftables_enabled'] == 'yes' {
+    if ! has_key($::facts['networking']['interfaces'], 'to_docer') {
+      notice("sunet::docker_compose: No to_docker interface found, not installing ${name}")
+      $_install_service = false
+    } else {
+      $_install_service = true
     }
+  } else {
+    $_install_service = true
   }
 
-  docker::run { $name :
-    ensure                   => $ensure,
-    volumes                  => flatten([$volumes, $_uid_gid]),
-    hostname                 => $hostname,
-    ports                    => $ports,
-    expose                   => $expose,
-    env                      => $env,
-    net                      => $net,
-    command                  => $command,
-    dns                      => $dns,
-    depends                  => $_depends,
-    require                  => flatten([$req]),
-    before_start             => $before_start,
-    before_stop              => $before_stop,
-    after_start              => $after_start,
-    after_stop               => $after_stop,
-    docker_service           => true,  # the service 'docker' is maintainer by puppet, so depend on it
-    image                    => $image_tag,
-    extra_parameters         => flatten([$extra_parameters]),
-    extra_systemd_parameters => $extra_systemd_parameters,
-  }
+  if $_install_service {
+    if $use_unbound {
+      warning('docker-unbound is deprecated, container name resolution should continue to work using docker network with DNS')
+    }
 
-  # Remove the upstart file created by earlier versions of garethr-docker
-  exec { "remove_docker_upstart_job_${name}":
-    command => "/bin/rm /etc/init/docker-${name}.conf",
-    onlyif  => "/usr/bin/test -f /etc/init/docker-${name}.conf",
+    if $uid_gid_consistency {
+      $_uid_gid = [
+        '/etc/passwd:/etc/passwd:ro',  # uid consistency
+        '/etc/group:/etc/group:ro',    # gid consistency
+      ]
+    } else { $_uid_gid = [] }
+
+    # Use start_on for docker::run $depends, unless the new argument 'depends' is given
+    $_depends = $depends ? {
+      []      => any2array($start_on),
+      default => flatten([$depends]),
+    }
+
+    # If a non-default network is used, make sure it is created before this container starts
+    $req = $net ? {
+      'host'   => [],
+      'bridge' => [],
+      default  => [Docker_network[$net]],
+    }
+
+    $image_tag = "${image}:${imagetag}"
+    if $fetch_docker_image {
+      docker::image { "${name}_${image_tag}" :  # make it possible to use the same docker image more than once on a node
+        ensure  => $ensure,
+        image   => $image_tag,
+        require => Class['sunet::dockerhost'],
+      }
+    }
+
+    docker::run { $name :
+      ensure                   => $ensure,
+      volumes                  => flatten([$volumes, $_uid_gid]),
+      hostname                 => $hostname,
+      ports                    => $ports,
+      expose                   => $expose,
+      env                      => $env,
+      net                      => $net,
+      command                  => $command,
+      dns                      => $dns,
+      depends                  => $_depends,
+      require                  => flatten([$req]),
+      before_start             => $before_start,
+      before_stop              => $before_stop,
+      after_start              => $after_start,
+      after_stop               => $after_stop,
+      docker_service           => true,  # the service 'docker' is maintainer by puppet, so depend on it
+      image                    => $image_tag,
+      extra_parameters         => flatten([$extra_parameters]),
+      extra_systemd_parameters => $extra_systemd_parameters,
+    }
+
+    # Remove the upstart file created by earlier versions of garethr-docker
+    exec { "remove_docker_upstart_job_${name}":
+      command => "/bin/rm /etc/init/docker-${name}.conf",
+      onlyif  => "/usr/bin/test -f /etc/init/docker-${name}.conf",
+    }
   }
 }
