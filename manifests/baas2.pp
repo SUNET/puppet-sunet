ca14dd8397a757ec204f83ba02ddc15bffc60b27
Patrik Lundin <patlu@sunet.se>
2023-03-20 13:47:52 +0100

    Fix missing brace and sync whitespace

--- manifests/baas2.pp
+++ manifests/baas2.pp
@@ -24,9 +24,9 @@
 # @param tcpserveraddress  The address of the TSM server we are sending backup data to
 # @param version           The version of the client to install
 class sunet::baas2(
-   String $nodename="",
-   String $tcpserveraddress="tsm12.backup.sto2.safedc.net",
-   String $version="8.1.15.2",
+  String $nodename="",
+  String $tcpserveraddress="tsm12.backup.sto2.safedc.net",
+  String $version="8.1.15.2",
 ) {
 
   # MUST be set properly in hiera to continue
@@ -35,37 +35,38 @@ class sunet::baas2(
   if $nodename and $baas_password != 'NOT_SET_IN_HIERA' {
 
     file { "/usr/local/sbin/sunet-bootstrap-baas2":
-       ensure  => 'file',
-       mode    => '0755',
-       owner   => 'root',
-       content => file('sunet/baas2/sunet-bootstrap-baas2')
+      ensure  => 'file',
+      mode    => '0755',
+      owner   => 'root',
+      content => file('sunet/baas2/sunet-bootstrap-baas2')
     }
 
     # Make sure the requested version is installed
     exec { 'sunet-bootstrap-baas2 --install':
-        command => "/usr/local/sbin/sunet-bootstrap-baas2 --install --version=$version",
+      command => "/usr/local/sbin/sunet-bootstrap-baas2 --install --version=$version",
     }
 
     # Install the configuration files
     file { "/opt/tivoli/tsm/client/ba/bin/dsm.sys":
-       ensure  => "file",
-       content => template("sunet/baas2/dsm.sys.erb")
+      ensure  => "file",
+      content => template("sunet/baas2/dsm.sys.erb")
     }
     file { "/opt/tivoli/tsm/client/ba/bin/dsm.opt":
-       ensure  => "file",
-       content => template("sunet/baas2/dsm.opt.erb")
+      ensure  => "file",
+      content => template("sunet/baas2/dsm.opt.erb")
     }
 
     # Make sure the client is registered with the server
     exec { 'sunet-bootstrap-baas2 --register':
-        command => "/usr/local/sbin/sunet-bootstrap-baas2 --register",
-        environment => [ "SUNET_BAAS_PASSWORD=$baas_password" ],
-        require => File['/opt/tivoli/tsm/client/ba/bin/dsm.sys'],
+      command => "/usr/local/sbin/sunet-bootstrap-baas2 --register",
+      environment => [ "SUNET_BAAS_PASSWORD=$baas_password" ],
+      require => File['/opt/tivoli/tsm/client/ba/bin/dsm.sys'],
     }
 
     service { 'dsmcad':
-        ensure  => 'running',
-        enable  => true,
-        require => Exec['sunet-bootstrap-baas2 --register'],
+       ensure  => 'running',
+       enable  => true,
+       require => Exec['sunet-bootstrap-baas2 --register'],
     }
+  }
 }
