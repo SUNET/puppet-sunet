625599b62c0d7f813f1f06fa9ef98c01d26df11e
Johan Wassberg <jocar@sunet.se>
2023-04-26 20:17:10 +0200

    `puppet-lint`

--- manifests/satosa.pp
+++ manifests/satosa.pp
@@ -1,48 +1,49 @@
+# Class to run Satosa in docker-compose
 class sunet::satosa(
   $dehydrated_name=undef,
   $image='docker.sunet.se/satosa',
-  $interface = "${::facts['interface_default']}",
+  $interface=$::facts['interface_default'],
   $tag='8.0.1',
 ) {
-   $proxy_conf = hiera("satosa_proxy_conf")
-   $default_conf = {
-      "STATE_ENCRYPTION_KEY"       => hiera("satosa_state_encryption_key"),
-      "USER_ID_HASH_SALT"          => hiera("satosa_user_id_hash_salt"),
-      "CUSTOM_PLUGIN_MODULE_PATHS" => ["plugins"],
-      "COOKIE_STATE_NAME"          => "SATOSA_STATE"
-   }
-   $merged_conf = merge($proxy_conf,$default_conf)
-   ensure_resource('file','/etc', { ensure => directory } )
-   ensure_resource('file','/etc/satosa', { ensure => directory } )
-   ensure_resource('file',"/etc/satosa/", { ensure => directory } )
-   ensure_resource('file',"/etc/satosa/run", { ensure => directory } )
-   ensure_resource('file',"/etc/satosa/plugins", { ensure => directory } )
-   ensure_resource('file',"/etc/satosa/metadata", { ensure => directory } )
-   ["backend","frontend","metadata"].each |$id| {
-      if hiera("satosa_${id}_key",undef) != undef {
-         sunet::snippets::secret_file { "/etc/satosa/${id}.key": hiera_key => "satosa_${id}_key" }
-         # assume cert is in cosmos repo
-      } else {
-         # make key pair
-         sunet::snippets::keygen {"satosa_${id}":
-            key_file  => "/etc/satosa/${id}.key",
-            cert_file => "/etc/satosa/${id}.crt"
-         }
+  $proxy_conf = hiera('satosa_proxy_conf')
+  $default_conf = {
+    'STATE_ENCRYPTION_KEY'       => hiera('satosa_state_encryption_key'),
+    'USER_ID_HASH_SALT'          => hiera('satosa_user_id_hash_salt'),
+    'CUSTOM_PLUGIN_MODULE_PATHS' => ['plugins'],
+    'COOKIE_STATE_NAME'          => 'SATOSA_STATE'
+  }
+  $merged_conf = merge($proxy_conf,$default_conf)
+  ensure_resource('file','/etc', { ensure => directory } )
+  ensure_resource('file','/etc/satosa', { ensure => directory } )
+  ensure_resource('file','/etc/satosa/', { ensure => directory } )
+  ensure_resource('file','/etc/satosa/run', { ensure => directory } )
+  ensure_resource('file','/etc/satosa/plugins', { ensure => directory } )
+  ensure_resource('file','/etc/satosa/metadata', { ensure => directory } )
+  ['backend','frontend','metadata'].each |$id| {
+    if hiera("satosa_${id}_key",undef) != undef {
+      sunet::snippets::secret_file { "/etc/satosa/${id}.key": hiera_key => "satosa_${id}_key" }
+      # assume cert is in cosmos repo
+    } else {
+      # make key pair
+      sunet::snippets::keygen {"satosa_${id}":
+        key_file  => "/etc/satosa/${id}.key",
+        cert_file => "/etc/satosa/${id}.crt"
       }
-   }
-   file {"/etc/satosa/proxy_conf.yaml":
-      content => inline_template("<%= @merged_conf.to_yaml %>\n"),
+    }
+  }
+  file {'/etc/satosa/proxy_conf.yaml':
+    content => inline_template("<%= @merged_conf.to_yaml %>\n"),
+    notify  => Service['sunet-satosa'],
+  }
+  $plugins = hiera('satosa_config')
+  sort(keys($plugins)).each |$n| {
+    $conf = hiera($n)
+    $fn = $plugins[$n]
+    file { $fn:
+      content => inline_template("<%= @conf.to_yaml %>\n"),
       notify  => Service['sunet-satosa'],
-   }
-   $plugins = hiera("satosa_config")
-   sort(keys($plugins)).each |$n| {
-      $conf = hiera($n)
-      $fn = $plugins[$n]
-      file { "$fn":
-         content => inline_template("<%= @conf.to_yaml %>\n"),
-         notify  => Service['sunet-satosa'],
-      }
-   }
+    }
+  }
 
   if $::facts['sunet_nftables_enabled'] == 'yes' {
     sunet::nftables::docker_expose { 'allow_https' :
@@ -56,16 +57,16 @@ class sunet::satosa(
       port => '443'
     }
   }
-   $dehydrated_status = $dehydrated_name ? {
-      undef   => 'absent',
-      default => 'present'
-   }
-   sunet::docker_run {'alwayshttps':
-      image    => 'docker.sunet.se/always-https',
-      ports    => ['80:80'],
-      env      => ['ACME_URL=http://acme-c.sunet.se'],
-      ensure   => $dehydrated_status
-   }
+  $dehydrated_status = $dehydrated_name ? {
+    undef   => 'absent',
+    default => 'present'
+  }
+  sunet::docker_run {'alwayshttps':
+    ensure => $dehydrated_status
+    image  => 'docker.sunet.se/always-https',
+    ports  => ['80:80'],
+    env    => ['ACME_URL=http://acme-c.sunet.se'],
+  }
 
   if $::facts['sunet_nftables_enabled'] == 'yes' {
     sunet::nftables::docker_expose { 'allow_http' :
@@ -79,15 +80,15 @@ class sunet::satosa(
       port => '80'
     }
   }
-   if ($dehydrated_name) {
-      file { '/etc/satosa/https.key': ensure => link, target => "/etc/dehydrated/certs/$dehydrated_name.key" }
-      file { '/etc/satosa/https.crt': ensure => link, target => "/etc/dehydrated/certs/${dehydrated_name}/fullchain.pem" }
-   } else {
-      sunet::snippets::keygen {"satosa_https":
-         key_file  => "/etc/satosa/https.key",
-         cert_file => "/etc/satosa/https.crt"
-      }
-   }
+  if ($dehydrated_name) {
+    file { '/etc/satosa/https.key': ensure => link, target => "/etc/dehydrated/certs/${dehydrated_name}.key" }
+    file { '/etc/satosa/https.crt': ensure => link, target => "/etc/dehydrated/certs/${dehydrated_name}/fullchain.pem" }
+  } else {
+    sunet::snippets::keygen {'satosa_https':
+      key_file  => '/etc/satosa/https.key',
+      cert_file => '/etc/satosa/https.crt'
+    }
+  }
 
   service {'docker-satosa.service':
     ensure => 'stopped',
