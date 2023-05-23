e9b126e12a6f54cdd87a7e9124953c1c19775edd
Maria Haider <mariah@sunet.se>
2023-05-23 14:21:35 +0200

    Changed nrpe check for apache2

--- manifests/nagios/nrpe_check_apache2.pp
+++ manifests/nagios/nrpe_check_apache2.pp
@@ -1,5 +1,5 @@
-define sunet::nagios::nrpe_check_apache2 ($display_name = undef) {
+define sunet::nagios::nrpe_check_apache2 ($display_name = undef, $number_of_processes = '4',) {
    sunet::nagios::nrpe_command {"check_process_${title}":
-     command_line => "/usr/lib/nagios/plugins/check_procs -C $title -c 4:"
+     command_line => "/usr/lib/nagios/plugins/check_procs -C $title -c $number_of_processes:"
    }
 }
