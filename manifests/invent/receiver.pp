97e9f1d935ac56488b98bee823c17bd061103951
Micke Nordin <kano@sunet.se>
2023-05-25 15:38:16 +0200

    TYPO: reciever -> receiver, thanks @patlu for noticing

--- manifests/invent/receiver.pp
+++ manifests/invent/receiver.pp
@@ -1,5 +1,5 @@
 # Invent frontend server
-class sunet::invent::reciever (
+class sunet::invent::receiver (
   String $docker_tag = '0.0.1-1',
   String $vhost = 'invent.sunet.se'
 ){
@@ -8,31 +8,31 @@ class sunet::invent::reciever (
   $nginx_dirs = [ 'acme', 'certs','conf','dhparam','html','vhost' ]
 
   sunet::docker_compose { 'invent_reciver':
-    content          => template('sunet/invent/reciever/docker-compose.yml.erb'),
-    service_name     => 'reciever',
+    content          => template('sunet/invent/receiver/docker-compose.yml.erb'),
+    service_name     => 'receiver',
     compose_dir      => '/opt/',
     compose_filename => 'docker-compose.yml',
-    description      => 'Invent Reciever Server',
+    description      => 'Invent receiver Server',
   }
 
-  file { '/opt/reciever/db':
+  file { '/opt/receiver/db':
     ensure => directory
   }
   $endpoints.each |$endpoint| {
-    file { "/opt/reciever/${endpoint}":
+    file { "/opt/receiver/${endpoint}":
       ensure => directory
     }
   }
 
-  file { '/opt/reciever/nginx':
+  file { '/opt/receiver/nginx':
     ensure => directory
   }
   $nginx_dirs.each |$dir| {
-    file { "/opt/reciever/nginx/${dir}":
+    file { "/opt/receiver/nginx/${dir}":
       ensure => directory
     }
   }
-  sunet::misc::ufw_allow { 'reciever_ports':
+  sunet::misc::ufw_allow { 'receiver_ports':
     from => 'any',
     port => [80, 443],
   }
