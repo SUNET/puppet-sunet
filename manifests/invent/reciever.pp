a6f0b62f19a62b820bb32b4e3e05cbb2e0f6fc45
Micke Nordin <kano@sunet.se>
2023-05-25 15:05:56 +0200

    Add nginxproxy

--- manifests/invent/reciever.pp
+++ manifests/invent/reciever.pp
@@ -1,9 +1,11 @@
 # Invent frontend server
 class sunet::invent::reciever (
   String $docker_tag = '0.0.1-1',
+  String $vhost = 'invent.sunet.se'
 ){
 
   $endpoints = ['hosts', 'images']
+  $nginx_dirs = [ 'acme', 'certs','conf','dhparam','html','vhost' ]
 
   sunet::docker_compose { 'invent_reciver':
     content          => template('sunet/invent/reciever/docker-compose.yml.erb'),
@@ -16,10 +18,22 @@ class sunet::invent::reciever (
   file { '/opt/reciever/db':
     ensure => directory
   }
-
   $endpoints.each |$endpoint| {
     file { "/opt/reciever/${endpoint}":
       ensure => directory
     }
+  }
+
+  file { '/opt/reciever/nginx':
+    ensure => directory
+  }
+  $nginx_dirs.each |$dir| {
+    file { "/opt/reciever/nginx/${dir}":
+      ensure => directory
+    }
+  }
+  sunet::misc::ufw_allow { 'reciever_ports':
+    from => 'any',
+    port => [80, 443],
   }
 }
