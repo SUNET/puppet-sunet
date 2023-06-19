151ac79d345134e81df22318e1736ee593faef31
Micke Nordin <kano@sunet.se>
2023-06-19 15:02:14 +0200

    PODMANHOST: Add storage conf

--- manifests/podmanhost.pp
+++ manifests/podmanhost.pp
@@ -3,5 +3,12 @@ class sunet::podmanhost(
 ) {
   include sunet::packages::podman
   include stdlib
+  file { '/etc/containers/storage.conf':
+    ensure  => file,
+    content => template('sunet/podmanhost/storage.conf.erb'),
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+  }
 
 }
