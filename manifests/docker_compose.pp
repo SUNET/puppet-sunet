1a69facfc5bac280751307f28f3b6d06c86c8e0c
Micke Nordin <kano@sunet.se>
2023-06-26 12:06:43 +0200

    Install service even if there is no to_docker interface

--- manifests/docker_compose.pp
+++ manifests/docker_compose.pp
@@ -13,36 +13,23 @@ define sunet::docker_compose (
   String           $owner = 'root',
   Optional[String] $start_command = undef,
 ) {
-  if $::facts['sunet_nftables_enabled'] == 'yes' {
-    if ! has_key($::facts['networking']['interfaces'], 'to_docker') {
-      notice("sunet::docker_compose: No to_docker interface found, not installing ${service_name}")
-      $_install_service = false
-    } else {
-      $_install_service = true
-    }
-  } else {
-    $_install_service = true
-  }
+  $compose_file = "${compose_dir}/${service_name}/${compose_filename}"
 
-  if $_install_service {
-    $compose_file = "${compose_dir}/${service_name}/${compose_filename}"
+  # docker-compose uses dirname as project name, so we add $service_name and put the compose_file in there
+  ensure_resource('sunet::misc::create_dir', ["${compose_dir}/${service_name}"], { owner => $owner, group => $group, mode => $mode })
 
-    # docker-compose uses dirname as project name, so we add $service_name and put the compose_file in there
-    ensure_resource('sunet::misc::create_dir', ["${compose_dir}/${service_name}"], { owner => $owner, group => $group, mode => $mode })
+  ensure_resource('file', $compose_file, {
+      ensure  => 'file',
+      mode    => '600',
+      content => $content,
+      require => Class[$docker_host_class],
+  })
 
-    ensure_resource('file', $compose_file, {
-        ensure  => 'file',
-        mode    => '600',
-        content => $content,
-        require => Class[$docker_host_class],
-    })
-
-    sunet::docker_compose_service { "${service_prefix}-${service_name}":
-      compose_file   => $compose_file,
-      description    => $description,
-      require        => File[$compose_file],
-      service_extras => $service_extras,
-      start_command  => $start_command,
-    }
+  sunet::docker_compose_service { "${service_prefix}-${service_name}":
+    compose_file   => $compose_file,
+    description    => $description,
+    require        => File[$compose_file],
+    service_extras => $service_extras,
+    start_command  => $start_command,
   }
 }
