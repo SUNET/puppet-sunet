3e0b217a36edf60314b922f5a76644b5576a9ba5
Micke Nordin <kano@sunet.se>
2023-06-26 10:22:50 +0200

    DOCKERIOHOST: Add a much simpler dockerhost using the native package
    This class uses th docker.io and docker-compose packages from
    debian/ubuntu and does not take a bunch of options, and does not support
    any old distros

--- manifests/dockeriohost.pp
+++ manifests/dockeriohost.pp
@@ -9,17 +9,17 @@ class sunet::dockeriohost(
   package { 'docker-compose' :
     ensure  => latest,
   }
-  if ($::facts['docker_service_exists'] == 'yes') {
-    $service_to_notify = Service['docker']
-  } else {
-    $service_to_notify = undef
-  }
   if $::facts['sunet_nftables_enabled'] == 'yes' {
     # Hackishly create the /etc/systemd/system/docker.service.d/ directory before the docker service is installed.
     # If we do this using 'file', the docker class will fail because of a duplicate declaration.
     exec { 'create_dockerio_service_dir':
       command => '/bin/mkdir -p /etc/systemd/system/docker.service.d/',
       unless  => '/usr/bin/test -d /etc/systemd/system/docker.service.d/',
+    }
+    if ($::facts['docker_service_exists'] == 'yes') {
+      $service_to_notify = Service['docker']
+    } else {
+      $service_to_notify = undef
     }
     # The nftables ns dropin file must be in place before the docker service is installed on a new host,
     # otherwise the docker0 interface will be created and interfere until reboot.
@@ -50,4 +50,5 @@ class sunet::dockeriohost(
       command => 'rmdir /etc/systemd/system/docker.service.d/',
       onlyif  => 'ls -A /etc/systemd/system/docker.service.d/*',
     }
+  }
 }
