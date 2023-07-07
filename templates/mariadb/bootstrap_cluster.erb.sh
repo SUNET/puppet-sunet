a0cde9aafd76f6485cc81ad605d0ac5601afd606
Micke Nordin <kano@sunet.se>
2023-07-07 14:15:02 +0200

    Stop running service first

--- templates/mariadb/bootstrap_cluster.erb.sh
+++ templates/mariadb/bootstrap_cluster.erb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MYSQL_ROOT_PASSWORD="<%= @mysql_root_password %>"
+systemctl stop mariadb.service
 galera_new_cluster
 mysql -p"${MYSQL_ROOT_PASSWORD}" < '/etc/mysql/02-backup_user.sql'
 
