525c5489d26f4045f2c89b5be91d7679af8b6939
Micke Nordin <kano@sunet.se>
2023-07-10 11:54:52 +0200

    Snippets: multiple classes or resource definitions is deprecated
    Having multiple classes or resource definitions is deprecated in puppet
    5.5 and not possible in puppet 7, so this is a refactor that should be
    compatible with both.

---
+++ manifests/snippets/secret_file.pp
@@ -0,0 +1,27 @@
+#Secret file
+define sunet::snippets::secret_file(
+  $hiera_key = undef,
+  $path      = undef,
+  $owner     = root,
+  $group     = root,
+  $mode      = '0400',
+  $base64    = false
+) {
+  require stdlib
+  $thefile = $path ? {
+    undef    => $name,
+    default  => $path
+  }
+  $safe_key = regsubst($hiera_key, '[^0-9A-Za-z_]', '_', 'G')
+  $data = hiera($hiera_key, hiera($safe_key))
+  $decoded_data = $base64 ? {
+    true     => base64('decode',$data),
+    default  => $data
+  }
+  file { $thefile:
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    content => inline_template('<%= @decoded_data %>')
+  }
+}
---
+++ manifests/snippets/somaxconn.pp
@@ -0,0 +1,14 @@
+# somaxconn
+define sunet::snippets::somaxconn($maxconn=512) {
+  require stdlib
+  $cfg = "/etc/sysctl.d/${title}_somaxconn.conf";
+  file { $cfg:
+    ensure  => file,
+    content => "net.core.somaxconn=${maxconn}",
+    notify  => Exec["refresh-sysctl-${title}"]
+  }
+  exec {"refresh-sysctl-${title}":
+    command     => "sysctl -p ${cfg}",
+    refreshonly => true
+  }
+}
