c9c3cbd80149ffe9d507d8a029b30ac15f41af3d
Micke Nordin <kano@sunet.se>
2023-07-20 15:44:35 +0200

    Add config files

--- manifests/postfix.pp
+++ manifests/postfix.pp
@@ -2,12 +2,14 @@
 class sunet::postfix(
   Array[String] $client_ips,
   String $interface = 'ens3',
-  String $dir_admin_username = 'admin',
   String $postfix_image  = 'docker.sunet.se/mail/postfix',
   String $postfix_tag    = '3.7.5-2-SUNET-1',
 )
 {
   $hostname = $facts['networking']['fqdn']
+  # FIXME: Use acme certs
+  $smtpd_tls_cert_file='/etc/ssl/certs/ssl-cert-snakeoil.pem'
+  $smtpd_tls_key_file='/etc/ssl/private/ssl-cert-snakeoil.key'
   # Composefile
   sunet::docker_compose { 'postfix':
     content          => template('sunet/postfix/docker-compose.erb.yml'),
@@ -25,6 +27,14 @@ class sunet::postfix(
   }
   file { '/opt/postfix/config':
     ensure => directory,
+  }
+  file { '/opt/postfix/config/main.cf':
+    ensure  => file,
+    content =>  template('sunet/postfix/main.erb.cf')
+  }
+  file { '/opt/postfix/config/master.cf':
+    ensure  => file,
+    content =>  template('sunet/postfix/master.erb.cf')
   }
 
 }
