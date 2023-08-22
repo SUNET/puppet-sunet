39b3d8b74b5fc3d07f10d7a7d5a9b3944dcfc681
Micke Nordin <kano@sunet.se>
2023-08-22 11:17:19 +0200

    Create db dirs

--- manifests/mail/mariadb.pp
+++ manifests/mail/mariadb.pp
@@ -1,5 +1,11 @@
 # Sunet mail db class
 class sunet::mail::mariadb () {
+  file { '/opt/mariadb/':
+    ensure  => directory,
+  }
+  file { '/opt/mariadb/init/':
+    ensure  => directory,
+  }
   file { '/opt/mariadb/init/03mail.sql':
     ensure  => file,
     content => template('sunet/mail/mariadb/03mail.erb.sql')
