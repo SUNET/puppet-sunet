fb5d2547f35aecce1fb6220cdc4ae4208d7b75cf
Micke Nordin <kano@sunet.se>
2023-09-11 11:09:05 +0200

    Add saml config

--- manifests/mail/sogo.pp
+++ manifests/mail/sogo.pp
@@ -7,6 +7,19 @@ class sunet::mail::sogo(
 )
 {
 
+  # SOGoSAML2PrivateKeyLocation
+  $saml_private_key='/etc/sogo/privkey.pem'
+  # SOGoSAML2CertificateLocation
+  $saml_cert='/etc/sogo/cert.pem'
+  # SOGoSAML2IdpMetadataLocation
+  $idp_metadata='/etc/sogo/metadata.xml'
+  # SOGoSAML2IdpPublicKeyLocation
+  $idp_pubkey='/etc/sogo/idp-pubkey.pem'
+  # SOGoSAML2IdpCertificateLocation
+  $idp_cert='/etc/sogo/idp-cert.pem'
+  # SOGoSAML2LoginAttribute
+  $saml_attribute='subject-id'
+
   $hostname = $facts['networking']['fqdn']
   # This looks esoteric, a longer example for parsing the hostname is available here:
   # https://wiki.sunet.se/display/sunetops/Platform+naming+standards#Platformnamingstandards-Parsingthename
@@ -50,6 +63,11 @@ class sunet::mail::sogo(
       ensure  => file,
       content =>  template("sunet/mail/sogo/${file}.erb.conf")
     }
+  }
+  $saml_private_key_content = safe_hiera('saml_private_key_content')
+  file { "/opt/sogo/config/privkey.pem":
+    ensure  => file,
+    content => $saml_private_key_content,
   }
 
 }
