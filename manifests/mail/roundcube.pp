a70bf7854e4092ae40ba2cc672dead97e9a637a7
Micke Nordin <kano@sunet.se>
2023-09-12 14:19:24 +0200

    Add roundcube

---
+++ manifests/mail/roundcube.pp
@@ -0,0 +1,44 @@
+# Postfix for SUNET
+class sunet::mail::roundcube(
+  String $domain                 = 'sunet.dev',
+  String $interface              = 'ens3',
+  String $roundcube_image          = 'roundcube/roundcubemail',
+  String $roundcube_tag            = '1.6.2-apache',
+)
+{
+
+  $hostname = $facts['networking']['fqdn']
+  # This looks esoteric, a longer example for parsing the hostname is available here:
+  # https://wiki.sunet.se/display/sunetops/Platform+naming+standards#Platformnamingstandards-Parsingthename
+  $my_environment = split(split($hostname, '[.]')[0],'[-]')[2]
+
+  $config = lookup($my_environment)
+  $db_hosts = $config['db_hosts']
+  $db_ips = $db_hosts.map |$host| {
+    $addrs = dns_lookup($host)
+    $addrs[0]
+  }
+
+  $roundcube_password = lookup('roundcube_password')
+
+  # Composefile
+  sunet::docker_compose { 'roundcube':
+    content          => template('sunet/mail/roundcube/docker-compose.erb.yml'),
+    service_name     => 'roundcube',
+    compose_dir      => '/opt',
+    compose_filename => 'docker-compose.yml',
+    description      => 'roundcube',
+  }
+  $ports = [80, 443]
+  $ports.each|$port| {
+    sunet::nftables::docker_expose { "mail_port_${port}":
+      allow_clients => 'any',
+      port          => $port,
+      iif           => $interface,
+    }
+  }
+  file { '/opt/roundcube/config':
+    ensure => directory,
+  }
+
+}
