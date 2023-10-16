d56722b5f6c5c70c67b1c04efd0746d79b50d780
Johan Wassberg <jocar@sunet.se>
2023-10-16 11:46:22 +0200

    Configure configurations

--- manifests/bankid_idp.pp
+++ manifests/bankid_idp.pp
@@ -8,6 +8,12 @@ class sunet::bankid_idp(
 ) {
 
 
+    file { '/opt/bankid/config/service.yml':
+      content => template('sunet/bankid_idp/service.yml.erb'),
+      mode    => '0755',
+    }
+
+
   sunet::docker_compose { 'bankid-idp':
     content          => template('sunet/bankid_idp/docker-compose-bankid-idp.yml.erb'),
     service_name     => 'bankid_idp',
