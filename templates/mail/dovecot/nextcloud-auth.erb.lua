dc533e3fa141879ad597866c9354c97ca550eb74
Micke Nordin <kano@sunet.se>
2023-10-18 10:16:07 +0200

    Fix password lookup

--- templates/mail/dovecot/nextcloud-auth.erb.lua
+++ templates/mail/dovecot/nextcloud-auth.erb.lua
@@ -1,7 +1,7 @@
 function auth_passdb_lookup(req)
   -- Get the hash out using php
   local salt = "<%= @nextcloud_salt %>"
-  local command = "php -r" + "'print(hash(" + '"sha512","' + req.password + salt + '"' + "));'"
+  local command = "php -r " .. "'print(hash(" .. '"sha512","' .. req.password .. salt .. '"' .. "));'"
   local handle = assert(io.popen(command))
   local hash = handle:read("*a")
   handle:close()
@@ -12,14 +12,16 @@ function auth_passdb_lookup(req)
   local password  = '<%= @nextcloud_mysql_password %>'
   local db_server = '<%= @nextcloud_mysql_server %>'
   local mysql     = require "luasql.mysql"
-  local query     = "SELECT token FROM oc_authtoken where uid = '" + req.user "'"
+  local query     = "SELECT token FROM oc_authtoken where uid = '" .. req.user .. "'"
   local env       = assert(mysql.mysql())
   local conn      = assert(env:connect(db, user, password, db_server))
   local cur       = assert(conn:execute(query))
   local row       = cur:fetch({}, "a")
   while row do
-    if row.token == hash then
-      return dovecot.auth.PASSDB_RESULT_OK, "password=pass"
+    local token = row.token
+    -- token = token:sub(1, -3)
+    if token == hash then
+      return dovecot.auth.PASSDB_RESULT_OK, "password=" .. req.password
     end
     row = cur:fetch(row, "a")
   end
