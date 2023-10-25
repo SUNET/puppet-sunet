b3b160f334c8be37979c79f995cf334c82dfb102
Victor Näslund <victor@sunet.se>
2023-10-25 15:48:26 +0200

    fix cron

--- manifests/vc/standalone.pp
+++ manifests/vc/standalone.pp
@@ -134,7 +134,7 @@ class sunet::vc::standalone(
     compose_dir      => '/opt',
     compose_filename => 'docker-compose.yml',
     description      => 'VC-standalone service',
-    subscribe        => 'renew_letsencrypt_cert',
+    subscribe        => File['/opt/vc/cert/tls-cert-key.pem'],
   }
 
   if $::facts['sunet_nftables_enabled'] == 'yes' {
