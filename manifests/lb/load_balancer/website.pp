a39276c9618505665dbace665898d33e0a278658
Micke Nordin <kano@sunet.se>
2023-10-25 16:28:56 +0200

    Break out to separate files

--- manifests/lb/load_balancer/website.pp
+++ manifests/lb/load_balancer/website.pp
@@ -212,7 +212,7 @@ define sunet::lb::load_balancer::website(
     # NEW way, configure forwarding and IPv6 NAT (for haproxy to reach ipv6-only backends) using
     # an nftables drop-in file.
 
-    $frontend_ips = get_all_frontend_ips($config)
+    $frontend_ips = sunet::lb::load_balancers::get_all_frontend_ips($config)
 
     # Variables used in template
     #
@@ -253,24 +253,4 @@ define sunet::lb::load_balancer::website(
       }
     }
   }
-}
-
-# Get a list of all the instances frontend addresses
-function get_all_frontend_ips(
-  Hash[String, Any] $config,
-) >> Array[String] {
-  if ! has_key($config, 'frontends') {
-    fail('Website config contains no frontends section')
-  }
-  $all_ips = map($config['frontends']) | $frontend_fqdn, $v | {
-    # k should be a frontend FQDN and $v a hash with ips in it:
-    #   $v = {ips => [192.0.2.1]}}
-    (is_hash($v) and has_key($v, 'ips')) ? {
-      true  => $v['ips'],
-      false => []
-    }
-  }
-
-  $uniq = flatten($all_ips).unique
-  $uniq
 }
