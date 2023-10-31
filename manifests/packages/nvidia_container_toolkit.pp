79fcf365376ed39f3046cb2318a77a55f23efbeb
Micke Nordin <kano@sunet.se>
2023-10-31 15:48:40 +0100

    Typo

--- manifests/packages/nvidia_container_toolkit.pp
+++ manifests/packages/nvidia_container_toolkit.pp
@@ -3,15 +3,15 @@ class sunet::packages::nvidia_container_toolkit {
     include sunet::packages::curl
     include sunet::packages::gpg
     exec { 'nvidia-container-toolkit-keyring':
-      cmd    => 'curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg',
-      unless => 'test -f /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg'
+      command => 'curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg',
+      unless  => 'test -f /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg'
     }
     exec { 'nvidia-container-toolkit-repo':
-      cmd    => 'curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed "s_deb https://_deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://_g" > /etc/apt/sources.list.d/nvidia-container-toolkit.list',
-      unless => 'test -f /etc/apt/sources.list.d/nvidia-container-toolkit.list'
+      command => 'curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed "s_deb https://_deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://_g" > /etc/apt/sources.list.d/nvidia-container-toolkit.list',
+      unless  => 'test -f /etc/apt/sources.list.d/nvidia-container-toolkit.list'
     }
     exec { 'nvidia-container-toolkit-update':
-      cmd     => 'apt update',
+      command => 'apt update',
       require => Exec['nvidia-container-toolkit-keyring', 'nvidia-container-toolkit-repo']
     }
     package { 'nvidia-container-toolkit':
