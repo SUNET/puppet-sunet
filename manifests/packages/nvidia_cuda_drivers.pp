e6fea6db2d7e4c60a68bf18408867df2d5ec70c3
Micke Nordin <kano@sunet.se>
2023-10-31 16:01:18 +0100

    Remove leading dot

--- manifests/packages/nvidia_cuda_drivers.pp
+++ manifests/packages/nvidia_cuda_drivers.pp
@@ -4,7 +4,7 @@ class sunet::packages::nvidia_cuda_drivers {
   include sunet::packages::linux_headers
   $distro = downcase($facts['os']['distro']['id'])
   $major = $facts['os']['distro']['release']['major']
-  $minor = $facts['os']['distro']['release']['minor']
+  $minor = $facts['os']['distro']['release']['minor'][1,-1]
   $cuda_keyring = 'cuda-keyring_1.0-1_all.deb'
   exec { 'nvidia-cuda-drivers-keyring':
     command => "curl https://developer.download.nvidia.com/compute/cuda/repos/${distro}${major}${minor}/x86_64/${cuda_keyring} -o /tmp/${cuda_keyring} && dpkg -i /tmp/${cuda_keyring}",
