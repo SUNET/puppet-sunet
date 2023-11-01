60eed2b043e39547ff37f2da6af23d5c96a819e8
Maria Haider <mariah@sunet.se>
2023-11-01 10:42:14 +0100

    added Johan W's changes from main
    commit f6bd6241e7cd2a34506f7230c26538707eafcc0e

--- manifests/metadata/mdqp.pp
+++ manifests/metadata/mdqp.pp
@@ -4,6 +4,10 @@ class sunet::metadata::mdqp(
   Integer $runs_per_hour=4,
   String $mdq_service='https://mds.swamid.se',
 ) {
+
+      include sunet::packages::jq
+      include sunet::packages::xmlstarlet
+
       $image_tag = "docker.sunet.se/mdqp:${imagetag}"
       docker::image { $image_tag :  # make it possible to use the same docker image more than once on a node
         ensure  => 'present',
