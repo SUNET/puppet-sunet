189bc8ac0e4ab1ea19ccd672af562115d3297afb
Johan Wassberg <jocar@sunet.se>
2023-11-14 10:23:08 +0100

    Unit must be specified

--- manifests/nagios/nrpe_check_memory.pp
+++ manifests/nagios/nrpe_check_memory.pp
@@ -1,7 +1,7 @@
 # Set up the memory check
 define sunet::nagios::nrpe_check_memory (
-  Integer $warning = 90,
-  Integer $critical = 95,
+  String $warning = '90%',
+  String $critical = '95%',
 ) {
     sunet::nagios::nrpe_command {'check_memory':
       command_line => "/usr/lib/nagios/plugins/check_memory -w ${warning} -c ${critical}"
