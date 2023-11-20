aee8cfb42f54942859d9738bbf5d8b4b122f0b50
Johan Wassberg <jocar@sunet.se>
2023-11-20 13:50:40 +0100

    Alias was never set

--- manifests/nagios/nrpe_check_process.pp
+++ manifests/nagios/nrpe_check_process.pp
@@ -5,9 +5,9 @@ define sunet::nagios::nrpe_check_process (
 
   include sunet::nagios::nrpe_check_process_command
 
-  $process_display_name = $alias ? {
+  $process_display_name = $display_name ? {
     undef   => $name,
-    default => $alias
+    default => $display_name
   }
   sunet::nagios::nrpe_command {"check_process_${name}":
     command_line => "/usr/lib/nagios/plugins/check_process -C PCPU -p ${name} -N ${process_display_name}"
