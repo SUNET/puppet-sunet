571597001efc4a789f020cef3b4ee10a2e40b3bd
Maria Haider <mariah@sunet.se>
2023-12-21 14:26:43 +0100

    introducing  $write_daemon_config

--- manifests/dockerhost.pp
+++ manifests/dockerhost.pp
@@ -14,6 +14,8 @@ class sunet::dockerhost(
   String $compose_image                       = 'docker.sunet.se/library/docker-compose',
   String $compose_version                     = '1.15.0',
   Optional[Array[String]] $tcp_bind           = undef,
+  Boolean $write_daemon_config                = false,
+  Boolean $enable_ipv6                        = false,
 ) {
 
   # Remove old versions, if installed
@@ -145,21 +147,69 @@ class sunet::dockerhost(
     $tls_key = undef
   }
 
-  class {'docker':
-    storage_driver              => $storage_driver,
-    manage_package              => false,
-    manage_kernel               => false,
-    use_upstream_package_source => false,
-    dns                         => $_docker_dns,
-    extra_parameters            => $docker_extra_parameters,
-    docker_command              => $docker_command,
-    daemon_subcommand           => $daemon_subcommand,
-    tcp_bind                    => $_tcp_bind,
-    tls_enable                  => $tls_enable,
-    tls_cacert                  => $tls_cacert,
-    tls_cert                    => $tls_cert,
-    tls_key                     => $tls_key,
-    require                     => Package[$docker_package_name],
+  # This is an approximation about how to enable IPv6 in Docker, but
+  # BEWARE! IPv6 is currently utterly dysfunctional in docker-compose (version 3 / 1.29.2). Sigh.
+  #
+  $ipv6_parameters = ($enable_ipv6 and ! $write_daemon_config) ? {
+    true => ['--ipv6',
+      $docker_network_v6 ? {
+        true => [],
+        default => ['--fixed-cidr-v6', $docker_network_v6],
+      }
+    ],
+    false => []
+  }
+
+  $_extra_parameters = flatten([
+    $docker_extra_parameters,
+    $ipv6_parameters,
+    ]).join(' ')
+
+  if $write_daemon_config {
+    if $docker_network =~ String[1] {
+      $default_address_pools = $docker_network
+    } else {
+      $default_address_pools = '172.16.0.0/12'
+    }
+    file {
+      '/etc/docker':
+        ensure => 'directory',
+        mode   => '0755',
+        ;
+      '/etc/docker/daemon.json':
+        ensure  => file,
+        mode    => '0644',
+        content => template('sunet/dockerhost/daemon.json.erb'),
+        ;
+    }
+
+    # Docker rejects options specified both from command line and in daemon.json
+    class {'docker':
+      manage_package              => false,
+      manage_kernel               => false,
+      use_upstream_package_source => false,
+      extra_parameters            => $_extra_parameters,
+      docker_command              => $docker_command,
+      daemon_subcommand           => $daemon_subcommand,
+      require                     => Package[$docker_package_name],
+    }
+  } else {
+    class {'docker':
+      storage_driver              => $storage_driver,
+      manage_package              => false,
+      manage_kernel               => false,
+      use_upstream_package_source => false,
+      dns                         => $_docker_dns,
+      extra_parameters            => $docker_extra_parameters,
+      docker_command              => $docker_command,
+      daemon_subcommand           => $daemon_subcommand,
+      tcp_bind                    => $_tcp_bind,
+      tls_enable                  => $tls_enable,
+      tls_cacert                  => $tls_cacert,
+      tls_cert                    => $tls_cert,
+      tls_key                     => $tls_key,
+      require                     => Package[$docker_package_name],
+    }
   }
 
   if $docker_network =~ String {
