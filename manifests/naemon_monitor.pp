e261dd338819f9265e5f9aab14e4cfb530c7b90d
Mikael Frykholm <mifr@sunet.se>
2024-01-09 14:17:20 +0100

    Allow to override thruk shibboleth config.

--- manifests/naemon_monitor.pp
+++ manifests/naemon_monitor.pp
@@ -20,6 +20,7 @@ class sunet::naemon_monitor(
   Array $exclude_hosts =  [],
   Optional[String] $default_host_group = undef,
   Array[Optional[String]] $optout_checks = [],
+  Array[Optional[String]] $override_shibxml = [],
 ){
 
   if $::facts['sunet_nftables_enabled'] == 'yes' {
