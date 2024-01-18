0fbdd5e58eb18be309496e49d970ca8e72ba1bfe
Johan Wassberg <jocar@sunet.se>
2024-01-18 09:36:06 +0100

    We only support modern dists

--- manifests/docker_compose_service.pp
+++ manifests/docker_compose_service.pp
@@ -7,29 +7,25 @@ define sunet::docker_compose_service(
   Array[String]    $service_extras = [],
   Optional[String] $start_command = undef,
 ) {
-  if ($::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemrelease, '15.04') >= 0) or $::operatingsystem == 'Debian' {
-    include sunet::systemd_reload
+  include sunet::systemd_reload
 
-    $_service_name = $service_name ? {
-      undef => $name,
-      default => $service_name,
-    }
+  $_service_name = $service_name ? {
+    undef => $name,
+    default => $service_name,
+  }
 
-    file {
-      "/etc/systemd/system/${_service_name}.service":
-        content => template('sunet/dockerhost/compose.service.erb'),
-        notify  => [Class['sunet::systemd_reload'],
-                    ],
-        ;
-    }
+  file {
+    "/etc/systemd/system/${_service_name}.service":
+      content => template('sunet/dockerhost/compose.service.erb'),
+      notify  => [Class['sunet::systemd_reload'],
+                  ],
+      ;
+  }
 
-    service { $_service_name :
-      ensure   => 'running',
-      enable   => true,
-      require  => File["/etc/systemd/system/${_service_name}.service"],
-      provider => 'systemd',  # puppet is really bad at figuring this out
-    }
-  } else {
-    err('sunet::docker_compose_service only available on Ubuntu >= 15.04 for now (only systemd is implemented)')
+  service { $_service_name :
+    ensure   => 'running',
+    enable   => true,
+    require  => File["/etc/systemd/system/${_service_name}.service"],
+    provider => 'systemd',  # puppet is really bad at figuring this out
   }
 }
