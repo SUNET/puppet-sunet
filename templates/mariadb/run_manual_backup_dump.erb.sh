2136b723882e17d131add14d6beabdeb87ff22f8
Johan Wassberg <jocar@sunet.se>
2024-02-23 16:15:26 +0100

    Do backup inside container

--- templates/mariadb/run_manual_backup_dump.erb.sh
+++ templates/mariadb/run_manual_backup_dump.erb.sh
@@ -4,7 +4,6 @@ dump_name="mariadb-dump-$(date +%Y-%m-%dT%H.%M.%S).sql.gz"
 dump_args="--all-databases --single-transaction --master-data=2"
 
 # Replication slave priv was not in backup user creation script previously
-mysql -e "GRANT REPLICATION SLAVE ON *.* TO 'backup'@'%'"
+docker exec mariadb-db-1 mysql -u root -p'<%= @mysql_root_password %>' -e "GRANT REPLICATION SLAVE ON *.* TO 'backup'@'%'"
 echo "Running backup as root user"
-mkdir -p /opt/backups
-mysqldump "${dump_args}" | gzip >"/opt/backups/${dump_name}"
+docker exec mariadb-db-1 mysqldump "${dump_args}" | gzip >"/backups/${dump_name}"
