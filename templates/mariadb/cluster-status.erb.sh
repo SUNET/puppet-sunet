1f91dc6965f2d6678ce467f48bee19ca3ba83d3b
Johan Wassberg <jocar@sunet.se>
2024-02-23 16:02:00 +0100

    Run command in docker

--- templates/mariadb/cluster-status.erb.sh
+++ templates/mariadb/cluster-status.erb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -N -B -e "show status like 'wsrep_cluster_status'"
+docker exec mariadb-db-1 mysql -u root -p'<%= @mysql_root_password %>' -N -B -e "show status like 'wsrep_cluster_status'"
