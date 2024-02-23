180510850cf958f92642ac60ad5ebbb6a35b8e50
Johan Wassberg <jocar@sunet.se>
2024-02-23 11:26:01 +0100

    Readd tooling
    Previously removed "by misstake" or lack of knowledge

--- manifests/mariadb.pp
+++ manifests/mariadb.pp
@@ -30,6 +30,57 @@ define sunet::mariadb(
     port => $ports,
   }
 
+  file { '/usr/local/bin/purge-binlogs':
+    ensure  => present,
+    content => template('sunet/mariadb/purge-binlogs.erb.sh'),
+    mode    => '0744',
+    owner   => 999,
+    group   => 999,
+  }
+  sunet::scriptherder::cronjob { 'purge_binlogs':
+    cmd           => '/usr/local/bin/purge-binlogs',
+    hour          => '6',
+    minute        => '0',
+    ok_criteria   => ['exit_status=0','max_age=2d'],
+    warn_criteria => ['exit_status=1','max_age=3d'],
+  }
+  file { '/usr/local/bin/bootstrap_cluster':
+    ensure  => present,
+    content => template('sunet/mariadb/bootstrap_cluster.erb.sh'),
+    mode    => '0744',
+  }
+  file { '/usr/local/bin/run_manual_backup_dump':
+    ensure  => present,
+    content => template('sunet/mariadb/run_manual_backup_dump.erb.sh'),
+    mode    => '0744',
+    owner   => 999,
+    group   => 999,
+  }
+  file { '/usr/local/bin/cluster-size':
+    ensure  => present,
+    content => template('sunet/mariadb/cluster-size.erb.sh'),
+    mode    => '0744',
+  }
+  file { '/usr/local/bin/cluster-status':
+    ensure  => present,
+    content => template('sunet/mariadb/cluster-status.erb.sh'),
+    mode    => '0744',
+  }
+  file { '/etc/sudoers.d/99-size-test':
+    ensure  => file,
+    content => "script ALL=(root) NOPASSWD: /usr/local/bin/cluster-size\n",
+    mode    => '0440',
+    owner   => 'root',
+    group   => 'root',
+  }
+  file { '/etc/sudoers.d/99-status-test':
+    ensure  => file,
+    content => "script ALL=(root) NOPASSWD: /usr/local/bin/cluster-status\n",
+    mode    => '0440',
+    owner   => 'root',
+    group   => 'root',
+  }
+
   $sql_files = ['02-backup_user.sql']
   $sql_files.each |$sql_file|{
     file { "${mariadb_dir}/init/${sql_file}":
