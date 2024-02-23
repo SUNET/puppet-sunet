2066ee47f370f4788a73331045b13341b07967f6
Johan Wassberg <jocar@sunet.se>
2024-02-23 16:20:58 +0100

    Make work

--- templates/mariadb/run_manual_backup_dump.erb.sh
+++ templates/mariadb/run_manual_backup_dump.erb.sh
@@ -4,6 +4,6 @@ dump_name="mariadb-dump-$(date +%Y-%m-%dT%H.%M.%S).sql.gz"
 dump_args="--all-databases --single-transaction --master-data=2"
 
 # Replication slave priv was not in backup user creation script previously
-docker exec mariadb-db-1 mysql -u root -p'<%= @mysql_root_password %>' -e "GRANT REPLICATION SLAVE ON *.* TO 'backup'@'%'"
+docker exec mariadb-db-1 mysql -u root -p'<%= @mariadb_root_password %>' -e "GRANT REPLICATION SLAVE ON *.* TO 'backup'@'%'"
 echo "Running backup as root user"
-docker exec mariadb-db-1 mysqldump "${dump_args}" | gzip >"/backups/${dump_name}"
+docker exec mariadb-db-1 bash -c "mysqldump ${dump_args} | gzip >\"/backups/${dump_name}\""
