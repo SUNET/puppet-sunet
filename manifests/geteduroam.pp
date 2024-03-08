02672e266305d11f466fcd67ebc78975ddc58f1c
Johan Wassberg <jocar@sunet.se>
2024-03-08 12:43:10 +0100

    Convert to new firewall class

--- manifests/geteduroam.pp
+++ manifests/geteduroam.pp
@@ -11,18 +11,13 @@ class sunet::geteduroam(
     port  => 1812,
     proto =>  'udp'
   }
-  if $::facts['sunet_nftables_enabled'] == 'yes' {
-      sunet::nftables::docker_expose { 'allow_http' :
-      iif           => $interface,
-      allow_clients => 'any',
-      port          => 80,
-    }
-
-      sunet::nftables::docker_expose { 'allow_https' :
-      iif           => $interface,
-      allow_clients => 'any',
-      port          => 443,
-    }
+  sunet::nftables::allow { 'expose-allow-http':
+    from => 'any',
+    port => 80,
+  }
+  sunet::nftables::allow { 'expose-allow-https':
+    from => 'any',
+    port => 443,
   }
 
   class { 'sunet::dehydrated::client': domain =>  $domain, ssl_links => true }
