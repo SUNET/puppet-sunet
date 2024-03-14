82c82da26a6270035ed226be6cc38e4fe988fd39
Johan Wassberg <jocar@sunet.se>
2024-03-14 12:31:10 +0100

    Improve name of class

--- manifests/certbot/acmed.pp
+++ manifests/certbot/acmed.pp
@@ -1,5 +1,5 @@
 # acmed - automatated
-class sunet::acmed(
+class sunet::certbot::acmed(
   String $server = 'https://acme-d.sunet.se',
 ){
 
@@ -9,22 +9,22 @@ class sunet::acmed(
   file { '/etc/letsencrypt/acme-dns-auth.py':
     ensure  => file,
     mode    => '0700',
-    content => template('sunet/acmed/acme-dns-auth.py.erb'),
+    content => template('sunet/certbot/acme-dns-auth.py.erb'),
   }
   file { '/etc/letsencrypt/certbot-acmed-renew-post-hook-wrapper':
     ensure  => file,
     mode    => '0700',
-    content => file('sunet/acmed/certbot-acmed-renew-post-hook-wrapper'),
+    content => file('sunet/certbot/certbot-acmed-renew-post-hook-wrapper'),
   }
 
-  $acmed_accounts = lookup('acmed_accounts', undef, undef, {})
+  $acmed_clients = lookup('certbot_acmed_clients', undef, undef, {})
   file { '/etc/letsencrypt/acmedns.json':
-    content => inline_template("<%= @acmed_accounts.to_json %>\n"),
+    content => inline_template("<%= @acmed_clients.to_json %>\n"),
     notify  => Exec['certbot'],
   }
-  $domain_arg = join($acmed_accounts.keys, ' -d ')
+  $domain_arg = join($acmed_clients.keys, ' -d ')
 
-  exec {'certbot':
+  exec {'certbot_issuing':
     command     => "certbot certonly --no-eff-email --agree-tos -m noc@sunet.se --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns -d ${domain_arg}",
     refreshonly => true,
   }
