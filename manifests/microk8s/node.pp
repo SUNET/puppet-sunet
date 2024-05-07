b2bee3a458272cb5bf1a1ede0b330d8855117184
Micke Nordin <mickenordin@users.noreply.github.com>
2024-05-07 11:39:26 +0200

    WIP:  migration path to nft for microk8s (#155)
    * try to migrate to nft
    
    * Fix separator

--- manifests/microk8s/node.pp
+++ manifests/microk8s/node.pp
@@ -1,29 +1,113 @@
 # microk8s cluster node
 class sunet::microk8s::node(
-  String  $channel            = '1.27/stable',
-  Boolean $mayastor           = false,
-  Boolean $traefik            = true,
-  Integer $failure_domain     = 42,
-  Integer $web_nodeport       = 30080,
-  Integer $websecure_nodeport = 30443,
+  String  $channel                = '1.28/stable',
+  Boolean $traefik                = true,
+  Integer $failure_domain         = 42,
+  Integer $web_nodeport           = 30080,
+  Integer $websecure_nodeport     = 30443,
+  Optional[Array[String]] $peers  = [],
 ) {
-  # Loop through peers and do things that require their ip:s
   include sunet::packages::snapd
-  if $::operatingsystem == 'Debian' and versioncmp($::operatingsystemrelease, '12') >= 0 {
-    include sunet::packages::ufw
+
+  $hiera_peers =  lookup('microk8s_peers', undef, undef, [])
+  if $facts['hostname'] =~ /(^kubew|k8sw-)[0-9]/ {
+    $type = 'worker'
+  } else {
+    $type = 'controller'
   }
 
-  split($facts['microk8s_peers'], ',').each | String $peer| {
-    unless $peer == 'unknown' {
+  if $peers != [] {
+    $final_peers = $peers
+  } elsif $hiera_peers != [] {
+    $final_peers = $hiera_peers
+  } else {
+    $final_peers = map(split($facts['microk8s_peers'], ',')) | String $peer| {
       $peer_ip = $facts[join(['microk8s_peer_', $peer])]
+      "${peer_ip} ${peer}"
+    }
+  }
+    # Loop through peers and do things that require their ip:s
+  $final_peers.each | String $peer_tuple| {
+    $peer_ip = split($peer_tuple, ' ')[0]
+    $peer = split($peer_tuple, ' ')[1]
+    unless $peer == 'unknown' or $peer_ip == $facts['ipaddress'] {
       file_line { "hosts_${peer}":
         path => '/etc/hosts',
         line => "${peer_ip} ${peer}",
       }
-      -> exec { "ufw_${peer}":
-        command => "ufw allow in from ${peer_ip}",
-        unless  => "ufw status | grep -Eq 'Anywhere.*ALLOW.*${peer_ip}'" ,
+    }
+    $public_controller_ports = [8080, 8443, 16443]
+    $private_controller_ports = [10250, 10255, 25000, 12379, 10257, 10259, 19001]
+    $private_worker_ports = [10250, 10255, 16443, 25000, 12379, 10257, 10259, 19001]
+    if $::facts['sunet_nftables_enabled'] == 'yes' {
+      if $type == 'controller' {
+        sunet::nftables::allow { "nft_${peer}_private":
+          port => $private_controller_ports,
+          from => $peer_ip,
+        }
+        sunet::nftables::allow { "nft_${peer}_public":
+          port => $public_controller_ports,
+          from => 'any',
+        }
+      } else {
+        sunet::nftables::allow { "nft_${peer}_private":
+          port => $private_worker_ports,
+          from => $peer_ip,
+        }
       }
+      sunet::nftables::allow { "nft_${peer}_udp":
+        port  => [4789],
+        from  => $peer_ip,
+        proto => 'udp',
+      }
+    } else {
+      if $type == 'controller' {
+        sunet::misc::ufw_allow {"nft_${peer}_private":
+          port => $private_controller_ports,
+          from => $peer_ip,
+        }
+        sunet::misc::ufw_allow { "nft_${peer}_public":
+          port => $public_controller_ports,
+          from => 'any',
+        }
+      } else {
+        sunet::misc::ufw_allow { "nft_${peer}_private":
+          port => $private_worker_ports,
+          from => $peer_ip,
+        }
+      }
+      sunet::misc::ufw_allow { "nft_${peer}_udp":
+        port  => [4789],
+        from  => $peer_ip,
+        proto => 'udp',
+      }
+    }
+  }
+  if $::facts['sunet_nftables_enabled'] == 'yes' {
+    file { '/etc/nftables/conf.d/500-microk8s-rules.nft':
+      ensure  => file,
+      content => template('sunet/microk8s/500-microk8s-rules.nft.erb'),
+      mode    => '0644',
+    }
+  } else {
+    # This is how ufw::allow does it, but that lacks support for "on"
+    exec { 'allow-outgoing-on-calico':
+      command  => 'ufw allow out on vxlan.calico',
+      path     => '/usr/sbin:/bin:/usr/bin',
+      unless   => 'ufw status | grep -qE "ALLOW OUT   Anywhere (\(v6\) |)on vxlan.calico"',
+      provider => 'posix',
+    }
+    -> exec { 'allow-incomming-on-calico':
+      command  => 'ufw allow in on vxlan.calico',
+      path     => '/usr/sbin:/bin:/usr/bin',
+      unless   => 'ufw status | grep -qE "Anywhere (\(v6\) |)on vxlan.calico"',
+      provider => 'posix',
+    }
+    -> exec { 'iptables-allow-forward':
+      command  => 'iptables -P FORWARD ACCEPT',
+      path     => '/usr/sbin:/bin:/usr/bin',
+      provider => 'shell',
+      unless   => 'iptables -L FORWARD | grep -q "Chain FORWARD (policy ACCEPT)"',
     }
   }
   exec { 'install_microk8s':
@@ -39,29 +123,6 @@ class sunet::microk8s::node(
     ensure  => file,
     content => "failure-domain=${failure_domain}\n",
     mode    => '0660',
-  }
-  -> sunet::misc::ufw_allow { 'microk8s_ports':
-    from => 'any',
-    port => [8080, 8443, 16443, 10250, 10255, 25000, 12379, 10257, 10259, 19001, 30443],
-  }
-  # This is how ufw::allow does it, but that lacks support for "on"
-  -> exec { 'allow-outgoing-on-calico':
-    command  => 'ufw allow out on vxlan.calico',
-    path     => '/usr/sbin:/bin:/usr/bin',
-    unless   => 'ufw status | grep -qE "ALLOW OUT   Anywhere (\(v6\) |)on vxlan.calico"',
-    provider => 'posix',
-  }
-  -> exec { 'allow-incomming-on-calico':
-    command  => 'ufw allow in on vxlan.calico',
-    path     => '/usr/sbin:/bin:/usr/bin',
-    unless   => 'ufw status | grep -qE "Anywhere (\(v6\) |)on vxlan.calico"',
-    provider => 'posix',
-  }
-  -> exec { 'iptables-allow-forward':
-    command  => 'iptables -P FORWARD ACCEPT',
-    path     => '/usr/sbin:/bin:/usr/bin',
-    provider => 'shell',
-    unless   => 'iptables -L FORWARD | grep -q "Chain FORWARD (policy ACCEPT)"',
   }
   unless any2bool($facts['microk8s_rbac']) {
     exec { 'enable_plugin_rbac':
@@ -98,33 +159,10 @@ class sunet::microk8s::node(
     command  => '/usr/bin/snap alias microk8s.helm helm',
     provider => 'shell',
   }
-  if $mayastor {
-    package { "linux-modules-extra-${facts['kernelrelease']}":
-      ensure   =>  installed,
-      provider => apt,
-    }
-    file {'/etc/modules-load.d/microk8s-mayastor.conf':
-      ensure  => file,
-      content => "nvme_tcp\n",
-      mode    => '0644',
-    }
-    file { '/etc/sysctl.d/20-microk8s-hugepages.conf':
-      ensure  => file,
-      content => "vm.nr_hugepages = 4096\n",
-      mode    => '0644',
-    }
-    unless any2bool($facts['microk8s_mayastor']) {
-      exec { 'enable_plugin_mayastor':
-        command  => '/snap/bin/microk8s enable mayastor',
-        provider => 'shell',
-      }
-    }
-  }
   $namespaces = lookup('microk8s_secrets', undef, undef, {})
   $namespaces.each |String $namespace, Hash $secrets| {
       $secrets.each |String $name, Array $secret| {
         set_microk8s_secret($namespace, $name, $secret)
     }
   }
-  #import_gpg_keys_to_microk8s()
 }
