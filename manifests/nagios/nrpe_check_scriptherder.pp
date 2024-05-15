39a8d1b28a5f38dbe972f12fa1ba78581b5b6008
Johan Wassberg <jocar@sunet.se>
2024-05-15 08:47:47 +0200

    Handle scriptherder bundled with puppet

--- manifests/nagios/nrpe_check_scriptherder.pp
+++ manifests/nagios/nrpe_check_scriptherder.pp
@@ -1,7 +1,11 @@
 # Check Scriptherder
 define sunet::nagios::nrpe_check_scriptherder (
 ) {
-    sunet::nagios::nrpe_command { 'check_scriptherder':
+    $scriptherder_from_cosmos = $sunet::scriptherder::init::nrpe_sudo
+
+    # Scriptherder bundled with puppet requires sudo and provides it's own NRPE configuration
+    if $scriptherder_from_cosmos != true {
+      sunet::nagios::nrpe_command { 'check_scriptherder':
       command_line => '/usr/local/bin/scriptherder --mode check',
     }
 }
