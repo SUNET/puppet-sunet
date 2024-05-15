12110345f6a45b04aef4bec72367ff99deb1744a
Johan Wassberg <jocar@sunet.se>
2024-05-15 08:58:59 +0200

    Syntax error

--- manifests/nagios/nrpe_check_scriptherder.pp
+++ manifests/nagios/nrpe_check_scriptherder.pp
@@ -6,6 +6,7 @@ define sunet::nagios::nrpe_check_scriptherder (
     # Scriptherder bundled with puppet requires sudo and provides it's own NRPE configuration
     if $scriptherder_from_cosmos != true {
       sunet::nagios::nrpe_command { 'check_scriptherder':
-      command_line => '/usr/local/bin/scriptherder --mode check',
+        command_line => '/usr/local/bin/scriptherder --mode check',
+      }
     }
 }
