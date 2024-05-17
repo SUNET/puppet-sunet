5848def441e68f379ddd9b14ad001e435a6a6c11
Johan Wassberg <jocar@sunet.se>
2024-05-17 09:46:51 +0200

    No need to compare a bool

--- manifests/nagios/nrpe_check_scriptherder.pp
+++ manifests/nagios/nrpe_check_scriptherder.pp
@@ -4,7 +4,7 @@ define sunet::nagios::nrpe_check_scriptherder (
     $scriptherder_from_cosmos = $sunet::server::install_scriptherder
 
     # Scriptherder bundled with puppet requires sudo and provides it's own NRPE configuration
-    if $scriptherder_from_cosmos != true {
+    if ! $scriptherder_from_cosmos {
       sunet::nagios::nrpe_command { 'check_scriptherder':
         command_line => '/usr/local/bin/scriptherder --mode check',
       }
