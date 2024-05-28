56b4e38e68836e92a07f2647cdfaa49248f844ee
Mikael Frykholm <mifr@sunet.se>
2024-05-28 10:00:58 +0200

    Add otel collector.

---
+++ manifests/otel/alloy.pp
@@ -0,0 +1,32 @@
+# The class to setup Nagios NRPE
+class sunet::otel::alloy () {
+  $nagios_ip_v4 = lookup('nagios_ip_v4', undef, undef, '109.105.111.111')
+  $nagios_ip_v6 = lookup('nagios_ip_v6', undef, undef, '2001:948:4:6::111')
+  $nrpe_clients = lookup('nrpe_clients', undef, undef, ['127.0.0.1','127.0.1.1',$nagios_ip_v4,$nagios_ip_v6])
+  $allowed_hosts = join($nrpe_clients,',')
+
+  file { '/etc/apt/keyrings/grafana.gpg' :
+    ensure  => 'file',
+    notify  => Service['alloy'],
+    mode    => '0644',
+    group   => 'root',
+    require => Package['alloy'],
+    content => file( 'sunet/otel/grafana.gpg' ),
+  }
+  file { '/etc/apt/sources.list.d/grafana.list' :
+    ensure  => 'file',
+    notify  => Service['alloy'],
+    mode    => '0644',
+    group   => 'root',
+    require => Package['alloy'],
+    content => 'deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main',
+  }
+  package { 'alloy':
+    ensure => 'installed',
+  }
+  -> service { 'alloy':
+    ensure  => 'running',
+    enable  => 'true',
+    require => Package['alloy'],
+  }
+}
