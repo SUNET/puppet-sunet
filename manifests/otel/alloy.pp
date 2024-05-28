c3c9bb57b1457be13e6f08c6a4cf6d3f9ee430e3
Mikael Frykholm <mifr@sunet.se>
2024-05-28 10:59:16 +0200

    Add parameter documentation.

--- manifests/otel/alloy.pp
+++ manifests/otel/alloy.pp
@@ -1,5 +1,8 @@
-# The class to setup Nagios NRPE
-class sunet::otel::alloy () {
+# @summary class to setup Grafana Alloy
+# @param otel_receiver Where should we send OpenTelemetry data?
+class sunet::otel::alloy (
+  String $otel_receiver    = undef,
+) {
   $nagios_ip_v4 = lookup('nagios_ip_v4', undef, undef, '109.105.111.111')
   $nagios_ip_v6 = lookup('nagios_ip_v6', undef, undef, '2001:948:4:6::111')
   $nrpe_clients = lookup('nrpe_clients', undef, undef, ['127.0.0.1','127.0.1.1',$nagios_ip_v4,$nagios_ip_v6])
