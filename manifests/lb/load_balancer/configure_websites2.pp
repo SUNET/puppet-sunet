44fd4bf18e83dc0a12efe2ff03b1cc716317b0a0
Rasmus Thorslund <thorslund@sunet.se>
2024-06-04 13:02:39 +0200

    fixed configure websites2 pointing to old manifest

--- manifests/lb/load_balancer/configure_websites2.pp
+++ manifests/lb/load_balancer/configure_websites2.pp
@@ -1,8 +1,8 @@
 
-define sunet::frontend::load_balancer::configure_websites2($websites, $basedir, $confdir, $scriptdir)
+define sunet::lb::load_balancer::configure_websites2($websites, $basedir, $confdir, $scriptdir)
 {
   each($websites) | $site, $config | {
-    create_resources('sunet::frontend::load_balancer::website2', {$site => {}}, {
+    create_resources('sunet::lb::load_balancer::website2', {$site => {}}, {
       'basedir'         => $basedir,
       'confdir'         => $confdir,
       'scriptdir'       => $scriptdir,
