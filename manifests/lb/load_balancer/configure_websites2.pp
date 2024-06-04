0ffc66265f6d57fd72848ac0a6c422428faaa1a0
Rasmus Thorslund <thorslund@sunet.se>
2024-06-04 13:33:48 +0200

    interface config

--- manifests/lb/load_balancer/configure_websites2.pp
+++ manifests/lb/load_balancer/configure_websites2.pp
@@ -1,5 +1,5 @@
 
-define sunet::lb::load_balancer::configure_websites2($websites, $basedir, $confdir, $scriptdir)
+define sunet::lb::load_balancer::configure_websites2($websites, $basedir, $confdir, $scriptdir, String $interface = 'eth0')
 {
   each($websites) | $site, $config | {
     create_resources('sunet::lb::load_balancer::website2', {$site => {}}, {
