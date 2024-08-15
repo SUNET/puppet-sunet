11b76c02801a2ce49ff50c78957d25c189d6a741
Mikael Frykholm <mifr@sunet.se>
2024-08-15 07:50:19 +0200

    Add a targets.d directory to add more targets to scrape for easy integration with other services. Add TLS.

--- manifests/otel/alloy.pp
+++ manifests/otel/alloy.pp
@@ -3,11 +3,6 @@
 class sunet::otel::alloy (
   String $otel_receiver    = undef,
 ) {
-  $nagios_ip_v4 = lookup('nagios_ip_v4', undef, undef, '109.105.111.111')
-  $nagios_ip_v6 = lookup('nagios_ip_v6', undef, undef, '2001:948:4:6::111')
-  $nrpe_clients = lookup('nrpe_clients', undef, undef, ['127.0.0.1','127.0.1.1',$nagios_ip_v4,$nagios_ip_v6])
-  $allowed_hosts = join($nrpe_clients,',')
-
   file { '/etc/apt/keyrings/grafana.gpg' :
     ensure  => 'file',
     notify  => Service['alloy'],
@@ -25,6 +20,12 @@ class sunet::otel::alloy (
   package { 'alloy':
     ensure => 'installed',
   }
+  file { '/etc/alloy/targets.d' :
+    ensure => 'directory',
+    notify => Service['alloy'],
+    mode   => '0644',
+    group  => 'root',
+  }
   file { '/etc/alloy/config.alloy' :
     ensure  => 'file',
     notify  => Service['alloy'],
@@ -32,7 +33,6 @@ class sunet::otel::alloy (
     group   => 'root',
     content => template( 'sunet/otel/config.alloy' ),
   }
-
   service { 'alloy':
     ensure  => 'running',
     enable  => 'true',
