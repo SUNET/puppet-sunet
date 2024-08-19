1fda7deec5cda1660c6df62da0feab970169f0b4
Johan Wassberg <jocar@sunet.se>
2024-08-19 11:55:56 +0200

    Handled by the sync server class

--- manifests/certbot/acmed.pp
+++ manifests/certbot/acmed.pp
@@ -23,16 +23,4 @@ class sunet::certbot::acmed(
     command     => "certbot certonly --no-eff-email --agree-tos -m noc@sunet.se --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns -d ${domain_arg}",
     refreshonly => true,
   }
-
-  $agent_ips = lookup('acmed_agent_ips', undef, undef, [])
-  $agent_ips.each |$ip| {
-    sunet::nftables::allow { "allow_agent_${ip}":
-      from => $ip,
-      port => 22,
-    }
-  }
-  sunet::ssh_keys { 'agent-keys':
-    config            => safe_hiera('acmed_agent_ssh_keys_mapping', {}),
-    key_database_name => 'acmed_agent_ssh_keys_db'
-  }
 }
