73b43fea55fcdfedb391bbbb8339dc53446dbee4
Mikael Frykholm <mifr@sunet.se>
2024-08-20 09:07:12 +0200

    Backport changes to legacy sunet branch.

--- manifests/pollinate.pp
+++ manifests/pollinate.pp
@@ -5,18 +5,12 @@ define sunet::pollinate($device = '/dev/random') {
     } else {
       apt::ppa {'ppa:ndn/pollen': ensure => absent }
     }
-    package {'pollinate': ensure => installed }
+    package {'pollinate': ensure => absent }
     file { '/etc/default/pollinate':
-      ensure => file,
-      owner  => root,
-      group  => root,
-      content => template('sunet/pollen/pollinate.erb')
+      ensure => absent,
     }
     cron {'repollinate':
-      command => 'pollinate -r',
-      user    => root,
-      hour    => '*',
-      minute  => '0'
+      command => 'pollinate -r', ensure => absent,
     }
   }
 }
