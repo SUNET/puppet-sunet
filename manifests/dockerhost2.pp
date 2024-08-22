a46285bb089e391ce53514c846d7e5d56673937a
Rasmus Thorslund <thorslund@sunet.se>
2024-08-22 11:20:15 +0200

    removed version check in docker_container nagios check for dockerhost2 - via template - final

--- manifests/dockerhost2.pp
+++ manifests/dockerhost2.pp
@@ -10,6 +10,7 @@ class sunet::dockerhost2(
   Variant[String,Boolean] $docker_dns         = lookup('dockerhost_docker_dns', undef, undef,false),
   Boolean $ipv6                               = true,
   Boolean $nat                                = true,
+  Boolean $use_version_check                          = false,
 ) {
 
   $container_name_delimiter = '-'
