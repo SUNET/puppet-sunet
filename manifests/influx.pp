e4b70677649759a489a28175eb0a8fe2d47c0fe6
Patrik Holmqvist <pahol@sunet.se>
2024-09-06 16:12:55 +0200

    Rebuild class to use more modern methods.
    -Use docker-compose instead of docker run
    -Use post hook reload for acme-d certs
    -Use sunet::nftables::allow instead (prepare for dockerhost2)
    -Add $influxdb2_tag to be able to use specific docker build (to not use 'latest')

--- manifests/influx.pp
+++ manifests/influx.pp
@@ -5,23 +5,36 @@
 # @param servername                The fqdn of the server or servicename?
 # @param tcpserveraddress          The version of the influx container to run
 # @param influx_producer_networks  A list of networks allowed to access influx port 8086
+# @param legacy_settings           Set to false to switch to docker-compose, a never acme-d reload method & and dockerhostv2 nftable compability
 class sunet::influx(
   String        $servicename='',
   String        $influxdb_version='latest',
+  String        $influxdb2_tag='ci-docker-influxdb2-292'
   Array[String] $influx_producer_networks,
+  Boolean       $legacy_settings = true,
 ) {
 
-  sunet::docker_run { 'influxdb2':
-    image    => 'docker.sunet.se/docker-influxdb2',
-    imagetag => $influxdb_version,
-    volumes  => [
-      '/etc/letsencrypt/live/:/etc/letsencrypt/live/:ro',
-      '/etc/letsencrypt/archive:/etc/letsencrypt/archive:ro',
-      '/data:/var/lib/influxdb',
-      '/usr/local/bin/backup-influx.sh:/usr/local/bin/backup-influx.sh:ro',
-    ],
-    env      => ["SERVICENAME=${servicename}"],
-    ports    => ['8086:8086'],
+  if $legacy_settings{
+    sunet::docker_run { 'influxdb2':
+      image    => 'docker.sunet.se/docker-influxdb2',
+      imagetag => $influxdb_version,
+      volumes  => [
+        '/etc/letsencrypt/live/:/etc/letsencrypt/live/:ro',
+        '/etc/letsencrypt/archive:/etc/letsencrypt/archive:ro',
+        '/data:/var/lib/influxdb',
+        '/usr/local/bin/backup-influx.sh:/usr/local/bin/backup-influx.sh:ro',
+      ],
+      env      => ["SERVICENAME=${servicename}"],
+      ports    => ['8086:8086'],
+    }
+  } else {
+    sunet::docker_compose { 'influxdb2':
+      content          => template('sunet/influx/docker-compose-influxdb2.yml.erb'),
+      service_name     => 'influxdb2',
+      compose_dir      => '/opt/',
+      compose_filename => 'docker-compose-influxdb2.yml',
+      description      => 'influxdb2',
+    }
   }
 
   # Create a script used to do backups of influxdb
@@ -42,18 +55,37 @@ class sunet::influx(
     warn_criteria => ['exit_status=1', 'max_age=50h'],
   }
 
-  # Cronjob for renewal of acme-d cert
-  sunet::scriptherder::cronjob { 'le_renew':
-    cmd     => '/usr/bin/certbot renew --post-hook "service docker-influxdb2 restart"',
-    special => 'daily',
+  if $legacy_settings{
+    # Cronjob for renewal of acme-d cert
+    sunet::scriptherder::cronjob { 'le_renew':
+      cmd     => '/usr/bin/certbot renew --post-hook "service docker-influxdb2 restart"',
+      special => 'daily',
+    }
+  }
+  else{
+    file {'/etc/letsencrypt/renewal-hooks/post/certbot-acmed-renew-post-hook':
+      ensure  => file,
+      path    => '/etc/letsencrypt/renewal-hooks/post/certbot-acmed-renew-post-hook',
+      mode    => '0500',
+      owner   => 'root',
+      content => template('sunet/influx/certbot-acmed-renew-post-hook')
+    }
   }
 
   # nftables
   # Port 8086 is used to access the influxdb2 container
-  sunet::nftables::docker_expose { 'allow-influxdb2' :
-    allow_clients => $influx_producer_networks,
-    port          => '8086',
-    proto         => 'tcp',
-    iif           => "${interface_default}",
+  if $legacy_settings{
+    sunet::nftables::docker_expose { 'allow-influxdb2' :
+      allow_clients => $influx_producer_networks,
+      port          => '8086',
+      proto         => 'tcp',
+      iif           => "${interface_default}",
+    }
+  } else {
+    sunet::nftables::allow { 'allow-influxdb2-port':
+      from  => $influx_producer_networks,
+      port  => '8086',
+      proto =>  'tcp'
+    }
   }
 }
