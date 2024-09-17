6e2ae7e84300350948a60c06cc372ebeb6c2b293
Johan Wassberg <jocar@sunet.se>
2024-09-17 14:11:27 +0200

    Show deprecation warnings

--- manifests/mariadb.pp
+++ manifests/mariadb.pp
@@ -1,8 +1,7 @@
 # Mariadb cluster definefor SUNET
 define sunet::mariadb(){
-
+  warning('Please transition to the class "sunet::mariadb::server" instead of this define')
   require sunet::mariadb::server
-
 }
 
 # Mariadb cluster class for SUNET
