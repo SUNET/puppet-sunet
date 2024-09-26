dbd322c7b59a2e1a2f2400794df5f889711e4e53
Johan Wassberg <jocar@sunet.se>
2024-09-26 14:30:45 +0200

    Tyop

--- templates/mariadb/backup/start_replica_from_init.erb.sh
+++ templates/mariadb/backup/start_replica_from_init.erb.sh
@@ -4,7 +4,7 @@ init_file='/backups/init.sql.gz'
 if [[ -f ${init_file} ]]; then
 	${mysql} -e "STOP SLAVE;RESET SLAVE;"
 	master_command=$(zgrep 'CHANGE MASTER TO MASTER_LOG_FILE' ${init_file} | sed -e 's/^-- //' -e 's/;$//')
-	master_command="${master_command}, MASTER_HOST='<%= @repliacte_from %>', MASTER_USER='backup'"
+	master_command="${master_command}, MASTER_HOST='<%= @replicate_from %>', MASTER_USER='backup'"
 	master_command="${master_command}, MASTER_PASSWORD='<%= @mariadb_backup_password%>', MASTER_SSL=1"
 	master_command="${master_command}, MASTER_CONNECT_RETRY=20"
 	zcat ${init_file} | ${mysql}
