e84a5a86b611590531df25773c390761aa398416
Micke Nordin <kano@sunet.se>
2024-10-09 13:05:56 +0200

    Fix string concatenation

--- manifests/security/apparmor.pp
+++ manifests/security/apparmor.pp
@@ -7,8 +7,12 @@ class sunet::security::apparmor () {
   package { 'apparmor-utils': ensure => latest }
   if $facts['os']['name'] == 'Debian' and $facts['os']['release']['major'] == '9' {
     exec { 'enable_apparmor_on_Debian_9':
-      command => 'bash -c \'source /etc/default/grub && sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT\"' +
-      '/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT apparmor=1 security=apparmor\"/g" /etc/default/grub && update-grub\'',
+      command => join(
+        [
+          'bash -c \'source /etc/default/grub && sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT\"',
+          '/GRUB_CMDLINE_LINUX_DEFAULT=\"$GRUB_CMDLINE_LINUX_DEFAULT apparmor=1 security=apparmor\"/g" /etc/default/grub && update-grub\''
+        ]
+      ),
       unless  => 'grep -q "apparmor" /etc/default/grub',
     }
   }
