95d7ff55aa167c7b4f01543fb1e4f98909001c97
Micke Nordin <kano@sunet.se>
2024-10-09 11:41:38 +0200

    Fix cloud_init autoload

--- manifests/cloud_init.pp
+++ manifests/cloud_init.pp
@@ -5,24 +5,3 @@ class sunet::cloud_init {
       refreshonly => true
   }
 }
-
-# Define a cloud-init config file
-define sunet::cloud_init::config ($prio = '99', $config = {}) {
-  ensure_resource ('class','sunet::cloud_init', { })
-  file { "/etc/cloud/cloud.cfg.d/${prio}_${title}.cfg":
-      owner   => root,
-      group   => root,
-      content => inline_template("<%= @config.to_yaml %>\n"),
-      notify  => Exec['update-cloud-init']
-  }
-}
-
-# Define a cloud-init config file for the IaaS
-class sunet::cloud_init::iaas_defaults {
-  sunet::cloud_init::config { 'disable_datasources':
-      config => { datasource_list => [ 'None' ] }
-  }
-  sunet::cloud_init::config { 'keep_root_enabled':
-      config => { disable_root => 'false' }
-  }
-}
