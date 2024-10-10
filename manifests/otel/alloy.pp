3bafff888b35f32dd990940fb62a09e0c67316e2
Mikael Frykholm <mifr@sunet.se>
2024-10-10 08:51:16 +0200

    puppet-lint complains.

--- manifests/otel/alloy.pp
+++ manifests/otel/alloy.pp
@@ -4,10 +4,10 @@ class sunet::otel::alloy (
   String $otel_receiver    = undef,
 ) {
   file { '/etc/apt/keyrings' :
-    ensure  => 'directory',
-    notify  => Service['alloy'],
-    mode    => '0644',
-    group   => 'root'
+    ensure => 'directory',
+    notify => Service['alloy'],
+    mode   => '0644',
+    group  => 'root'
   }
   file { '/etc/apt/keyrings/grafana.gpg' :
     ensure  => 'file',
