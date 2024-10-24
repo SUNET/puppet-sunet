ce1f628ec061342fc83dcaa4ec5eef08257a2d43
Maria Haider <mariah@sunet.se>
2024-10-24 20:01:59 +0200

    run the old manifest

--- manifests/rediscluster.pp
+++ manifests/rediscluster.pp
@@ -4,27 +4,9 @@ class sunet::rediscluster(
   Boolean $hostmode = false,
   Optional[Boolean] $tls = false,
   Optional[String] $cluster_announce_ip = '',
-  Optional[Boolean] $automatic_rectify = false,
-  Optional[Boolean] $prevent_reboot = false,
-  Optional[String] $image = 'redis',
-  Optional[String] $tag = '7-bookworm',
 )
 {
-
-  # Allow the user to either specify the variable in cosmos-rules or in hiera
-  if $cluster_announce_ip == '' {
-    $__cluster_announce_ip = lookup('cluster_announce_ip', undef, undef, '')
-  } else {
-    $__cluster_announce_ip = $cluster_announce_ip
-  }
-  # Allow the user to use the explicit string ipaddress or ipaddress6 to use the corresponding facts
-  if $__cluster_announce_ip == 'ipaddress' {
-    $_cluster_announce_ip = $facts['networking']['ip']
-  } elsif $__cluster_announce_ip == 'ipaddress6' {
-    $_cluster_announce_ip = $facts['networking']['ip6']
-  } else {
-    $_cluster_announce_ip = $__cluster_announce_ip
-  }
+  include stdlib
 
   $redis_password = safe_hiera('redis_password')
 
@@ -39,30 +21,6 @@ class sunet::rediscluster(
     ensure  => present,
     content => template('sunet/rediscluster/55-vm-overcommit.conf.erb'),
   }
-  file {'/opt/redis-rectify.sh':
-    ensure  => present,
-    mode    => '0755',
-    content => template('sunet/rediscluster/redis-rectify.sh.erb'),
-  }
-  if $automatic_rectify {
-    sunet::scriptherder::cronjob { 'redis-rectify':
-      cmd           => '/opt/redis-rectify.sh',
-      hour          => '*',
-      minute        => '*/10',
-      ok_criteria   => ['exit_status=0','max_age=2d'],
-      warn_criteria => ['exit_status=1','max_age=3d'],
-    }
-  }
-
-  if $prevent_reboot {
-    include sunet::packages::cowsay
-    file {'/etc/molly-guard/run.d/11-rediscluster':
-      ensure  => present,
-      mode    => '0755',
-      content => template('sunet/rediscluster/11-rediscluster.erb'),
-    }
-  }
-
   range(0, $numnodes - 1).each |$i|{
     $clusterportnum = 16379 + $i
     $redisportnum = 6379 + $i
@@ -74,7 +32,7 @@ class sunet::rediscluster(
       ensure  => present,
       content => template('sunet/rediscluster/server.conf.erb'),
     }
-    if $::facts['sunet_nftables_enabled'] == 'yes' or $::facts['dockerhost_advanced_network'] == 'yes' or $::facts['dockerhost2'] == 'yes' {
+    if $::facts['sunet_nftables_enabled'] == 'yes' or $::facts['dockerhost_advanced_network'] == 'yes' {
       $ports = [$redisportnum, $clusterportnum]
       $ports.each|$port| {
         sunet::nftables::rule { "redis_port_${port}":
@@ -82,13 +40,9 @@ class sunet::rediscluster(
         }
       }
     } else {
-      sunet::misc::ufw_allow { "redis_port_${i}_v6":
-        port => [$redisportnum,$clusterportnum],
-        from => '::/0',
-      }
-      sunet::misc::ufw_allow { "redis_port_${i}_v4":
-        port => [$redisportnum,$clusterportnum],
+      sunet::misc::ufw_allow { "redis_port_${i}":
         from => '0.0.0.0/0',
+        port => [$redisportnum,$clusterportnum],
       }
     }
   }
