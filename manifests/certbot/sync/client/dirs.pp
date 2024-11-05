951d5de198dd85039c9898aa2a3e57ecb873dadd
Johan Wassberg <jocar@sunet.se>
2024-11-05 14:58:21 +0100

    Ensure ownership

--- manifests/certbot/sync/client/dirs.pp
+++ manifests/certbot/sync/client/dirs.pp
@@ -4,21 +4,31 @@ class sunet::certbot::sync::client::dirs(
   file { '/opt/certbot-sync':
     ensure => directory,
     mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
   }
   file { '/opt/certbot-sync/libexec':
     ensure => directory,
     mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
   }
   file { '/opt/certbot-sync/letsencrypt':
     ensure => directory,
     mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
   }
   file { '/opt/certbot-sync/renewal-hooks':
     ensure => directory,
     mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
   }
   file { '/opt/certbot-sync/renewal-hooks/deploy':
     ensure => directory,
     mode   => '0700',
+    owner  => 'root',
+    group  => 'root',
   }
 }
