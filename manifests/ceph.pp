488cc912da3b5b779009fd950c58e897ff0f812d
Micke Nordin <kano@sunet.se>
2024-11-12 12:45:46 +0100

    Add ceph class and packages

---
+++ manifests/ceph.pp
@@ -0,0 +1,48 @@
+# Ceph for SUNET
+class sunet::ceph(
+  String $type,
+)
+{
+  $adm_public_key = lookup('adm_public_key', undef, undef, 'NOT_SET_IN_HIERA');
+  file {'/root/.ssh/':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700',
+  }
+  file {'/root/.ssh/authorized_keys':
+    ensure => 'present',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0600',
+  }
+  file_line { 'adm_public_key':
+    path => '/root/.ssh/authorized_keys',
+    line => $adm_public_key,
+  }
+  include sunet::packages::ceph_common
+  if $type == 'adm' {
+    include sunet::packages::cephadm
+    $adm_private_key = lookup('adm_private_key', undef, undef, 'NOT_SET_IN_HIERA');
+    file {'/root/.ssh/id_rsa_adm':
+      ensure => 'present',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
+    }
+    file_line { 'adm_private_key':
+      path => '/root/.ssh/id_rsa_adm',
+      line => $adm_private_key,
+    }
+  }
+  elsif $type == 'osd' {
+    include sunet::packages::ceph_osd
+  }
+  elsif $type == 'mds' {
+    include sunet::packages::ceph_mds
+  }
+  elsif $type == 'mon' {
+    include sunet::packages::ceph_mon
+  }
+
+}
