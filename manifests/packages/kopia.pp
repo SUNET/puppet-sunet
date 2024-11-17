75620af7e15e7b450327981a8b22dbcf73fd20c2
Micke Nordin <kano@sunet.se>
2024-11-17 14:05:30 +0100

    Use only 16 chars

--- manifests/packages/kopia.pp
+++ manifests/packages/kopia.pp
@@ -1,14 +1,14 @@
 # kopia
 class sunet::packages::kopia {
     apt::key { 'kopia':
-      id     => '7fb99dfd47809f0d5339d7d92273699afd56a556',
+      id     => '2273699afd56a556',
       source => 'https://kopia.io/signing-key'
     }
     -> apt::source {'kopia':
       location => 'http://packages.kopia.io/apt/',
       release  => 'stable',
       repos    => 'main',
-      key      => {'id' => 'fb99dfd47809f0d5339d7d92273699afd56a556'},
+      key      => {'id' => '2273699afd56a556'},
       require  => Apt::Key['kopia'],
     }
     -> package { 'kopia':
