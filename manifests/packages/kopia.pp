2154dcc96ee57cd1f0ffa898a0cdf9a7d6509968
Micke Nordin <kano@sunet.se>
2024-11-17 14:16:19 +0100

    Try keyring

--- manifests/packages/kopia.pp
+++ manifests/packages/kopia.pp
@@ -1,14 +1,15 @@
 # kopia
 class sunet::packages::kopia {
-    apt::key { 'kopia':
-      id     => '2273699afd56a556',
-      source => 'https://kopia.io/signing-key'
+    apt::keyring { 'kopia':
+      ensure   => 'present',
+      source   => 'https://kopia.io/signing-key',
+      filename => 'kopia.io.asc',
     }
     -> apt::source {'kopia':
       location => 'http://packages.kopia.io/apt/',
       release  => 'stable',
       repos    => 'main',
-      key      => {'id' => '2273699afd56a556'},
+      key      => {'id' => '2273699afd56a556', 'source' => 'kopia.io.asc'},
       require  => Apt::Key['kopia'],
     }
     -> package { 'kopia':
