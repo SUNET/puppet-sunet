e3f368862fc6e6a77f528334c24f166139e708bc
Micke Nordin <kano@sunet.se>
2024-11-17 14:29:41 +0100

    Import key

--- manifests/packages/kopia.pp
+++ manifests/packages/kopia.pp
@@ -1,19 +1,27 @@
 # kopia
 class sunet::packages::kopia {
-    apt::keyring { 'kopia':
-      ensure   => 'present',
-      source   => 'https://kopia.io/signing-key',
-      filename => 'kopia.io.asc',
+    file {'/etc/cosmos/apt/keys/kopia-7FB99DFD47809F0D5339D7D92273699AFD56A556.pub':
+        ensure  => file,
+        mode    => '0644',
+        content => template('sunet/packages/kopia-7FB99DFD47809F0D5339D7D92273699AFD56A556.pub.erb'),
+    }
+    -> apt::key { 'kopia':
+      id     => '7FB99DFD47809F0D5339D7D92273699AFD56A556',
+      source => '/etc/cosmos/apt/keys/kopia-7FB99DFD47809F0D5339D7D92273699AFD56A556.pub'
     }
     -> apt::source {'kopia':
       location => 'http://packages.kopia.io/apt/',
       release  => 'stable',
       repos    => 'main',
-      key      => {'id' => '2273699afd56a556', 'source' => 'kopia.io.asc'},
+      key      => {'id' => '7FB99DFD47809F0D5339D7D92273699AFD56A556'},
       require  => Apt::Key['kopia'],
     }
+    -> exec { 'kopia_apt_get_update':
+      command => '/usr/bin/apt-get update',
+      unless  => '/usr/bin/dpkg -l kopia',
+    }
     -> package { 'kopia':
-      ensure   => 'latest',
+      ensure   => 'installed',
       provider => 'apt',
       require  => Apt::Source['kopia'],
     }
