b5b5813da5f65af3399b3cd10be3cc73b1caa88e
Micke Nordin <kano@sunet.se>
2024-11-19 16:51:43 +0100

    condition

--- manifests/kopia/repository.pp
+++ manifests/kopia/repository.pp
@@ -12,6 +12,7 @@ define sunet::kopia::repository(
   exec { "kopia_repository_create_${repository_name}":
     command => "kopia repository create rclone --no-check-for-updates --password=${password} \
     --config-file=${config_file} --remote-path=${remote_path} --log-dir=${repo_dir}",
+    unless  => "test -f ${config_file}",
   }
   exec { "kopia_repository_connect_${repository_name}":
     command => "kopia repository connect rclone --remote-path=${remote_path} --password=${password}  --config-file=${config_file} \
