9cd96b4e26b7dc8bf3bb0176719356c72f0bd12c
Rikard Danielsson <richir@sunet.se>
2024-12-05 13:01:45 +0100

    replacing perl uptime script with a slimmed down python version by kano

--- manifests/nagios/nrpe_check_uptime.pp
+++ manifests/nagios/nrpe_check_uptime.pp
@@ -14,6 +14,6 @@ define sunet::nagios::nrpe_check_uptime (
       content => template('sunet/nagioshost/check_uptime.py.erb'),
   }
   sunet::nagios::nrpe_command {'check_uptime':
-    command_line => "/usr/lib/nagios/plugins/check_uptime.py -w ${uptimew} -c ${uptimec}"
+    command_line => '/usr/lib/nagios/plugins/check_uptime.py'
   }
 }
