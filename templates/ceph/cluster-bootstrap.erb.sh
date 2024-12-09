e033eb4b8786e96d1c9c8c833caacc230cd7a0a8
Micke Nordin <kano@sunet.se>
2024-12-09 13:22:47 +0100

    Add boostrap scripts

---
+++ templates/ceph/cluster-bootstrap.erb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ceph="/usr/sbin/cephadm shell ceph"
+
+ssh "<%= @firstmon>" /opt/ceph/bootstrap.sh
+scp "<%- @firstmon>:/etc/ceph/*" /etc/ceph/
+# <% monitors = [] %>
+# <% osd = [] %>
+# <%- @nodes.each do |node| -%>
+${ceph} orch host add "<%= node['hostname'] %>"
+# <%- node['labels'].each do |label| -%>
+# <%- if label == 'mon' -%>
+# <%- monitors.append(node['hostname']) -%>
+# <%- elsif label == 'osd' -%>
+# <%- osd.append(node['hostname']) -%>
+# <%- endif -%>
+${ceph} orch host label add "<%= node['hostname'] %>" "<%= label %>"
+# <%- end -%>
+# <%- end -%>
+# <%- monitors.each do |mon| -%>
+${ceph} orch apply mon "<%= mon.length() %>"
+${ceph} orch apply mon "<%= mon.join(',') %>"
+# <%- end -%>
+# <%- osd.each do |osd| -%>
+# <%- ['b','c','d','e','f','g','h','i','j','k'].each do |device| -%>
+${ceph} orch daemon add osd "<%= osd %>:/dev/<%= device %>"
+# <%- end -%>
