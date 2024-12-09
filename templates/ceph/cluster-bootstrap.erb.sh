8b6c7deb4eaf9f497aa98e86fdb0db229729e210
Micke Nordin <kano@sunet.se>
2024-12-09 14:07:38 +0100

    Cleanup generated script

--- templates/ceph/cluster-bootstrap.erb.sh
+++ templates/ceph/cluster-bootstrap.erb.sh
@@ -2,25 +2,10 @@
 
 ceph="/usr/sbin/cephadm shell ceph"
 
-ssh "<%= @firstmon %>" /opt/ceph/bootstrap.sh
-scp "<%= @firstmon %>:/etc/ceph/*" /etc/ceph/
-# <% monitors = [] %>
-# <% osd = [] %>
-# <%- @nodes.each do |node| %>
-${ceph} orch host add "<%= node['hostname'] %>"
-# <%- node['labels'].each do |label| %>
-# <%- if label == 'mon' %>
-# <%- monitors.append(node['hostname']) %>
-# <%- elsif label == 'osd' -%>
-# <%- osd.append(node['hostname']) %>
-# <%- end %>
-${ceph} orch host label add "<%= node['hostname'] %>" "<%= label %>"
-# <%- end %>
-# <%- end %>
-${ceph} orch apply mon "<%= monitors.length() %>"
-${ceph} orch apply mon "<%= monitors.join(',') %>"
-# <%- osd.each do |osd| %>
-# <%- ['b','c','d','e','f','g','h','i','j','k'].each do |device| %>
-${ceph} orch daemon add osd "<%= osd %>:/dev/sd<%= device %>"
-# <%- end %>
-# <%- end %>
+ssh "<%= @firstmon %>" /opt/ceph/bootstrap.sh # Run bootstrap script on first monitor
+scp "<%= @firstmon %>:/etc/ceph/*" /etc/ceph/ # Copy over config <% monitors = [] %><% osd = [] %><% @nodes.each do |node| %>
+${ceph} orch host add "<%= node['hostname'] %>" # Add <%= node['hostname'] %><% node['labels'].each do |label| %><% if label == 'mon' %><% monitors.append(node['hostname']) %><% elsif label == 'osd' %><% osd.append(node['hostname']) %><% end %>
+${ceph} orch host label add "<%= node['hostname'] %>" "<%= label %>" # <% end %> <% end %>
+${ceph} orch apply mon "<%= monitors.length() %>" # Number of monitors
+${ceph} orch apply mon "<%= monitors.join(',') %>" # Monitor servers <% osd.each do |osd| %><% ['b','c','d','e','f','g','h','i','j','k'].each do |device| %>
+${ceph} orch daemon add osd "<%= osd %>:/dev/sd<%= device %>" # Add <%= osd %><% end %><% end %>
