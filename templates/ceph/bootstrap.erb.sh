24473ae612b1e533b4b1c13da519c8f6a585e100
Micke Nordin <kano@sunet.se>
2024-12-09 13:26:27 +0100

    Typo

--- templates/ceph/bootstrap.erb.sh
+++ templates/ceph/bootstrap.erb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cephadm bootstrap \
-    --mon-ip "<% @= facts['networking']['ip'] %>" \
+    --mon-ip "<%= @facts['networking']['ip'] %>" \
     --ssh-user root \
     --ssh-private-key /root/.ssh/id_ed25519_adm \
     --allow-overwrite
