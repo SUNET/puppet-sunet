0bd667507516a4a7ec6a2489c09a56716bf2bda7
Micke Nordin <kano@sunet.se>
2024-12-10 13:17:57 +0100

    Use prexxisting keys

--- templates/ceph/bootstrap.erb.sh
+++ templates/ceph/bootstrap.erb.sh
@@ -3,6 +3,8 @@
 cephadm bootstrap \
     --mon-ip "<%= @facts['networking']['ip'] %>" \
     --ssh-user root \
+    --ssh-private-key /root/.ssh/id_ed25519_adm \
+    --ssh-public-key /root/.ssh/id_ed25519_adm.pub \
     --allow-overwrite 
 cephadm shell ceph cephadm get-pub-key > /etc/ceph/ceph.pub
 cephadm shell ceph config-key get mgr/cephadm/ssh_identity_key > /etc/ceph/ceph.key
