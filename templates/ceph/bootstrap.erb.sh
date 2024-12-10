077b65289298b73accb5b99dacd9f6db4cb6ca0a
Micke Nordin <kano@sunet.se>
2024-12-10 13:44:55 +0100

    Allow fqdn

--- templates/ceph/bootstrap.erb.sh
+++ templates/ceph/bootstrap.erb.sh
@@ -5,4 +5,5 @@ cephadm bootstrap \
     --ssh-user root \
     --ssh-private-key /root/.ssh/id_ed25519_adm \
     --ssh-public-key /root/.ssh/id_ed25519_adm.pub \
+    --allow-fqdn-hostname \
     --allow-overwrite 
