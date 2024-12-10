1e5039347b3b93818da703849bc1c07b3e0dad9e
Micke Nordin <kano@sunet.se>
2024-12-10 12:16:31 +0100

    Force v4

--- templates/ceph/cluster-bootstrap.erb.sh
+++ templates/ceph/cluster-bootstrap.erb.sh
@@ -6,8 +6,8 @@ adm_private_key="$(cat /root/.ssh/id_ed25519_adm)"
 adm_public_key="$(ssh-keygen -y -f /root/.ssh/id_ed25519_adm)"
 echo "$adm_public_key" > /root/.ssh/id_ed25519_adm.pub
 
-ssh "<%= @firstmon %>" /opt/ceph/bootstrap.sh # Run bootstrap script on first monitor
-scp "<%= @firstmon %>:/etc/ceph/*" /etc/ceph/ # Copy over config <% monitors = [] %><% osd = [] %><% @nodes.each do |node| %>
+ssh -4 -i /root/.ssh/id_ed25519_adm "<%= @firstmon %>" /opt/ceph/bootstrap.sh # Run bootstrap script on first monitor
+scp -4 -i /root/.ssh/id_ed25519_adm "<%= @firstmon %>:/etc/ceph/*" /etc/ceph/ # Copy over config <% monitors = [] %><% osd = [] %><% @nodes.each do |node| %>
 ${ceph} orch host add "<%= node['hostname'] %>" # Add <%= node['hostname'] %><% node['labels'].each do |label| %><% if label == 'mon' %><% monitors.append(node['hostname']) %><% elsif label == 'osd' %><% osd.append(node['hostname']) %><% end %>
 ${ceph} orch host label add "<%= node['hostname'] %>" "<%= label %>" # <% end %><% end %>
 ceph config-key set mgr/cephadm/ssh_identity_key -i /root/.ssh/id_ed25519_adm
