8e35052b00dda5cb2beb626c2da30ceddf995aa1
Micke Nordin <kano@sunet.se>
2024-12-10 13:34:06 +0100

    Use our keys

--- templates/ceph/cluster-bootstrap.erb.sh
+++ templates/ceph/cluster-bootstrap.erb.sh
@@ -8,8 +8,6 @@ echo "$adm_public_key" > /root/.ssh/id_ed25519_adm.pub
 
 ssh -4 -i /root/.ssh/id_ed25519_adm "<%= @firstmon %>" /opt/ceph/bootstrap.sh # Run bootstrap script on first monitor
 scp -4 -i /root/.ssh/id_ed25519_adm "<%= @firstmon %>:/etc/ceph/*" /etc/ceph/ # Copy over config <% monitors = [] %><% osd = [] %><% @nodes.each do |node| %>
-ssh -F /etc/ceph/ssh_config -i /etc/ceph/ceph.key "root@<%= node['addr'] %>"
-ssh-copy-id -f -i /etc/ceph/ceph.pub "root@<%= node['addr'] %>"
 ${ceph} orch host add "<%= node['hostname'] %>" "<%= node['addr'] %>" # Add <%= node['hostname'] %><% node['labels'].each do |label| %><% if label == 'mon' %><% monitors.append(node['hostname']) %><% elsif label == 'osd' %><% osd.append(node['hostname']) %><% end %>
 ${ceph} orch host label add "<%= node['hostname'] %>" "<%= label %>" # <% end %><% end %>
 
