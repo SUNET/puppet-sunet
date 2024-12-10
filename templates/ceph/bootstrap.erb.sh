7a0bc59616b30b3b623d10b3b35616f76010b178
Micke Nordin <kano@sunet.se>
2024-12-10 12:54:33 +0100

    Fix ssh keys

--- templates/ceph/bootstrap.erb.sh
+++ templates/ceph/bootstrap.erb.sh
@@ -3,5 +3,7 @@
 cephadm bootstrap \
     --mon-ip "<%= @facts['networking']['ip'] %>" \
     --ssh-user root \
-    --allow-overwrite \
-    --output-dir /etc/ceph
+    --allow-overwrite 
+cephadm shell ceph cephadm get-pub-key > /etc/ceph/ceph.pub
+cephadm shell ceph config-key get mgr/cephadm/ssh_identity_key > /etc/ceph/ceph.key
+chmod 600 /etc/ceph/ceph.key
