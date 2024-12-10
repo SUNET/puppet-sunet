263de9bc6a397be10c5acb72fc1da51df597b691
Micke Nordin <kano@sunet.se>
2024-12-10 13:34:06 +0100

    Use our keys

--- templates/ceph/bootstrap.erb.sh
+++ templates/ceph/bootstrap.erb.sh
@@ -6,7 +6,3 @@ cephadm bootstrap \
     --ssh-private-key /root/.ssh/id_ed25519_adm \
     --ssh-public-key /root/.ssh/id_ed25519_adm.pub \
     --allow-overwrite 
-cephadm shell ceph cephadm get-pub-key > /etc/ceph/ceph.pub
-cephadm shell ceph config-key get mgr/cephadm/ssh_identity_key > /etc/ceph/ceph.key
-chmod 600 /etc/ceph/ceph.key
-cephadm shell ceph cephadm get-ssh-config > /etc/ceph/ssh_config
