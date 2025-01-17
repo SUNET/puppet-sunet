a18dce7ef811e52a985eb95ce97f88e44f3e8d9b
Micke Nordin <kano@sunet.se>
2025-01-17 10:59:59 +0100

    Debug

--- manifests/nagios/nrpe_check_uptime.pp
+++ manifests/nagios/nrpe_check_uptime.pp
@@ -1,12 +1,12 @@
 # Check uptime
 define sunet::nagios::nrpe_check_uptime (
-  Integer $xuptimew = 30,
-  Integer $xuptimec = 50,
+  Integer $uptimew = 30,
+  Integer $uptimec = 50,
 ) {
 
-  $_uptimew = lookup('check_uptime_warning', undef, undef, $xuptimew)
-  $_uptimec = lookup('check_uptime_critical', undef, undef, $xuptimec)
 
+  $_uptimew = lookup('check_uptime_warning', undef, undef, $uptimew)
+  $_uptimec = lookup('check_uptime_critical', undef, undef, $uptimec)
   file { '/usr/lib/nagios/plugins/check_uptime.pl' :
       ensure  => 'absent',
   }
@@ -18,6 +18,6 @@ define sunet::nagios::nrpe_check_uptime (
       content => template('sunet/nagioshost/check_uptime.py.erb'),
   }
   sunet::nagios::nrpe_command {'check_uptime':
-    command_line => "/usr/lib/nagios/plugins/check_uptime.py -w ${_uptimew} -c 500"
+    command_line => "/usr/lib/nagios/plugins/check_uptime.py -w ${uptimew} -c ${uptimec}"
   }
 }
