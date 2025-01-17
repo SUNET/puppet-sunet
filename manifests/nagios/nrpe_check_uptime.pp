0a145278668a4117713b8e04dc3f03d419c46f62
Rikard Danielsson <richir@sunet.se>
2025-01-17 09:34:58 +0100

    adding ability to set uptime check values in hiera

--- manifests/nagios/nrpe_check_uptime.pp
+++ manifests/nagios/nrpe_check_uptime.pp
@@ -1,8 +1,20 @@
 # Check uptime
 define sunet::nagios::nrpe_check_uptime (
-  Integer $uptimew         = 30,
-  Integer $uptimec         = 50,
+  Integer $uptimew = 30,
+  Integer $uptimec = 50,
 ) {
+
+  $__uptimew = lookup('check_uptime_warning'),
+  $_uptimew = $__uptimew ? {
+    undef => $uptimew,
+    default => $__uptimew,
+  }
+  $__uptimec = lookup('check_uptime_critical'),
+  $_uptimec = $__uptimec ? {
+    undef => $uptimec,
+    default => $__uptimec,
+  }
+
   file { '/usr/lib/nagios/plugins/check_uptime.pl' :
       ensure  => 'absent',
   }
@@ -14,6 +26,6 @@ define sunet::nagios::nrpe_check_uptime (
       content => template('sunet/nagioshost/check_uptime.py.erb'),
   }
   sunet::nagios::nrpe_command {'check_uptime':
-    command_line => "/usr/lib/nagios/plugins/check_uptime.py -w ${uptimew} -c ${uptimec}"
+    command_line => "/usr/lib/nagios/plugins/check_uptime.py -w ${_uptimew} -c ${_uptimec}"
   }
 }
