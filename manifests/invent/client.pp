73bb9d9905309130641c9ebeb02482b54f3a9094
Rikard Danielsson <richir@sunet.se>
2025-01-23 10:53:41 +0100

    get client script from invent repo

--- manifests/invent/client.pp
+++ manifests/invent/client.pp
@@ -1,30 +1,45 @@
 # SUNET Inventory Service
 class sunet::invent::client(
   String  $invent_dir            = '/opt/invent',
-  String  $export_endpoint        = '',
+  String  $export_endpoint       = '',
   Integer $invent_retention_days = 30,
+  String  $repo_path             = '/var/cache/invent/repo',
+  String  $repo_url              = 'https://github.com/SUNET/invent.git',
 ) {
   $host_os = String($facts['os']['name'], '%d')
-  $awk = $host_os ? {
-    alpine => 'gawk',
-    default => 'awk',
-  }
-  $script_dir = "${invent_dir}/scripts"
 
-  file { $invent_dir:
-    ensure => directory,
+  include sunet::packages::curl
+  include sunet::packages::git
+  include sunet::packages::jq
+
+  exec {'create_repo_path':
+    command => "mkdir -p ${repo_path}",
+    unless  =>  "test -d ${repo_path}",
   }
-  -> file { $script_dir:
-    ensure => directory,
+  -> exec { 'clone_invent_repo':
+    command => "git clone ${repo_url} ${repo_path}",
+    unless  => "test -d ${repo_path}/.git"
   }
-  -> file { "${script_dir}/invent.sh":
-    content => template('sunet/invent/invent.sh.erb'),
-    mode    => '0700',
+  -> exec { 'update_invent_repo':
+    command => "sh -c 'cd ${repo_path} && git pull'"
+  }
+  -> file { "${repo_path}/client/invent.sh":
+    mode   => '0755',
+  }
+  -> file { "/etc/default/invent-client":
+    ensure  => 'file',
+    mode    => '0644',
+    content => template('sunet/invent/invent-client.erb'),
   }
   -> sunet::scriptherder::cronjob { 'inventory':
-    cmd      => "${script_dir}/invent.sh",
-    job_name => 'gather_inventory',
-    user     => 'root',
-    minute   =>  '*/10',
+    cmd         => "${repo_path}/client/invent.sh",
+    job_name    => 'gather_inventory',
+    minute      => '*/10',
+    user        => 'root',
+  }
+  # remove old script path to not get confused over what script is running
+  file { "${invent_dir}/scripts":
+    ensure => 'absent',
+    force  => 'true',
   }
 }
