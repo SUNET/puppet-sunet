538e7bd79e4412db1a5b3b0366e1252d84d40667
Johan Wassberg <jocar@sunet.se>
2025-07-08 15:49:55 +0200

    Allow for PED access

--- manifests/hsm/client.pp
+++ manifests/hsm/client.pp
@@ -1,9 +1,10 @@
 # The HSM client
 class sunet::hsm::client (
-  String $tarball     = '610-000397-015_SW_Linux_Luna_Client_V10.9.0_RevB.tar',
-  String $baseurl     = 'https://sunet.drive.sunet.se/s/4z96YiBA2c3oFo3/download',
-  String $shasum      = '8f5644e0aced01ac5718db41321d0fb6c21c5f9c80dbeda815b39cb735139e2f',
-  Array  $hsm_servers = ['tug-hsm-lab2.sunet.se'],
+  String $tarball               = '610-000397-015_SW_Linux_Luna_Client_V10.9.0_RevB.tar',
+  String $baseurl               = 'https://sunet.drive.sunet.se/s/4z96YiBA2c3oFo3/download',
+  String $shasum                = '8f5644e0aced01ac5718db41321d0fb6c21c5f9c80dbeda815b39cb735139e2f',
+  Array  $hsm_servers           = ['tug-hsm-lab2.sunet.se'],
+  Array  $allow_remote_ped_from = [],
 ) {
 
   include sunet::packages::alien
@@ -52,5 +53,12 @@ class sunet::hsm::client (
 
   exec { '/opt/hsmclient/libexec/configure-luna':
     onlyif =>  'test `grep  sunet.se /etc/Chrystoki.conf |wc -l` -eq 0',
+  }
+
+  if ($allow_remote_ped_from) {
+    sunet::nftables::allow{ "ped_${client}":
+      from => $allow_remote_ped_from,
+      port => 1503,
+    }
   }
 }
