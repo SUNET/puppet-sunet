6239d63d4f3f7f29502146376b4c36a658da65b0
Johan Wassberg <jocar@sunet.se>
2025-07-09 10:35:55 +0200

    Check to monitor the HSMs

--- manifests/hsm/client.pp
+++ manifests/hsm/client.pp
@@ -1,10 +1,11 @@
 # The HSM client
 class sunet::hsm::client (
-  String $tarball               = '610-000397-015_SW_Linux_Luna_Client_V10.9.0_RevB.tar',
-  String $baseurl               = 'https://sunet.drive.sunet.se/s/4z96YiBA2c3oFo3/download',
-  String $shasum                = '8f5644e0aced01ac5718db41321d0fb6c21c5f9c80dbeda815b39cb735139e2f',
-  Array  $hsm_servers           = ['tug-hsm-lab2.sunet.se'],
-  Array  $allow_remote_ped_from = [],
+  String  $tarball               = '610-000397-015_SW_Linux_Luna_Client_V10.9.0_RevB.tar',
+  String  $baseurl               = 'https://sunet.drive.sunet.se/s/4z96YiBA2c3oFo3/download',
+  String  $shasum                = '8f5644e0aced01ac5718db41321d0fb6c21c5f9c80dbeda815b39cb735139e2f',
+  Array   $hsm_servers           = ['tug-hsm-lab2.sunet.se'],
+  Array   $allow_remote_ped_from = [],
+  Boolean $monitor               = false,
 ) {
 
   include sunet::packages::alien
@@ -68,4 +69,21 @@ class sunet::hsm::client (
       notify => Service['ssh'],
     }
   }
+  if ($monitor) {
+    $pin = lookup('partition_pin', undef, undef, 'NOT_SET_IN_HIERA');
+    file { '/root/hsm-partition-pin':
+      ensure  => 'file',
+      mode    => '0700',
+      owner   => 'root',
+      content => template('sunet/hsm/hsm-partition-pin.erb')
+    }
+
+    file { '/usr/lib/nagios/plugins/check_hsm':
+      ensure  => 'file',
+      mode    => '0755',
+      owner   => 'root',
+      content => file('sunet/hsm/check-hsm.sh')
+    }
+  }
+
 }
