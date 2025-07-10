413d7ec86279ccdf94773cca55d6197a48d14b69
Johan Wassberg <jocar@sunet.se>
2025-07-10 15:09:35 +0200

    Puppet-lint

--- manifests/hsm/client.pp
+++ manifests/hsm/client.pp
@@ -57,7 +57,7 @@ class sunet::hsm::client (
   }
 
   if ($allow_remote_ped_from) {
-    sunet::nftables::allow{ "ped":
+    sunet::nftables::allow{ 'ped':
       from => $allow_remote_ped_from,
       port => 1503,
     }
