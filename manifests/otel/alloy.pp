66cb1d973baa8e692f7b02368ce64036a8cafe67
Johan Wassberg <jocar@sunet.se>
2025-08-15 13:02:29 +0200

    Only expose ports to docker network if docker is available

--- manifests/otel/alloy.pp
+++ manifests/otel/alloy.pp
@@ -44,12 +44,15 @@ class sunet::otel::alloy (
     enable  => 'true',
     require => Package['alloy'],
   }
-  sunet::nftables::docker_expose { 'allow_local_opentelemetry_grpc' :
-    allow_clients => '172.16.0.0/12',
-    port          => '4317',
-  }
-  sunet::nftables::docker_expose { 'allow_local_opentelemetry_http' :
-    allow_clients => '172.16.0.0/12',
-    port          => '4318',
+
+  if $facts['dockerhost2'] == 'yes' {
+    sunet::nftables::docker_expose { 'allow_local_opentelemetry_grpc' :
+      allow_clients => '172.16.0.0/12',
+      port          => '4317',
+    }
+    sunet::nftables::docker_expose { 'allow_local_opentelemetry_http' :
+      allow_clients => '172.16.0.0/12',
+      port          => '4318',
+    }
   }
 }
