0dd2954a191da4aedfa41ff195243328d0a82790
Johan Wassberg <jocar@sunet.se>
2025-09-09 16:40:58 +0200

    We only run stable nowadays

--- manifests/apt/repo_docker.pp
+++ manifests/apt/repo_docker.pp
@@ -1,9 +1,14 @@
 # The apt repo for docker
 define sunet::apt::repo_docker (
 ) {
+
+  $distro = $facts['os']['distro']['id']
+  $lc_distro = downcase($distro)
+  $release = $facts['os']['distro']['release']['major']
+
   apt::source { 'docker':
     location => "https://download.docker.com/linux/${lc_distro}",
-    repos    => $docker_repo,
+    repos    => 'stable',
     key      => {
       name    => 'docker.asc',
       content => file('sunet/apt/docker.asc'),
