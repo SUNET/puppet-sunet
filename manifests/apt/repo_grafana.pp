557b0392637dbf6357fc5dbcda1801c1c8c1a85c
Johan Wassberg <jocar@sunet.se>
2025-09-15 15:18:39 +0200

    Old dists requires id

--- manifests/apt/repo_grafana.pp
+++ manifests/apt/repo_grafana.pp
@@ -2,16 +2,34 @@
 # E.g used for alloy and grafana
 define sunet::apt::repo_grafana (
 ) {
+
+  $distro = $facts['os']['distro']['id']
+  $release = $facts['os']['distro']['release']['major']
+
+  if ($distro == 'Debian' and versioncmp($release, '12') <= 0) or ($distro == 'Ubunutu' and versioncmp($release, '22.04') <= 0) {
+
+    apt::source { 'grafana':
+        location => 'https://apt.grafana.com',
+        repos    => 'main',
+        release  => 'stable',
+        key      => {
+          id      => 'B53AE77BADB630A683046005963FA27710458545',
+          name    => 'grafana.gpg',
+          content => file('sunet/apt/grafana.gpg'),
+        }
+    }
+
+  } else {
   apt::source { 'grafana':
       location => 'https://apt.grafana.com',
       repos    => 'main',
       release  => 'stable',
       key      => {
-        id      => 'B53AE77BADB630A683046005963FA27710458545',
         name    => 'grafana.gpg',
         content => file('sunet/apt/grafana.gpg'),
       }
   }
+
 
   $origins_from_template = '/etc/apt/apt.conf.d/51unattended-upgrades-origins'
   # This might be a race on first run depending on exection order but it's the best I can come up with at the time. Sorry.
