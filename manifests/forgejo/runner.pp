e0a75f72ec68cd5412c0bde4c7102b4ed883784b
Johan Wassberg <jocar@sunet.se>
2025-10-06 13:20:15 +0200

    Establish trust

--- manifests/forgejo/runner.pp
+++ manifests/forgejo/runner.pp
@@ -2,6 +2,7 @@
 class sunet::forgejo::runner (
   String $version           = '11.1.2',
   String $version_sha256sum = '6442d46db2434a227e567a116c379d0eddbe9e7a3f522596b25d31979fd59c8d',
+  String $machine_image_url = '',
 ) {
 
   file {'/opt/forgejo-runner':
@@ -15,11 +16,20 @@ class sunet::forgejo::runner (
     ensure  => 'directory',
   }
 
+  file {'/opt/forgejo-runner/trust':
+    ensure  => 'directory',
+  }
+
   file { '/opt/forgejo-runner/bin/forgejo-runner':
     source         => "https://code.forgejo.org/forgejo/runner/releases/download/v${version}/forgejo-runner-${version}-linux-amd64",
     checksum       => 'sha256',
     checksum_value => $version_sha256sum,
     mode           => '0755',
+    ensure         => 'file',
+  }
+
+  file { '/opt/forgejo-runner/trust/fedora.gpg':
+    source         => file('sunet/forgejo/fedora.gpg'),
     ensure         => 'file',
   }
 
