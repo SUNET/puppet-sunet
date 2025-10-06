f9150a700628b47244242be4363f7ea31adf1ed5
Johan Wassberg <jocar@sunet.se>
2025-10-06 13:36:59 +0200

    Verify image

--- manifests/forgejo/runner.pp
+++ manifests/forgejo/runner.pp
@@ -42,4 +42,9 @@ class sunet::forgejo::runner (
     source         => "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/${machine_version}/x86_64/fedora-coreos-${machine_version}-qemu.x86_64.qcow2.xz",
   }
 
+  exec { "verify image":
+    command     => '/usr/bin/gpgv --keyring /opt/forgejo-runner/trust/fedora.gpg /opt/forgejo-runner/images/unverified/fedora-coreos-${machine_version}-qemu.x86_64.qcow2.xz',
+    refreshonly => true,
+  }
+
 }
