0d19d3169bb040ec586df3a89bf953b84c676b53
Johan Wassberg <jocar@sunet.se>
2025-10-06 13:28:01 +0200

    Download the image in where we will run runner

--- manifests/forgejo/runner.pp
+++ manifests/forgejo/runner.pp
@@ -2,7 +2,7 @@
 class sunet::forgejo::runner (
   String $version           = '11.1.2',
   String $version_sha256sum = '6442d46db2434a227e567a116c379d0eddbe9e7a3f522596b25d31979fd59c8d',
-  String $machine_image_url = '',
+  String $machine_version = '42.20250914.3.0',
 ) {
 
   file {'/opt/forgejo-runner':
@@ -15,6 +15,10 @@ class sunet::forgejo::runner (
   file {'/opt/forgejo-runner/images':
     ensure  => 'directory',
   }
+  file {'/opt/forgejo-runner/images/unverified':
+    ensure  => 'directory',
+  }
+
 
   file {'/opt/forgejo-runner/trust':
     ensure  => 'directory',
@@ -31,6 +35,12 @@ class sunet::forgejo::runner (
   file { '/opt/forgejo-runner/trust/fedora.gpg':
     ensure => 'file',
     content => file('sunet/forgejo/fedora.gpg'),
+  }
+
+  file { '/opt/forgejo-runner/bin/forgejo-runner':
+    ensure         => 'file',
+    source         => "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/${machine_version}/x86_64/fedora-coreos-${machine_version}-qemu.x86_64.qcow2.xz",
+    checksum_value => $version_sha256sum,
   }
 
 }
