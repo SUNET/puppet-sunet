d8864dfdc1fab1bd22c48d4e68af788cd9f36e11
Johan Wassberg <jocar@sunet.se>
2025-10-17 16:04:45 +0200

    There is no tty in cron

--- files/geteduroam/geteduroam-db-thinning.sh
+++ files/geteduroam/geteduroam-db-thinning.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for colum in expires revoked; do
-	docker exec -it mariadb-db-1 mysql -p"$(puppet lookup --render-as s mariadb_root_password 2>/dev/null)" geteduroam -e "delete from realm_signing_log WHERE ${colum} <= DATE_SUB(NOW(),INTERVAL 6 MONTH);"
+	docker exec mariadb-db-1 mysql -p"$(puppet lookup --render-as s mariadb_root_password 2>/dev/null)" geteduroam -e "delete from realm_signing_log WHERE ${colum} <= DATE_SUB(NOW(),INTERVAL 6 MONTH);"
 	sleep 10
 done
