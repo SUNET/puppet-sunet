c395432d55d283cb7d7f9f2c2983e9b131c6ab0a
Johan Wassberg <jocar@sunet.se>
2025-10-27 11:10:10 +0100

    We do need realm

--- manifests/geteduroam/radius.pp
+++ manifests/geteduroam/radius.pp
@@ -7,6 +7,7 @@ class sunet::geteduroam::radius(
   String $haproxy_tag = '3.0.2',
   Array $app_admins = [],
   Boolean $qa_federation = false,
+  String $realm,
 ){
 
   class {'sunet::geteduroam::common': }
