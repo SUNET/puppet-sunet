003d6ee1b1c46a4efafa9b7ad940363043c6b0c1
Joao Paulo Oliveira de Araujo Rangel Pamplona <paulo@nordu.net>
2025-12-05 12:30:31 +0100

    syntax fixing and added logic to enable NFT for Argus

--- manifests/argus/server.pp
+++ manifests/argus/server.pp
@@ -2,10 +2,12 @@
 # @param docker_image    Specific Argus image
 # @param docker_tag      Specific Argus image tag
 # @param url             Specific hostname where Argus will be running
+# @param argus_clients   Hiera Variable that contains list of prefixes allowed to reach Argus
 class sunet::argus::server (
-  String  $docker_image = "docker.sunet.se/sunet/argus-api",
-  String  $docker_tag   = "v2.5.0_sunetbuild",
-  String  $url          = $facts['networking']['hostname']
+  String  $docker_image     = "docker.sunet.se/sunet/argus-api",
+  String  $docker_tag       = "v2.5.0_sunetbuild",
+  String  $url              = $facts['networking']['hostname'],
+  String  $argus_clients    = ''
 ){
 
     $argus_config_dir = '/opt/argus_config'
@@ -25,7 +27,7 @@ class sunet::argus::server (
         ensure  => file,
         group   => 'root',
         mode    => '0755',
-        content => template('argus/localsettings.py.erb'),
+        content => template('sunet/argus/localsettings.py.erb'),
         require => File[$argus_config_dir]
     }
 
@@ -33,7 +35,7 @@ class sunet::argus::server (
         ensure  => file,
         group   => 'root',
         mode    => '0755',
-        content => template('argus/default.conf.erb'),
+        content => template('sunet/argus/default.conf.erb'),
         require => File[$nginx_config_dir]
     }
 
@@ -42,7 +44,19 @@ class sunet::argus::server (
         description      => 'Argus Alarm Aggregator for SUNET CNaaS',
         compose_filename => 'docker-compose.yml',
         compose_dir      => '/opt',
-        content          => template('argus/docker-compose.yml.erb'),
+        content          => template('sunet/argus/docker-compose.yml.erb'),
+    }
+
+    if $argus_clients != '' {
+        $argus_allow_networks = hiera_array($argus_clients,[])
+        $argus_interface = safe_hiera('argus_interface',$facts['interface_default'])
+        sunet::nftables::docker_expose { 'allow_https' :
+            allow_clients   => $argus_allow_networks,
+            port            => '443',
+            iif             => $argus_interface,
+        }
+    } else  {
+        warning('No configured Client IPs for argus. Not allowing HTTPS access for anyone.')
     }
 
 }
