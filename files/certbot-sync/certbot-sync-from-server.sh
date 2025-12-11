20a07eb5893beed766475ff81b667731a5a3a9b8
Johan Wassberg <jocar@sunet.se>
2025-12-11 13:41:05 +0100

    `shellcheck`
    and a bit of Sonar

--- files/certbot-sync/certbot-sync-from-server.sh
+++ files/certbot-sync/certbot-sync-from-server.sh
@@ -7,7 +7,8 @@ syncdir="$basedir/letsencrypt"
 hookdir="$basedir/renewal-hooks/deploy"
 
 config=${basedir}/conf/certbot-sync-from-server.source
-if [ -f "${config}" ]; then
+if [[ -f "${config}" ]]; then
+  # shellcheck source=/dev/null
 	. ${config}
 else
 	echo "No config (${config}) found!"
@@ -15,17 +16,17 @@ else
 fi
 
 bootstrap=0
-if [ ! -d $syncdir/live ]; then
+if [[ ! -d $syncdir/live ]]; then
 	bootstrap=1
 fi
 
-if [ "${bootstrap}" == 1 ]; then
+if [[ "${bootstrap}" == 1 ]]; then
 	targetdir=$syncdir
 else
 	targetdir=$(mktemp -d /tmp/certbot_sync.XXXXX)
 fi
 
-if [ "${CERTBOT_SYNC_SERVER}" = "$(hostname -f)" ]; then
+if [[ "${CERTBOT_SYNC_SERVER}" = "$(hostname -f)" ]]; then
 	rsync -az -v /etc/letsencrypt/ "${targetdir}"
 else
 	rsync -e "ssh -i $HOME/.ssh/id_certbot_sync_client" -az -v root@"${CERTBOT_SYNC_SERVER}": "${targetdir}"
@@ -34,15 +35,15 @@ fi
 certs_to_deploy=()
 for file_system_entity in "${targetdir}"/live/*; do
 	basename=$(basename "${file_system_entity}")
-	if [ "$basename" == "README" ]; then
+	if [[ "$basename" == "README" ]]; then
 		continue
 	fi
 	# Everthing is new - deploy all!
-	if [ "${bootstrap}" == 1 ]; then
+	if [[ "${bootstrap}" == 1 ]]; then
 		certs_to_deploy+=("${basename}")
 	else
 		# Cert exists…
-		if [ -d "$syncdir/live/${basename}" ]; then
+		if [[ -d "$syncdir/live/${basename}" ]]; then
 			# … but is it changed?
 			if ! cmp -s "$syncdir/live/${basename}/fullchain.pem" "${targetdir}/live/${basename}/fullchain.pem"; then
 				# Yes, changed! - deploy!
@@ -55,13 +56,13 @@ for file_system_entity in "${targetdir}"/live/*; do
 	fi
 done
 
-if [ "${bootstrap}" == 0 ]; then
+if [[ "${bootstrap}" == 0 ]]; then
 	rsync -av --delete "${targetdir}/" $syncdir
 	rm -vrf "${targetdir}"
 fi
 
 for cert in "${certs_to_deploy[@]}"; do
-	for hook in $hookdir/*; do
+	for hook in "$hookdir"/*; do
 		echo "Running deploy hook ${hook} for ${cert}."
 		RENEWED_LINEAGE="$syncdir/live/${cert}" "${hook}"
 	done
