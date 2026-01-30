f64d0bfa721e0abb66fd389589a7ede63b372ec2
Patrik Holmqvist <pahol@sunet.se>
2026-01-30 15:36:12 +0100

    Initial commit

---
+++ manifests/pgbackrest/repo.pp
@@ -0,0 +1,95 @@
+# This puppet manifest is used to configure a pgBackRest repo/backup server
+
+# @param retention_full_days           The number of days to save full backups
+# @param pg_user                       The user that the repo/backup server uses towards the DB nodes
+# @param schedule_full                 How often should we run full backups
+# @param schedule_incr                 How often should we run incremental backups
+# @param schedule_diff                 How often should we run differential backups
+class sunet::pgbackrest::repo (
+  String                $pg_stanza,
+  Integer               $retention_full_days=14,
+  String                $pg_user="postgres",
+  String                $pg_command="/usr/local/bin/pgbackrest-docker"
+  Enum['daily','none']  $schedule_full='daily',
+  Enum['hourly','none'] $schedule_incr='none',
+  Enum['hourly','none'] $schedule_diff='none',
+) {
+
+  # Install pgBackRest
+  include sunet::packages::pgbackrest
+
+  # Get the db node information from parameter or hiera
+  $db_nodes = lookup('db_nodes', hash, undef, {})
+
+  $db_nodes.each | $db_node, $value| {
+    # Allow inbound SSH used by DB nodes to send backups
+    sunet::nftables::allow { "allow-ssh-${db_node}":
+      from => $value['prefixes'],
+      port => "22",
+    }
+
+    # Accept hostkeys so SSH towards the db nodes does not hang
+    sunet::ssh_keyscan::host {"${db_node}": }
+  }
+
+  # Accept SSH-keys for DB nodes
+  sunet::ssh_keys { 'pg_db_nodes':
+    config => safe_hiera('pg_db_nodes_ssh_keys', {}),
+	  key_database_name => 'pg_db_nodes_db',
+  }
+
+  # Generate SSH-key used to access DB nodes
+  $key_path = '/root/.ssh/id_id_ed25519'
+  if lookup('pgbackrest_ssh_key', undef, undef, undef) { # Key is in secrets, write it to host
+    ensure_resource('sunet::snippets::secret_file', $key_path, {
+    hiera_key => 'pgbackrest_ssh_key',
+  })
+  } else {
+    if (!find_file($key_path)){
+      sunet::snippets::ssh_keygen{$key_path:} # This will not overwrite an existing key
+    }
+  }
+
+  # Create the config file for pgbackrest
+  file { '/etc/pgbackrest.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('sunet/pgbackrest/pgbackrest.conf.erb'),
+  }
+
+  case $schedule_full {
+    'daily': {
+      sunet::scriptherder::cronjob { 'pgbackrest_full_backup':
+        cmd         => "pgbackrest --stanza=${pg_stanza} --type=full backup",
+        minute      => '10',
+        hour        => '4',
+        ok_criteria => ['exit_status=0', 'max_age=1d'],
+      }
+    }
+  }
+
+  case $schedule_incr {
+    'hourly': {
+      sunet::scriptherder::cronjob { 'pgbackrest_incr_backup':
+        cmd         => "pgbackrest --stanza=${pg_stanza} --type=incr backup",
+        minute      => '10',
+        hour        => '0,1,2,3,5-23',
+        ok_criteria => ['exit_status=0', 'max_age=1d'],
+      }
+    }
+  }
+
+  case $schedule_diff {
+    'hourly': {
+      sunet::scriptherder::cronjob { 'pgbackrest_diff_backup':
+        cmd         => "pgbackrest --stanza=${pg_stanza} --type=diff backup",
+        minute      => '10',
+        hour        => '0,1,2,3,5-23',
+        ok_criteria => ['exit_status=0', 'max_age=1d'],
+      }
+    }
+  }
+
+}
