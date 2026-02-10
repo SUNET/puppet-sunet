cc3d6f02f453cc46a0df32c5f7aed5d016fe961c
Johan Wassberg <jocar@sunet.se>
2026-02-10 08:34:04 +0100

    Syntax error

--- files/mastodon/www/thinning.sh
+++ files/mastodon/www/thinning.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-/usr/local/bin/tootctl media remove -days 7 --verbose
-/usr/local/bin/tootctl media remove -days 7 --verbose --prune-profiles
-/usr/local/bin/tootctl media remove -days 7 --verbose --remove-headers
+/usr/local/bin/tootctl media remove --days 7 --verbose
+/usr/local/bin/tootctl media remove --days 7 --verbose --prune-profiles
+/usr/local/bin/tootctl media remove --days 7 --verbose --remove-headers
 
 /usr/local/bin/tootctl preview_cards remove --days 30 --verbose
