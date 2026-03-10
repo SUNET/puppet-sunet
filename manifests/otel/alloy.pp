8e978bf27ba68cdf62230a317970a960eb9ddb25
Mikael Frykholm <mifr@sunet.se>
2026-03-10 13:04:45 +0100

    Pin to specific version.

--- manifests/otel/alloy.pp
+++ manifests/otel/alloy.pp
@@ -4,6 +4,7 @@
 class sunet::otel::alloy (
   String  $otel_receiver    = undef,
   Boolean $enabled=true,
+  String  $version='1.14.0-1'
 ) {
 
   if $enabled {
@@ -14,7 +15,7 @@ class sunet::otel::alloy (
       unless  => 'dpkg -l alloy',
     }
     package { 'alloy':
-      ensure => 'installed',
+      ensure => $version,
     }
     file { '/etc/alloy' :
       ensure => 'directory',
