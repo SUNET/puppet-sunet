4fdd1779d03fde9e87aad0189290f9828ad9ce3c
Johan Wassberg <jocar@sunet.se>
2026-03-27 16:27:00 +0100

    Intitial commit of orchestrator
    Things will not work

---
+++ manifests/forgejo/runner/orchestrator.pp
@@ -0,0 +1,59 @@
+# A class to install and manage Forgejo runner(s)
+class sunet::forgejo::runner::orchestrator (
+  String $version           = '12.6.4',
+  String $version_sha256sum = 'fe83c5d5fffbbb81f2f8d93f4638d098ad9c08b77aa6b5035856ae9643d25684',
+  Integer $runners = 4,
+  String $forgejo_instance  =  'platform.sunet.se',
+) {
+
+  $registration_token = lookup('forgejo_registration_token', undef, undef, 'NOT_SET_IN_HIERA');
+
+  file {'/opt/forgejo-runner-orchestrator':
+    ensure => 'directory',
+  }
+  file {'/opt/forgejo-runner-orchestrator/bin':
+    ensure => 'directory',
+  }
+
+  file {'/opt/forgejo-runner-orchestrator/libexec':
+    ensure => 'directory',
+  }
+
+  file { "/opt/forgejo-runner/bin/forgejo-runner-${version}":
+    ensure         => 'file',
+    source         => "https://code.forgejo.org/forgejo/runner/releases/download/v${version}/forgejo-runner-${version}-linux-amd64",
+    checksum       => 'sha256',
+    checksum_value => $version_sha256sum,
+    mode           => '0755',
+  }
+  file { '/opt/forgejo-runner/bin/forgejo-runner':
+    ensure => link,
+    target => "/opt/forgejo-runner/bin/forgejo-runner-${version}",
+  }
+
+  file { '/opt/forgejo-runner/libexec/runner-orchestrator':
+    ensure  => 'file',
+    content => template('sunet/forgejo/runner-orchestrator.erb'),
+    mode    => '0755',
+  }
+
+  file { '/opt/forgejo-runner/libexec/runner-wrapper':
+    ensure  => 'file',
+    content => template('sunet/forgejo/runner-wrapper.2.0.erb'),
+    mode    => '0755',
+  }
+
+  file { '/etc/systemd/system/sunet-forgejo-runner@.service':
+    ensure  => 'file',
+    content => file('sunet/forgejo/forgejo-runner-2.0.service'),
+    mode    => '0644',
+  }
+
+  range(0, $runners - 1).each |$runner|{
+
+    service { "sunet-forgejo-runner@runner-${runner}":
+      ensure => 'running',
+      enable => true,
+    }
+  }
+}
