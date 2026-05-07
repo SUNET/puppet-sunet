e92006c11e1870c6e30f7e851ecb8dd1284f9b0a
Johan Wassberg <jocar@sunet.se>
2026-05-07 15:27:15 +0200

    Improved comments

--- manifests/packages/python3_cryptography.pp
+++ manifests/packages/python3_cryptography.pp
@@ -1,4 +1,4 @@
-# alien
+# python3-cryptography
 class sunet::packages::python3_cryptography {
     package { 'python3-cryptography': ensure => installed }
 }
--- manifests/packages/python3_josepy.pp
+++ manifests/packages/python3_josepy.pp
@@ -1,4 +1,4 @@
-# alien
+# python3-josepy
 class sunet::packages::python3_josepy {
     package { 'python3-josepy': ensure => installed }
 }
