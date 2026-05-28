f6446a9a68c199815894cefdc496cb193cf291e2
Patrik Holmqvist <pahol@sunet.se>
2026-05-28 17:04:37 +0200

    Allign with testing branch so we can merge with testing

--- manifests/grafana.pp
+++ manifests/grafana.pp
@@ -6,17 +6,18 @@
 # @param grafana_version           The version of the grafana container to run
 # @param grafana_webuser_networks  A list of networks allowed to grafana web on port 80/443
 class sunet::grafana(
+  Array[String] $grafana_webuser_networks,
   String        $servicename='',
   String        $grafana_version='latest',
-  Array[String] $grafana_webuser_networks,
 ) {
 
   sunet::system_user {'grafana': username => 'grafana', group => 'grafana' }
   $smtp_password = safe_hiera('smtp_password', 'NOT_SET')
   file { '/etc/grafana': ensure => 'directory', owner => 'grafana' }
-  file { '/var/log/grafana': ensure => 'directory', mode => '775', owner => 'grafana' }
-  file { '/var/lib/grafana': ensure => 'directory', mode => '775', owner => 'grafana' }
-  file { '/usr/share/grafana': ensure => 'directory', mode => '775', owner => 'grafana' }
+  file { '/etc/grafana': ensure => 'directory', owner => 'grafana' }
+  file { '/var/log/grafana': ensure => 'directory', mode => '0775', owner => 'grafana' }
+  file { '/var/lib/grafana': ensure => 'directory', mode => '0775', owner => 'grafana' }
+  file { '/usr/share/grafana': ensure => 'directory', mode => '0775', owner => 'grafana' }
 
   file {'/etc/grafana/grafana.ini':
       ensure  => file,
@@ -33,8 +34,8 @@ class sunet::grafana(
       content => template('sunet/grafana/grafana2influxdb.yaml.erb')
   }
 
-  sunet::docker_run {"grafana":
-      hostname => "${servicename}",
+  sunet::docker_run {'grafana':
+      hostname => $servicename,
       image    => 'docker.sunet.se/docker-grafana',
       volumes  => [
                   '/var/lib/grafana:/var/lib/grafana',
@@ -56,12 +57,15 @@ class sunet::grafana(
   }
 
   # nftables
-  sunet::nftables::allow { 'allow-http':
-    from => $grafana_webuser_networks,
-    port => 80,
+  sunet::nftables::docker_expose { 'allow_http' :
+    allow_clients => $grafana_webuser_networks,
+    port          => '80',
+    iif           => $facts['interface_default'],
   }
-  sunet::nftables::allow { 'allow-https':
-    from => $grafana_webuser_networks,
-    port => 443,
+
+  sunet::nftables::docker_expose { 'allow_https' :
+    allow_clients => $grafana_webuser_networks,
+    port          => '443',
+    iif           => $facts['interface_default'],
   }
 }
