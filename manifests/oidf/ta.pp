f640051387384544c5c8ce6bea28923bc61dbac1
Björn Mattsson <bjorn@sunet.se>
2026-05-29 15:55:04 +0200

    Added support for acmed and creation of configfiles / keys

--- manifests/oidf/ta.pp
+++ manifests/oidf/ta.pp
@@ -1,7 +1,8 @@
 # Wrapper to setup a OpenIDFed TA host
 class sunet::oidf::ta(
   String $hostname              = undef,
-  String $inmor_version         = undef
+  String $inmor_version         = undef,
+  Optional[Boolean] $acmed      = true
 ){
   sunet::docker_compose { 'oidf-ta':
     content          => template('sunet/oidf/docker-compose-ta.yml.erb'),
@@ -9,7 +10,28 @@ class sunet::oidf::ta(
     compose_dir      => '/opt/',
     compose_filename => 'docker-compose.yml',
     description      => 'OpenIDFed TA',
+  } ->  file { '/opt/oidf-ta/taconfig.toml':
+    content => template('sunet/oidf/taconfig.toml.erb'),
+    mode    => '0755',
+    replace => false,
+  } -> file { '/opt/oidf-ta/localsettings.py':
+    content => template('sunet/oidf/localsettings.py.erb'),
+    mode    => '0755',
+    replace => false,
+  } -> if lookup('inmor_key', undef, undef, undef) != undef {
+    sunet::snippets::secret_file { '/opt/oidf-ta/private.json':
+      hiera_key => 'inmor_key',
+      mode      => '0644',
+    }
+    # assume cert is in cosmos repo
+  } else {
+    exec { 'generate_privat_public_keys':
+      command => "mkdir -p /opt/oidf-ta/keys/publickeys; chmod -R 777 /opt/oidf-ta/keys; touch /opt/oidf-ta/keys/private.json ; docker run --rm -v /opt/oidf-ta/keys:/app/keys docker.sunet.se/inmor:${inmor_version} /app/inmor-keygeneration --type RS256 --output /app/keys --force && mv /opt/oidf-ta/keys/publickeys/* /opt/oidf-ta/publickeys && mv /opt/oidf-ta/keys/private.json /opt/oidf-ta/",
+      cwd     => '/opt/oidf-ta',
+      creates => '/opt/oidf-ta/private.json'
+    }
   }
+
   file { '/opt/oidf-ta/updateKeys.bash':
     ensure  => 'file',
     mode    => '0700',
@@ -19,7 +41,13 @@ class sunet::oidf::ta(
     minute        => '*/5',
     ok_criteria   => ['exit_status=0', 'max_age=1h'],
     warn_criteria => ['exit_status=1', 'max_age=2h'],
+  } -> sunet::scriptherder::cronjob { 'updateCollection':
+    cmd           => "docker exec oidf-ta-ta-1 /app/inmor-collection --config /app/taconfig.toml https://${hostname}",
+    minute        => '*/30',
+    ok_criteria   => ['exit_status=0', 'max_age=1h'],
+    warn_criteria => ['exit_status=1', 'max_age=2h'],
   }
+
   #NGINX Conf
   file { '/opt/nginx_conf':
     ensure => directory,
@@ -33,25 +61,15 @@ class sunet::oidf::ta(
     group   => 'root',
     content => file('sunet/oidf/ta-nginx_default.conf')
   }
-  sunet::nftables::allow { 'expose-admin':
-    from => 'any',
-    port => 8000,
-  }
-  sunet::nftables::allow { 'expose-always-https':
-    from => 'any',
-    port => 80,
-  }
   sunet::nftables::allow { 'expose-TA-ssl':
     from => 'any',
     port => 443,
   }
-  sunet::nftables::allow { 'expose-admin-ssl':
-    from => 'any',
-    port => 8443,
-  }
 
-  sunet::snippets::secret_file { '/opt/oidf-ta/private.json':
-    hiera_key => 'inmor_key',
-    mode      => '0644',
+  if (! $acmed) {
+    sunet::nftables::allow { 'expose-always-https':
+      from => 'any',
+      port => 80,
+    }
   }
 }
