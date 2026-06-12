363420b04ddb01ec019bb7d8ad60e073d2b8a850
Maria Haider <mariah@sunet.se>
2026-06-12 15:14:16 +0200

    fixed syntam mistake

--- templates/mariadb/backup/start_replica_from_init.erb.sh
+++ templates/mariadb/backup/start_replica_from_init.erb.sh
@@ -13,9 +13,9 @@ if [[ -f ${init_file} ]]; then
 	${mysql} -e "STOP SLAVE;RESET SLAVE;"
 	master_command=$(zgrep 'CHANGE MASTER TO MASTER_LOG_FILE' ${init_file} | sed -e 's/^-- //' -e 's/;$//')
 	master_command="${master_command}, MASTER_HOST='<%= @replicate_from %>', MASTER_USER='backup'"
-	master_command="${master_command}, MASTER_PASSWORD='<%= @mariadb_backup_password%>, MASTER_SSL=1'"
+	master_command="${master_command}, MASTER_PASSWORD='<%= @mariadb_backup_password%>', MASTER_SSL=1s"
 	<% if @use_tls -%>
-	master_command="${master_command}, MASTER_SSL_CA=<%= @ca_cert_path %>, MASTER_SSL_CERT=/etc/mysql/ssl/cert.pem, MASTER_SSL_KEY= /etc/mysql/ssl/privkey.pem"
+	master_command="${master_command}, MASTER_SSL_CA='<%= @ca_cert_path %>', MASTER_SSL_CERT='/etc/mysql/ssl/cert.pem', MASTER_SSL_KEY='/etc/mysql/ssl/privkey.pem'"
 	<% end -%>
 	master_command="${master_command}, MASTER_CONNECT_RETRY=20"
 	zcat ${init_file} | ${mysql}
