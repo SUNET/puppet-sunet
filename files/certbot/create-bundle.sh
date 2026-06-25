73eed811f8babd720ab92a16086655ec68f0cccc
Maria Haider <mariah@sunet.se>
2026-06-25 11:35:56 +0200

    optional creation of certbot bundle

---
+++ files/certbot/create-bundle.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+install -m 640 \
+    "${RENEWED_LINEAGE}/privkey.pem" \
+    "/tmp/bundle.pem"
+cat "${RENEWED_LINEAGE}/fullchain.pem >> /tmp/bundle.pem
+mv /tmp/bundle.pem "${RENEWED_LINEAGE}/"
