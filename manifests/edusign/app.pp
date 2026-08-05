af7459985df325ec58e592adc970457e966b66bd
Mikael Andersson <mikand@sunet.se>
2026-08-05 11:59:12 +0200

    removing static certhandling as its now handled dynamically.

--- manifests/edusign/app.pp
+++ manifests/edusign/app.pp
@@ -55,7 +55,7 @@ class sunet::edusign::app(
       imagetag => $version,
       hostname => $facts['networking']['fqdn'],
       volumes  => ['/var/log:/var/log','/etc/ssl:/etc/ssl','/etc/dehydrated:/etc/dehydrated','/etc/metadata:/etc/metadata:ro',
-                    '/etc/edusign:/etc/edusign:ro', '/opt/metadata/trust/swamid/swamid-qa.crt:/etc/shibboleth/md-signer2.crt:ro'],
+                    '/etc/edusign:/etc/edusign:ro'],
       env      => $env_sp_final,
       depends  => ['edusign-app'],
       ports    => ['443:443','80:80']
