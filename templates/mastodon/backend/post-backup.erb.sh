2aa9d023bab4cea6e57cf405c5e3b22eced8f077
Johan Wassberg <jocar@sunet.se>
2026-09-22 16:00:49 +0200

    Please Sonar
    And also shfmt

--- templates/mastodon/backend/post-backup.erb.sh
+++ templates/mastodon/backend/post-backup.erb.sh
@@ -9,11 +9,10 @@ find "${backupdir}/postgres" -mtime +${localretentiondays} -exec rm {} \;
 find "${backupdir}/redis" -mtime +${localretentiondays} -exec rm {} \;
 
 # Only store one dump per day for files older then yesterday
-while IFS= read -r -d '' file
-do
-  oclock=$(echo "$file" | cut -d . -f 2)
-  if [ "${oclock}x" != "00x" ]; then
-    rm "$file"
-  fi
+while IFS= read -r -d '' file; do
+	oclock=$(echo "$file" | cut -d . -f 2)
+	if [[ "${oclock}x" != "00x" ]]; then
+		rm "$file"
+	fi
 
 done < <(find /opt/backups/ -type f -mtime +1 -print0)
